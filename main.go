b2619b98c79cf6c1a57bbb6f07f01cbb16716e8e
Tim Schindler <xh3b4sd@gmail.com>
2017-12-12 10:31:07 +0100

    setup circle builds (#1)
    * added circle config
    
    * updated README.md
    
    * added main.go
    
    * added Circle Status Badge to README.md

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+var (
+	description string = "The cluster-operator is an in-cluster agent that handles Giant Swarm guest cluster specific resources."
+	gitCommit   string = "n/a"
+	name        string = "cluster-operator"
+	source      string = "https://github.com/giantswarm/cluster-operator"
+)
+
+func main() {
+
+}
