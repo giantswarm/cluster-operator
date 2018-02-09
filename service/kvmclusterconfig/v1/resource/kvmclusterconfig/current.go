01a6cb6f6822bed2a5267df13e237575b1bf8a9a
Tuomas Makinen <tuommaki@gmail.com>
2018-02-09 10:45:13 +0200

    Return nil instead of error in unimplemented methods

--- service/kvmclusterconfig/v1/resource/kvmclusterconfig/current.go
+++ service/kvmclusterconfig/v1/resource/kvmclusterconfig/current.go
@@ -2,10 +2,8 @@ package kvmclusterconfig
 
 import (
 	"context"
-
-	"github.com/giantswarm/microerror"
 )
 
 func (r *Resource) GetCurrentState(ctx context.Context, obj interface{}) (interface{}, error) {
-	return nil, microerror.New("not implemented")
+	return nil, nil
 }
--- service/kvmclusterconfig/v1/resource/kvmclusterconfig/delete.go
+++ service/kvmclusterconfig/v1/resource/kvmclusterconfig/delete.go
@@ -3,14 +3,13 @@ package kvmclusterconfig
 import (
 	"context"
 
-	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/operatorkit/framework"
 )
 
 func (r *Resource) ApplyDeleteChange(ctx context.Context, obj, deleteChange interface{}) error {
-	return microerror.New("not implemented")
+	return nil
 }
 
 func (r *Resource) NewDeletePatch(ctx context.Context, obj, currentState, desiredState interface{}) (*framework.Patch, error) {
-	return nil, microerror.New("not implemented")
+	return nil, nil
 }
