adb154d784ff64a52af17a605b4f1f690fa5194c
Tuomas Mäkinen <1947505+tuommaki@users.noreply.github.com>
2018-02-14 11:43:14 +0200

    Add encryptionkey.GetCurrentState() implementation (#13)
    Simple implementation that looks for secret named after clusterID.

--- service/kvmclusterconfig/v1/resource/encryptionkey/update.go
+++ service/kvmclusterconfig/v1/resource/encryptionkey/update.go
@@ -3,14 +3,18 @@ package encryptionkey
 import (
 	"context"
 
-	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/operatorkit/framework"
 )
 
+// ApplyUpdateChange takes observed custom object and update portion of the
+// Patch provided by NewUpdatePatch or NewDeletePatch. This is currently a NOP
+// for encryptionkey Resource.
 func (r *Resource) ApplyUpdateChange(ctx context.Context, obj, updateChange interface{}) error {
-	return microerror.New("not implemented")
+	return nil
 }
 
+// NewUpdatePatch computes appropriate Patch based on difference in current
+// state and desired state.
 func (r *Resource) NewUpdatePatch(ctx context.Context, obj, currentState, desiredState interface{}) (*framework.Patch, error) {
-	return nil, microerror.New("not implemented")
+	return nil, nil
 }
--- service/kvmclusterconfig/v1/resource/kvmconfig/create.go
+++ service/kvmclusterconfig/v1/resource/kvmconfig/create.go
@@ -4,6 +4,9 @@ import (
 	"context"
 )
 
+// ApplyCreateChange takes observed custom object and create portion of the
+// Patch provided by NewUpdatePatch or NewDeletePatch. It creates KVMConfig if
+// needed.
 func (r *Resource) ApplyCreateChange(ctx context.Context, obj, createChange interface{}) error {
 	return nil
 }
