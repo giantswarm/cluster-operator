7edc795da35e77fe20907b077a9a1e955ef9e9e9
Tuomas Makinen <tuommaki@gmail.com>
2018-02-14 11:38:09 +0200

    Sort functions alphabetically in key package

--- service/kvmclusterconfig/v1/key/key.go
+++ service/kvmclusterconfig/v1/key/key.go
@@ -8,6 +8,17 @@ import (
 	"github.com/giantswarm/randomkeytpr"
 )
 
+// ClusterID extracts clusterID from v1alpha1.KVMClusterConfig.
+func ClusterID(customObject v1alpha1.KVMClusterConfig) string {
+	return customObject.Spec.Guest.ID
+}
+
+// EncryptionKeySecretName generates name for a Kubernetes secret based on
+// information in given v1alpha1.KVMClusterConfig.
+func EncryptionKeySecretName(customObject v1alpha1.KVMClusterConfig) string {
+	return fmt.Sprintf("%s-%s", ClusterID(customObject), randomkeytpr.EncryptionKey.String())
+}
+
 // ToCustomObject converts value to v1alpha1.KVMClusterConfig and returns it or
 // error if type does not match.
 func ToCustomObject(v interface{}) (v1alpha1.KVMClusterConfig, error) {
@@ -18,15 +29,4 @@ func ToCustomObject(v interface{}) (v1alpha1.KVMClusterConfig, error) {
 	customObject := *customObjectPointer
 
 	return customObject, nil
-}
-
-// ClusterID extracts clusterID from v1alpha1.KVMClusterConfig.
-func ClusterID(customObject v1alpha1.KVMClusterConfig) string {
-	return customObject.Spec.Guest.ID
-}
-
-// EncryptionKeySecretName generates name for a Kubernetes secret based on
-// information in given v1alpha1.KVMClusterConfig.
-func EncryptionKeySecretName(customObject v1alpha1.KVMClusterConfig) string {
-	return fmt.Sprintf("%s-%s", ClusterID(customObject), randomkeytpr.EncryptionKey.String())
 }
