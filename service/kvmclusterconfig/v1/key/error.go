5d0e5162eefbe66413bd7459205b4e904333b9c7
Tuomas Makinen <tuommaki@gmail.com>
2018-02-15 12:44:20 +0200

    Sort errors alphabetically

--- service/kvmclusterconfig/v1/key/error.go
+++ service/kvmclusterconfig/v1/key/error.go
@@ -2,16 +2,16 @@ package key
 
 import "github.com/giantswarm/microerror"
 
-var wrongTypeError = microerror.New("wrong type")
-
-// IsWrongTypeError asserts wrongTypeError.
-func IsWrongTypeError(err error) bool {
-	return microerror.Cause(err) == wrongTypeError
-}
-
 var emptyValueError = microerror.New("empty value")
 
 // IsEmptyValueError asserts emptyValueError.
 func IsEmptyValueError(err error) bool {
 	return microerror.Cause(err) == emptyValueError
+}
+
+var wrongTypeError = microerror.New("wrong type")
+
+// IsWrongTypeError asserts wrongTypeError.
+func IsWrongTypeError(err error) bool {
+	return microerror.Cause(err) == wrongTypeError
 }
