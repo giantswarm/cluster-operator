76bc039a685a9c5bcf089f7ff0f282138c628d1e
Tuomas Mäkinen <1947505+tuommaki@users.noreply.github.com>
2018-02-15 13:11:51 +0200

    Add unit tests to key package (#17)
    Key package lacked unit tests. While creating them, also noticed that
    ToCustomObject was missing empty value check so added it (accompanied
    with corresponding error).

--- service/kvmclusterconfig/v1/key/error.go
+++ service/kvmclusterconfig/v1/key/error.go
@@ -2,6 +2,13 @@ package key
 
 import "github.com/giantswarm/microerror"
 
+var emptyValueError = microerror.New("empty value")
+
+// IsEmptyValueError asserts emptyValueError.
+func IsEmptyValueError(err error) bool {
+	return microerror.Cause(err) == emptyValueError
+}
+
 var wrongTypeError = microerror.New("wrong type")
 
 // IsWrongTypeError asserts wrongTypeError.
