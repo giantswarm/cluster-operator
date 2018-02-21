1b2100c557e2b16eb26c4523ad5a6815a7d0f40a
Tuomas Mäkinen <1947505+tuommaki@users.noreply.github.com>
2018-02-21 11:01:06 +0200

    Add initial Resource structure for certconfig (#22)
    Certconfig resource orders certificates (from cert-operator) for guest
    cluster. This adds basic operatorkit resource implementation skeleton
    for it.
    
    Because certconfig is used in all platforms (AWS, Azure and KVM), the
    resource implementation is made generic so that it can be shared between
    provider specific CRD framework implementations.
    
    Main design difference from common in-framework resource implementation
    is the requirement for v1alpha1.ClusterGuestConfig extraction function.
    This makes it possible to extract detailed information from custom object in
    provider independent way.

---
+++ pkg/resource/v1/certconfig/resource.go
@@ -0,0 +1,59 @@
+package certconfig
+
+import (
+	"github.com/giantswarm/apiextensions/pkg/apis/core/v1alpha1"
+	"github.com/giantswarm/microerror"
+	"github.com/giantswarm/micrologger"
+	"github.com/giantswarm/operatorkit/framework"
+	"k8s.io/client-go/kubernetes"
+)
+
+const (
+	// Name is the identifier of the resource.
+	Name = "certconfigv1"
+)
+
+// Config represents the configuration used to create a new cloud config resource.
+type Config struct {
+	K8sClient                kubernetes.Interface
+	Logger                   micrologger.Logger
+	ToClusterGuestConfigFunc func(obj interface{}) (*v1alpha1.ClusterGuestConfig, error)
+}
+
+// Resource implements the cloud config resource.
+type Resource struct {
+	k8sClient                kubernetes.Interface
+	logger                   micrologger.Logger
+	toClusterGuestConfigFunc func(obj interface{}) (*v1alpha1.ClusterGuestConfig, error)
+}
+
+// New creates a new configured cloud config resource.
+func New(config Config) (*Resource, error) {
+	if config.K8sClient == nil {
+		return nil, microerror.Maskf(invalidConfigError, "config.K8sClient must not be empty")
+	}
+	if config.ToClusterGuestConfigFunc == nil {
+		return nil, microerror.Maskf(invalidConfigError, "config.ToClusterGuestConfigFunc must not be empty")
+	}
+	if config.Logger == nil {
+		return nil, microerror.Maskf(invalidConfigError, "config.Logger must not be empty")
+	}
+
+	newService := &Resource{
+		k8sClient:                config.K8sClient,
+		toClusterGuestConfigFunc: config.ToClusterGuestConfigFunc,
+		logger: config.Logger.With(
+			"resource", Name,
+		),
+	}
+
+	return newService, nil
+}
+
+func (r *Resource) Name() string {
+	return Name
+}
+
+func (r *Resource) Underlying() framework.Resource {
+	return r
+}
