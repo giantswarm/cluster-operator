2f6f6625125ee304b5462447a7a518532e56fcab
Tuomas Makinen <tuommaki@gmail.com>
2018-02-26 10:16:48 +0200

    Style: Full stop after comment.

--- pkg/label/label.go
+++ pkg/label/label.go
@@ -1,5 +1,5 @@
 // Package label contains common Kubernetes object labels. These are defined in
-// https://github.com/giantswarm/fmt/blob/master/kubernetes/annotations_and_labels.md
+// https://github.com/giantswarm/fmt/blob/master/kubernetes/annotations_and_labels.md.
 package label
 
 const (
