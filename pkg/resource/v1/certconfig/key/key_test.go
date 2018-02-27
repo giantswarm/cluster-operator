016433826dbda332917be53ed5ff607d10bcde85
Tuomas Mäkinen <1947505+tuommaki@users.noreply.github.com>
2018-02-27 11:10:58 +0200

    Initial implementation of GetCurrentState() for certconfig resource (#23)
    Basic implementation that lists current CertConfig objects from K8S API
    when they belong to cluster defined in custom object.

--- pkg/resource/v1/certconfig/key/key_test.go
+++ pkg/resource/v1/certconfig/key/key_test.go
@@ -1,1 +1,83 @@
 package key
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/giantswarm/apiextensions/pkg/apis/core/v1alpha1"
+	"github.com/giantswarm/certs"
+)
+
+func Test_CertConfigName(t *testing.T) {
+	testCases := []struct {
+		description            string
+		clusterConfig          v1alpha1.ClusterGuestConfig
+		cert                   certs.Cert
+		expectedCertConfigName string
+	}{
+		{
+			description:   "empty ClusterGuestConfig value with APICert",
+			clusterConfig: v1alpha1.ClusterGuestConfig{},
+			cert:          certs.APICert,
+			expectedCertConfigName: fmt.Sprintf("-%s", certs.APICert),
+		},
+		{
+			description: "ClusterGuestConfig with ID and WorkerCert",
+			clusterConfig: v1alpha1.ClusterGuestConfig{
+				ID:   "cluster-1",
+				Name: "Test cluster nr. 1",
+			},
+			cert: certs.WorkerCert,
+			expectedCertConfigName: fmt.Sprintf("cluster-1-%s", certs.WorkerCert),
+		},
+		{
+			description: "ClusterGuestConfig with ID and empty value for cert",
+			clusterConfig: v1alpha1.ClusterGuestConfig{
+				ID:   "cluster-1",
+				Name: "Test cluster nr. 1",
+			},
+			cert: "",
+			expectedCertConfigName: "cluster-1-",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.description, func(t *testing.T) {
+			name := CertConfigName(tc.clusterConfig, tc.cert)
+			if name != tc.expectedCertConfigName {
+				t.Fatalf("CertConfigName '%s' doesn't match expected '%s'", name, tc.expectedCertConfigName)
+			}
+		})
+	}
+}
+
+func Test_ClusterID(t *testing.T) {
+	testCases := []struct {
+		description   string
+		clusterConfig v1alpha1.ClusterGuestConfig
+		expectedID    string
+	}{
+		{
+			description:   "empty value",
+			clusterConfig: v1alpha1.ClusterGuestConfig{},
+			expectedID:    "",
+		},
+		{
+			description: "ClusterGuestConfig with ID",
+			clusterConfig: v1alpha1.ClusterGuestConfig{
+				ID:   "cluster-1",
+				Name: "Test cluster nr. 1",
+			},
+			expectedID: "cluster-1",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.description, func(t *testing.T) {
+			id := ClusterID(tc.clusterConfig)
+			if id != tc.expectedID {
+				t.Fatalf("ClusterID '%s' doesn't match expected '%s'", id, tc.expectedID)
+			}
+		})
+	}
+}
