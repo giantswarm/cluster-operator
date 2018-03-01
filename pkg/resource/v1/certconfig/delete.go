819787e0a0f8ecc0fecc036a8ec3c15477ae7642
Tuomas Makinen <tuommaki@gmail.com>
2018-03-01 14:56:31 +0200

    Implement NewDeletePatch() for certconfig resource
    Basic implementation of NewDeletePatch() that creates delete change to
    Patch for all current certconfigs that GetCurrentState() returns.

--- pkg/resource/v1/certconfig/delete.go
+++ pkg/resource/v1/certconfig/delete.go
@@ -2,7 +2,10 @@ package certconfig
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/giantswarm/apiextensions/pkg/apis/core/v1alpha1"
+	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/operatorkit/framework"
 )
 
@@ -11,5 +14,26 @@ func (r *Resource) ApplyDeleteChange(ctx context.Context, obj, deleteChange inte
 }
 
 func (r *Resource) NewDeletePatch(ctx context.Context, obj, currentState, desiredState interface{}) (*framework.Patch, error) {
-	return nil, nil
+	delete, err := r.newDeleteChange(ctx, obj, currentState, desiredState)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	patch := framework.NewPatch()
+	patch.SetDeleteChange(delete)
+
+	return patch, nil
+}
+
+func (r *Resource) newDeleteChange(ctx context.Context, obj, currentState, desiredState interface{}) ([]*v1alpha1.CertConfig, error) {
+	currentCertConfigs, err := toCertConfigs(currentState)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	r.logger.LogCtx(ctx, "debug", "finding out which certconfigs have to be deleted")
+
+	r.logger.LogCtx(ctx, "debug", fmt.Sprintf("found %d certconfigs that have to be deleted", len(currentCertConfigs)))
+
+	return currentCertConfigs, nil
 }
