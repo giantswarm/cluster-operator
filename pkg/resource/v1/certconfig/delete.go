b51814a47cca8dfd95bb1dbfdd5ee1c0678e39e8
Tuomas Mäkinen <1947505+tuommaki@users.noreply.github.com>
2018-03-02 15:23:28 +0200

    Add newDeleteChangeForUpdatePatch() for resource cleanup (#28)
    When there are changes in GetDesiredState(), it is possible that there
    are resources in existing environments that are not desired anymore.
    Undesired resources should be delete so that they don't cause
    distraction for engineers when investigating system state.

--- pkg/resource/v1/certconfig/delete.go
+++ pkg/resource/v1/certconfig/delete.go
@@ -19,7 +19,7 @@ func (r *Resource) ApplyDeleteChange(ctx context.Context, obj, deleteChange inte
 // analyses the current and desired state and returns the patch to be applied by
 // Create, Update and Delete functions.
 func (r *Resource) NewDeletePatch(ctx context.Context, obj, currentState, desiredState interface{}) (*framework.Patch, error) {
-	delete, err := r.newDeleteChange(ctx, obj, currentState, desiredState)
+	delete, err := r.newDeleteChangeForDeletePatch(ctx, obj, currentState, desiredState)
 	if err != nil {
 		return nil, microerror.Mask(err)
 	}
@@ -30,7 +30,7 @@ func (r *Resource) NewDeletePatch(ctx context.Context, obj, currentState, desire
 	return patch, nil
 }
 
-func (r *Resource) newDeleteChange(ctx context.Context, obj, currentState, desiredState interface{}) ([]*v1alpha1.CertConfig, error) {
+func (r *Resource) newDeleteChangeForDeletePatch(ctx context.Context, obj, currentState, desiredState interface{}) ([]*v1alpha1.CertConfig, error) {
 	currentCertConfigs, err := toCertConfigs(currentState)
 	if err != nil {
 		return nil, microerror.Mask(err)
@@ -41,4 +41,30 @@ func (r *Resource) newDeleteChange(ctx context.Context, obj, currentState, desir
 	r.logger.LogCtx(ctx, "debug", fmt.Sprintf("found %d certconfigs that have to be deleted", len(currentCertConfigs)))
 
 	return currentCertConfigs, nil
+}
+
+func (r *Resource) newDeleteChangeForUpdatePatch(ctx context.Context, obj, currentState, desiredState interface{}) ([]*v1alpha1.CertConfig, error) {
+	currentCertConfigs, err := toCertConfigs(currentState)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+	desiredCertConfigs, err := toCertConfigs(desiredState)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	r.logger.LogCtx(ctx, "debug", "finding out which certconfigs have to be deleted")
+
+	var certConfigsToDelete []*v1alpha1.CertConfig
+	for _, currentCertConfig := range currentCertConfigs {
+		_, err := getCertConfigByName(desiredCertConfigs, currentCertConfig.Name)
+		// Existing CertConfig is not desired anymore so it should be deleted.
+		if IsNotFound(err) {
+			certConfigsToDelete = append(certConfigsToDelete, currentCertConfig)
+		}
+	}
+
+	r.logger.LogCtx(ctx, "debug", fmt.Sprintf("found %d certconfigs that have to be deleted", len(certConfigsToDelete)))
+
+	return certConfigsToDelete, nil
 }
