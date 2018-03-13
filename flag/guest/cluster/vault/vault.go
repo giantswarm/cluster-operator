67e8880024bfd90482d904c36ac9f83690b60b1b
Tuomas Makinen <tuommaki@gmail.com>
2018-03-13 12:42:11 +0200

    Fix import

--- flag/guest/cluster/vault/vault.go
+++ flag/guest/cluster/vault/vault.go
@@ -1,7 +1,7 @@
 package vault
 
 import (
-	"github.com/giantswarm/kubernetesd/flag/service/vault/certificate"
+	"github.com/giantswarm/cluster-operator/flag/guest/cluster/vault/certificate"
 )
 
 // Vault is a data structure to hold guest cluster vault related configuration.
