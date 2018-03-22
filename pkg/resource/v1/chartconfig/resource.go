d999a03f1a80aff3cb320bbda5004cff4905e147
Ross Fairbanks <ross@giantswarm.io>
2018-03-22 13:46:22 +0100

    Address review comments.

--- pkg/resource/v1/chartconfig/resource.go
+++ pkg/resource/v1/chartconfig/resource.go
@@ -2,7 +2,6 @@ package chartconfig
 
 import (
 	"github.com/giantswarm/apiextensions/pkg/clientset/versioned"
-	"github.com/giantswarm/cluster-operator/pkg/cluster"
 	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/micrologger"
 	"k8s.io/client-go/kubernetes"
@@ -15,27 +14,22 @@ const (
 
 // Config represents the configuration used to create a new chart config resource.
 type Config struct {
-	BaseClusterConfig *cluster.Config
-	G8sClient         versioned.Interface
-	K8sClient         kubernetes.Interface
-	Logger            micrologger.Logger
-	ProjectName       string
+	G8sClient   versioned.Interface
+	K8sClient   kubernetes.Interface
+	Logger      micrologger.Logger
+	ProjectName string
 }
 
 // Resource implements the chart config resource.
 type Resource struct {
-	baseClusterConfig *cluster.Config
-	g8sClient         versioned.Interface
-	k8sClient         kubernetes.Interface
-	logger            micrologger.Logger
-	projectName       string
+	g8sClient   versioned.Interface
+	k8sClient   kubernetes.Interface
+	logger      micrologger.Logger
+	projectName string
 }
 
 // New creates a new configured chart config resource.
 func New(config Config) (*Resource, error) {
-	if config.BaseClusterConfig == nil {
-		return nil, microerror.Maskf(invalidConfigError, "config.BaseClusterConfig must not be empty")
-	}
 	if config.G8sClient == nil {
 		return nil, microerror.Maskf(invalidConfigError, "%T.G8sClient must not be empty", config)
 	}
@@ -50,11 +44,10 @@ func New(config Config) (*Resource, error) {
 	}
 
 	newResource := &Resource{
-		baseClusterConfig: config.BaseClusterConfig,
-		g8sClient:         config.G8sClient,
-		k8sClient:         config.K8sClient,
-		logger:            config.Logger,
-		projectName:       config.ProjectName,
+		g8sClient:   config.G8sClient,
+		k8sClient:   config.K8sClient,
+		logger:      config.Logger,
+		projectName: config.ProjectName,
 	}
 
 	return newResource, nil
