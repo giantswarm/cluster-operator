130b7c78846b9f681f061be27ff8d3148992df81
Tuomas Makinen <tuommaki@gmail.com>
2018-03-27 10:53:15 +0300

    Fix resource names to correct version

--- service/awsclusterconfig/v1/resource/awsconfig/resource.go
+++ service/awsclusterconfig/v1/resource/awsconfig/resource.go
@@ -9,7 +9,7 @@ import (
 
 const (
 	// Name is the identifier of the resource.
-	Name = "awsconfigv10"
+	Name = "awsconfigv1"
 )
 
 // Config represents the configuration used to create a new cloud config resource.
