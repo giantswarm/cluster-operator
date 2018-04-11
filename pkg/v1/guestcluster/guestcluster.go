1b67a5c9802cea939a033742524abd8dda96cf2e
Ross Fairbanks <ross@giantswarm.io>
2018-04-11 19:17:03 +0200

    Add NewK8sClient for connecting to guest clusters

--- pkg/v1/guestcluster/guestcluster.go
+++ pkg/v1/guestcluster/guestcluster.go
@@ -8,6 +8,8 @@ import (
 	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/micrologger"
 	"github.com/giantswarm/operatorkit/client/k8srestconfig"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 )
 
 // Config represents the configuration used to create a new guest cluster service.
@@ -41,6 +43,43 @@ func New(config Config) (*Service, error) {
 
 // NewG8sClient returns a generated clientset for the specified guest cluster.
 func (s *Service) NewG8sClient(ctx context.Context, clusterID, apiDomain string) (versioned.Interface, error) {
+	s.logger.LogCtx(ctx, "level", "debug", "message", "creating G8s client for the guest cluster")
+
+	restConfig, err := s.newKubernetesRestConfig(ctx, clusterID, apiDomain)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	g8sClient, err := versioned.NewForConfig(restConfig)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	s.logger.LogCtx(ctx, "level", "debug", "message", "created G8s client for the guest cluster")
+
+	return g8sClient, nil
+}
+
+// NewK8sClient returns a Kubernetes clientset for the specified guest cluster.
+func (s *Service) NewK8sClient(ctx context.Context, clusterID, apiDomain string) (kubernetes.Interface, error) {
+	s.logger.LogCtx(ctx, "level", "debug", "message", "creating K8s client for the guest cluster")
+
+	restConfig, err := s.newKubernetesRestConfig(ctx, clusterID, apiDomain)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	k8sClient, err := kubernetes.NewForConfig(restConfig)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	s.logger.LogCtx(ctx, "level", "debug", "message", "created K8s client for the guest cluster")
+
+	return k8sClient, nil
+}
+
+func (s *Service) newKubernetesRestConfig(ctx context.Context, clusterID, apiDomain string) (*rest.Config, error) {
 	s.logger.LogCtx(ctx, "level", "debug", "message", "looking for certificate to connect to the guest cluster")
 
 	operatorCerts, err := s.certsSearcher.SearchClusterOperator(clusterID)
@@ -49,8 +88,6 @@ func (s *Service) NewG8sClient(ctx context.Context, clusterID, apiDomain string)
 	}
 
 	s.logger.LogCtx(ctx, "level", "debug", "message", "found certificate for connecting to the guest cluster")
-
-	s.logger.LogCtx(ctx, "level", "debug", "message", "creating Kubernetes client for the guest cluster")
 
 	c := k8srestconfig.Config{
 		Logger: s.logger,
@@ -68,12 +105,5 @@ func (s *Service) NewG8sClient(ctx context.Context, clusterID, apiDomain string)
 		return nil, microerror.Mask(err)
 	}
 
-	g8sClient, err := versioned.NewForConfig(restConfig)
-	if err != nil {
-		return nil, microerror.Mask(err)
-	}
-
-	s.logger.LogCtx(ctx, "level", "debug", "message", "created Kubernetes client for the guest cluster")
-
-	return g8sClient, nil
+	return restConfig, nil
 }
--- pkg/v1/guestcluster/spec.go
+++ pkg/v1/guestcluster/spec.go
@@ -4,8 +4,12 @@ import (
 	"context"
 
 	"github.com/giantswarm/apiextensions/pkg/clientset/versioned"
+	"k8s.io/client-go/kubernetes"
 )
 
 type Interface interface {
+	// NewG8sClient returns a new generated clientset for a guest cluster.
 	NewG8sClient(ctx context.Context, clusterID, apiDomain string) (versioned.Interface, error)
+	// NewK8sClient returns a new Kubernetes clientset for a guest cluster.
+	NewK8sClient(ctx context.Context, clusterID, apiDomain string) (kubernetes.Interface, error)
 }
