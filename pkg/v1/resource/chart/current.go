4ced77003d6817e700c14289d3cc43e7d9b603d0
Ross Fairbanks <ross@giantswarm.io>
2018-04-13 19:32:17 +0200

    Implement GetCurrentState

--- pkg/v1/resource/chart/current.go
+++ pkg/v1/resource/chart/current.go
@@ -1,7 +1,65 @@
 package chart
 
-import "context"
+import (
+	"context"
+
+	"github.com/giantswarm/helmclient"
+	"github.com/giantswarm/microerror"
+	"github.com/giantswarm/operatorkit/framework/context/resourcecanceledcontext"
+
+	"github.com/giantswarm/cluster-operator/pkg/v1/guestcluster"
+)
 
 func (r *Resource) GetCurrentState(ctx context.Context, obj interface{}) (interface{}, error) {
-	return nil, nil
+	clusterGuestConfig, err := r.toClusterGuestConfigFunc(obj)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	r.logger.LogCtx(ctx, "level", "debug", "message", "looking for chart-operator chart in the guest cluster")
+
+	clusterConfig, err := prepareClusterConfig(r.baseClusterConfig, clusterGuestConfig)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	guestHelmClient, err := r.guest.NewHelmClient(ctx, clusterConfig.ClusterID, clusterConfig.Domain.API)
+	if guestcluster.IsNotFound(err) {
+		r.logger.LogCtx(ctx, "level", "debug", "message", "did not find the cluster-operator api cert in the Kubernetes API")
+
+		// We can't continue without the cluster-operator cert. We will retry
+		// during the next execution.
+		resourcecanceledcontext.SetCanceled(ctx)
+		r.logger.LogCtx(ctx, "level", "debug", "message", "canceling resource reconciliation for custom object")
+
+		return nil, nil
+
+	} else if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	releaseContent, err := guestHelmClient.GetReleaseContent(chartOperatorRelease)
+	if helmclient.IsReleaseNotFound(err) {
+		r.logger.LogCtx(ctx, "level", "debug", "message", "did not find the chart-operator chart in the guest cluster")
+		return nil, nil
+
+	} else if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	releaseHistory, err := guestHelmClient.GetReleaseHistory(chartOperatorRelease)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	chartState := &ResourceState{
+		ChartName:      chartOperatorChart,
+		ReleaseName:    chartOperatorRelease,
+		ReleaseStatus:  releaseContent.Status,
+		ReleaseVersion: releaseHistory.Version,
+	}
+
+	r.logger.LogCtx(ctx, "level", "debug", "message", "found chart-operator chart in the guest cluster")
+
+	return chartState, nil
 }
