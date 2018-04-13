43a17bd42bdfe2f08647057b19dee100e31c4eae
Ross Fairbanks <ross@giantswarm.io>
2018-04-13 19:28:46 +0200

    Add NewHelmClient to guestcluster service

--- pkg/v1/resource/chartconfig/mock_test.go
+++ pkg/v1/resource/chartconfig/mock_test.go
@@ -4,23 +4,22 @@ import (
 	"context"
 
 	"github.com/giantswarm/apiextensions/pkg/clientset/versioned"
+	"github.com/giantswarm/helmclient"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
 )
 
 type guestMock struct {
 	fakeGuestG8sClient  versioned.Interface
+	fakeGuestHelmClient helmclient.Interface
 	fakeGuestK8sClient  kubernetes.Interface
-	fakeGuestRestConfig *rest.Config
 }
 
 func (g *guestMock) NewG8sClient(ctx context.Context, clusterID, apiDomain string) (versioned.Interface, error) {
 	return g.fakeGuestG8sClient, nil
 }
+func (g *guestMock) NewHelmClient(ctx context.Context, clusterID, apiDomain string) (helmclient.Interface, error) {
+	return g.fakeGuestHelmClient, nil
+}
 func (g *guestMock) NewK8sClient(ctx context.Context, clusterID, apiDomain string) (kubernetes.Interface, error) {
 	return g.fakeGuestK8sClient, nil
-}
-
-func (g *guestMock) NewRestConfig(ctx context.Context, clusterID, apiDomain string) (*rest.Config, error) {
-	return g.fakeGuestRestConfig, nil
 }
