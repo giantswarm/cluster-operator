97f72a81c4ad31587d1d2ac7d328ea6e08174429
Tobias Richter <tr4711@gmail.com>
2018-04-17 10:47:23 +0200

    Add ApplyDeleteChange

--- pkg/v2/resource/chart/delete.go
+++ pkg/v2/resource/chart/delete.go
@@ -7,6 +7,28 @@ import (
 )
 
 func (r *Resource) ApplyDeleteChange(ctx context.Context, obj, deleteChange interface{}) error {
+	guestHelmClient, err := r.getGuestHelmClient(ctx, obj)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	deleteState, err := toResourceState(deleteChange)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	if deleteState.ReleaseName != "" {
+		r.logger.LogCtx(ctx, "level", "debug", "message", "deleting chart-operator chart")
+
+		guestHelmClient.DeleteRelease(deleteState.ReleaseName, helm.DeletePurge(true))
+		if err != nil {
+			return microerror.Mask(err)
+		}
+		r.logger.LogCtx(ctx, "level", "debug", "message", "deleted chart-operator chart")
+	} else {
+		r.logger.LogCtx(ctx, "level", "debug", "message", "not deleting chart-operator chart")
+	}
+
 	return nil
 }
 
