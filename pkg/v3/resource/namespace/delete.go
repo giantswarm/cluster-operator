cee663bba4763d0f5d4c55c19c64fbe2e7a0ed9d
Ross Fairbanks <ross@giantswarm.io>
2018-04-30 13:25:43 +0200

    Comment NewDeletePatch

--- pkg/v3/resource/namespace/delete.go
+++ pkg/v3/resource/namespace/delete.go
@@ -10,6 +10,8 @@ func (r *Resource) ApplyDeleteChange(ctx context.Context, obj, deleteChange inte
 	return nil
 }
 
+// NewDeletePatch is a no-op because the namespace in the guest cluster is
+// deleted with the guest cluster resources.
 func (r *Resource) NewDeletePatch(ctx context.Context, obj, currentState, desiredState interface{}) (*controller.Patch, error) {
 	return nil, nil
 }
