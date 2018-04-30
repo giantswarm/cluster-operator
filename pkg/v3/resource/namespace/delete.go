85394042fbe540b4ec8f254ed1d77ddff86353d3
Ross Fairbanks <ross@giantswarm.io>
2018-04-30 13:28:16 +0200

    Add godoc for ApplyDeleteChange

--- pkg/v3/resource/namespace/delete.go
+++ pkg/v3/resource/namespace/delete.go
@@ -6,6 +6,8 @@ import (
 	"github.com/giantswarm/operatorkit/controller"
 )
 
+// ApplyDeleteChange is a no-op because the namespace in the guest cluster is
+// deleted with the guest cluster resources.
 func (r *Resource) ApplyDeleteChange(ctx context.Context, obj, deleteChange interface{}) error {
 	return nil
 }
