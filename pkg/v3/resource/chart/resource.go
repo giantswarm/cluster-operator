0b0868b5e936bebb6a5666820178009edc27801f
Ross Fairbanks <ross@giantswarm.io>
2018-05-08 15:37:04 +0200

    Use 0-1-beta channel not stable

--- pkg/v3/resource/chart/resource.go
+++ pkg/v3/resource/chart/resource.go
@@ -23,7 +23,7 @@ const (
 	Name = "chartv3"
 
 	chartOperatorChart         = "chart-operator-chart"
-	chartOperatorChannel       = "0-1-stable"
+	chartOperatorChannel       = "0-1-beta"
 	chartOperatorRelease       = "chart-operator"
 	chartOperatorNamespace     = "kube-system"
 	chartOperatorDesiredStatus = "DEPLOYED"
