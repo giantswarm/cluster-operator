d4e7ae3e3a0094cd7dd7a061696bb254d1d78c59
Ross Fairbanks <ross@giantswarm.io>
2018-05-08 13:23:28 +0200

    Wire up v3 version bundles

--- service/version_bundles.go
+++ service/version_bundles.go
@@ -5,10 +5,13 @@ import (
 
 	awsv1 "github.com/giantswarm/cluster-operator/service/controller/aws/v1"
 	awsv2 "github.com/giantswarm/cluster-operator/service/controller/aws/v2"
+	awsv3 "github.com/giantswarm/cluster-operator/service/controller/aws/v3"
 	azurev1 "github.com/giantswarm/cluster-operator/service/controller/azure/v1"
 	azurev2 "github.com/giantswarm/cluster-operator/service/controller/azure/v2"
+	azurev3 "github.com/giantswarm/cluster-operator/service/controller/azure/v3"
 	kvmv1 "github.com/giantswarm/cluster-operator/service/controller/kvm/v1"
 	kvmv2 "github.com/giantswarm/cluster-operator/service/controller/kvm/v2"
+	kvmv3 "github.com/giantswarm/cluster-operator/service/controller/kvm/v3"
 )
 
 func NewVersionBundles() []versionbundle.Bundle {
@@ -21,6 +24,10 @@ func NewVersionBundles() []versionbundle.Bundle {
 	versionBundles = append(versionBundles, awsv2.VersionBundle())
 	versionBundles = append(versionBundles, azurev2.VersionBundle())
 	versionBundles = append(versionBundles, kvmv2.VersionBundle())
+
+	versionBundles = append(versionBundles, awsv3.VersionBundle())
+	versionBundles = append(versionBundles, azurev3.VersionBundle())
+	versionBundles = append(versionBundles, kvmv3.VersionBundle())
 
 	return versionBundles
 }
