8cc929d94e502f4e0f0cabd10f6be61c569a2aae
Ross Fairbanks <ross@giantswarm.io>
2018-05-30 15:36:16 +0200

    controller/azure/v4: copy v3

---
+++ service/controller/azure/v4/version_bundle.go
@@ -0,0 +1,27 @@
+package v3
+
+import (
+	"time"
+
+	"github.com/giantswarm/versionbundle"
+)
+
+func VersionBundle() versionbundle.Bundle {
+	return versionbundle.Bundle{
+		Changelogs: []versionbundle.Changelog{
+			{
+				Component:   "cluster-operator",
+				Description: "Added giantswarm namespace.",
+				Kind:        versionbundle.KindAdded,
+			},
+		},
+		Components:   []versionbundle.Component{},
+		Dependencies: []versionbundle.Dependency{},
+		Deprecated:   false,
+		Name:         "cluster-operator",
+		Provider:     "azure",
+		Time:         time.Date(2018, time.April, 26, 12, 00, 0, 0, time.UTC),
+		Version:      "0.3.0",
+		WIP:          true,
+	}
+}
