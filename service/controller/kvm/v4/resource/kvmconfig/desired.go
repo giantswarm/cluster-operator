44c74af9339abaea0a68ed1db5badee080290e31
Tuomas Makinen <tuommaki@gmail.com>
2018-06-07 12:57:16 +0300

    opsctl: copy kvm/v3 to kvm/v4

---
+++ service/controller/kvm/v4/resource/kvmconfig/desired.go
@@ -0,0 +1,11 @@
+package kvmconfig
+
+import (
+	"context"
+)
+
+// GetDesiredState takes observed (during create, delete and update events)
+// custom object as an input and returns computed desired state for it.
+func (r *Resource) GetDesiredState(ctx context.Context, obj interface{}) (interface{}, error) {
+	return nil, nil
+}
---
+++ service/controller/kvm/v4/version_bundle.go
@@ -0,0 +1,21 @@
+package v3
+
+import (
+	"github.com/giantswarm/versionbundle"
+)
+
+func VersionBundle() versionbundle.Bundle {
+	return versionbundle.Bundle{
+		Changelogs: []versionbundle.Changelog{
+			{
+				Component:   "cluster-operator",
+				Description: "Added giantswarm namespace.",
+				Kind:        versionbundle.KindAdded,
+			},
+		},
+		Components: []versionbundle.Component{},
+		Name:       "cluster-operator",
+		Provider:   "kvm",
+		Version:    "0.3.0",
+	}
+}
