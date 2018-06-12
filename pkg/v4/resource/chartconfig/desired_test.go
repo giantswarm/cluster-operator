591793a6118bba73868266ebc91da8af3faa527a
Ross Fairbanks <rossf7@users.noreply.github.com>
2018-06-12 14:39:12 +0200

    Enable Ingress Controller chartconfig for Azure only (#142)
    * Enable Ingress Controller chartconfig for Azure only
    
    * Update version bundle.
    
    * Set ingress component version.

--- pkg/v4/resource/chartconfig/desired_test.go
+++ pkg/v4/resource/chartconfig/desired_test.go
@@ -15,15 +15,49 @@ import (
 
 func Test_ChartConfig_GetDesiredState(t *testing.T) {
 	testCases := []struct {
-		name string
-		obj  interface{}
+		name                     string
+		obj                      interface{}
+		provider                 string
+		expectedChartConfigNames []string
 	}{
 		{
-			name: "empty chart configs",
+			name: "basic match for aws",
 			obj: v1alpha1.ClusterGuestConfig{
 				DNSZone: "5xchu.aws.giantswarm.io",
 				ID:      "5xchu",
 				Owner:   "giantswarm",
+			},
+			provider: label.ProviderAWS,
+			expectedChartConfigNames: []string{
+				"kubernetes-node-exporter-chart",
+				"kubernetes-kube-state-metrics-chart",
+			},
+		},
+		{
+			name: "basic match for kvm",
+			obj: v1alpha1.ClusterGuestConfig{
+				DNSZone: "5xchu.kvm.giantswarm.io",
+				ID:      "5xchu",
+				Owner:   "giantswarm",
+			},
+			provider: label.ProviderKVM,
+			expectedChartConfigNames: []string{
+				"kubernetes-node-exporter-chart",
+				"kubernetes-kube-state-metrics-chart",
+			},
+		},
+		{
+			name: "azure also has ingress controller chartconfig",
+			obj: v1alpha1.ClusterGuestConfig{
+				DNSZone: "5xchu.azure.giantswarm.io",
+				ID:      "5xchu",
+				Owner:   "giantswarm",
+			},
+			provider: label.ProviderAzure,
+			expectedChartConfigNames: []string{
+				"kubernetes-node-exporter-chart",
+				"kubernetes-kube-state-metrics-chart",
+				"kubernetes-nginx-ingress-controller-chart",
 			},
 		},
 	}
@@ -39,7 +73,7 @@ func Test_ChartConfig_GetDesiredState(t *testing.T) {
 				K8sClient:   clientgofake.NewSimpleClientset(),
 				Logger:      microloggertest.New(),
 				ProjectName: "cluster-operator",
-				Provider:    label.ProviderAWS,
+				Provider:    tc.provider,
 				ToClusterGuestConfigFunc: func(v interface{}) (v1alpha1.ClusterGuestConfig, error) {
 					return v.(v1alpha1.ClusterGuestConfig), nil
 				},
@@ -59,8 +93,17 @@ func Test_ChartConfig_GetDesiredState(t *testing.T) {
 				t.Fatal("expected", nil, "got", err)
 			}
 
-			if len(chartConfigs) != 2 {
-				t.Fatal("expected", 2, "got", len(chartConfigs))
+			if len(chartConfigs) != len(tc.expectedChartConfigNames) {
+				t.Fatal("expected", len(tc.expectedChartConfigNames), "got", len(chartConfigs))
+			}
+
+			for _, expectedName := range tc.expectedChartConfigNames {
+				_, err := getChartConfigByName(chartConfigs, expectedName)
+				if IsNotFound(err) {
+					t.Fatalf("expected chart %s not found", expectedName)
+				} else if err != nil {
+					t.Fatalf("expected nil, got %#v", err)
+				}
 			}
 		})
 	}
