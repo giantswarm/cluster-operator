9433606c2cba2871e0764774ed837f2b1d67e453
Ross Fairbanks <ross@giantswarm.io>
2018-06-15 16:30:33 +0200

    Get current chartconfig and set values when updating CRs

--- pkg/v3/resource/chartconfig/update.go
+++ pkg/v3/resource/chartconfig/update.go
@@ -5,8 +5,10 @@ import (
 	"fmt"
 
 	"github.com/giantswarm/apiextensions/pkg/apis/core/v1alpha1"
+	"github.com/giantswarm/apiextensions/pkg/clientset/versioned"
 	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/operatorkit/controller"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func (r *Resource) ApplyUpdateChange(ctx context.Context, obj, updateChange interface{}) error {
@@ -24,7 +26,7 @@ func (r *Resource) ApplyUpdateChange(ctx context.Context, obj, updateChange inte
 		}
 
 		for _, chartConfigToUpdate := range chartConfigsToUpdate {
-			_, err := guestG8sClient.CoreV1alpha1().ChartConfigs(resourceNamespace).Update(chartConfigToUpdate)
+			err := r.updateChartConfig(ctx, chartConfigToUpdate, guestG8sClient)
 			if err != nil {
 				return microerror.Mask(err)
 			}
@@ -95,4 +97,24 @@ func (r *Resource) newUpdateChange(ctx context.Context, obj, currentState, desir
 	r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("found %d chartconfigs which have to be updated", len(chartConfigsToUpdate)))
 
 	return chartConfigsToUpdate, nil
+}
+
+func (r *Resource) updateChartConfig(ctx context.Context, updatedChartConfig *v1alpha1.ChartConfig, guestG8sClient versioned.Interface) error {
+	chartConfigName := updatedChartConfig.ObjectMeta.Name
+
+	chartConfig, err := guestG8sClient.CoreV1alpha1().ChartConfigs(resourceNamespace).Get(chartConfigName, metav1.GetOptions{})
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	// Update current CR with new desired state.
+	chartConfig.ObjectMeta.Labels = updatedChartConfig.ObjectMeta.Labels
+	chartConfig.Spec = updatedChartConfig.Spec
+
+	_, err = guestG8sClient.CoreV1alpha1().ChartConfigs(resourceNamespace).Update(chartConfig)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	return nil
 }
