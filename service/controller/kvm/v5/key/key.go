f7f90942e62d93341885fe1e1d520331adf23593
Ross Fairbanks <rossf7@users.noreply.github.com>
2018-07-11 09:21:49 +0200

    Set worker count for AWS and KVM (#185)
    * Set worker count for AWS and KVM
    
    * Fix test

--- service/controller/kvm/v5/key/key.go
+++ service/controller/kvm/v5/key/key.go
@@ -40,3 +40,7 @@ func ToCustomObject(v interface{}) (v1alpha1.KVMClusterConfig, error) {
 func VersionBundleVersion(kvmClusterConfig v1alpha1.KVMClusterConfig) string {
 	return kvmClusterConfig.Spec.VersionBundle.Version
 }
+
+func WorkerCount(kvmClusterConfig v1alpha1.KVMClusterConfig) int {
+	return len(kvmClusterConfig.Spec.Guest.Workers)
+}
