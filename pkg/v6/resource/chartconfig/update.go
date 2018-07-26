21281ad02544badd476cf885d138c2b4b29439de
Tim Schindler <xh3b4sd@gmail.com>
2018-07-26 14:27:19 +0200

    aligns logging based on recent advances

--- pkg/v6/resource/chartconfig/update.go
+++ pkg/v6/resource/chartconfig/update.go
@@ -33,7 +33,7 @@ func (r *Resource) ApplyUpdateChange(ctx context.Context, obj, updateChange inte
 				// We should not hammer guest API if it is not available, the guest cluster
 				// might be initializing. We will retry on next reconciliation loop.
 				resourcecanceledcontext.SetCanceled(ctx)
-				r.logger.LogCtx(ctx, "level", "debug", "message", "canceling resource reconciliation for custom object")
+				r.logger.LogCtx(ctx, "level", "debug", "message", "canceling resource")
 
 				return nil
 			} else if err != nil {
