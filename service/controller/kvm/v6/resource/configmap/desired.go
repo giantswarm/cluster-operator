95f6b135100234804ff3282f8f64b9dc21113e25
Ross Fairbanks <ross@giantswarm.io>
2018-08-13 12:45:05 +0200

    Config for IC values for KVM

--- service/controller/kvm/v6/resource/configmap/desired.go
+++ service/controller/kvm/v6/resource/configmap/desired.go
@@ -20,7 +20,10 @@ func (r *Resource) GetDesiredState(ctx context.Context, obj interface{}) (interf
 	configMapValues := configmap.ConfigMapValues{
 		ClusterID:    key.ClusterID(clusterGuestConfig),
 		Organization: key.ClusterOrganization(clusterGuestConfig),
-		WorkerCount:  kvmkey.WorkerCount(customObject),
+		// TODO Better comments and issue.
+		IngressControllerMigrationEnabled: true,
+		IngressControllerServiceEnabled:   false,
+		WorkerCount:                       kvmkey.WorkerCount(customObject),
 	}
 	desiredConfigMaps, err := r.configMap.GetDesiredState(ctx, configMapValues)
 	if err != nil {
