0108788b53f52cb17c874b6439547dac6d70e2d9
Ross Fairbanks <ross@giantswarm.io>
2018-08-17 19:28:09 +0200

    Use updated configmap service

--- service/controller/azure/v6/resource/configmap/desired.go
+++ service/controller/azure/v6/resource/configmap/desired.go
@@ -17,6 +17,16 @@ func (r *Resource) GetDesiredState(ctx context.Context, obj interface{}) (interf
 	}
 
 	clusterGuestConfig := azurekey.ClusterGuestConfig(customObject)
+	guestAPIDomain, err := key.APIDomain(clusterGuestConfig)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	configMapConfig := configmap.ConfigMapConfig{
+		ClusterID:      key.ClusterID(clusterGuestConfig),
+		GuestAPIDomain: guestAPIDomain,
+	}
+
 	configMapValues := configmap.ConfigMapValues{
 		ClusterID: key.ClusterID(clusterGuestConfig),
 		// Migration is disabled because Azure is already migrated.
@@ -24,7 +34,7 @@ func (r *Resource) GetDesiredState(ctx context.Context, obj interface{}) (interf
 		Organization:                      key.ClusterOrganization(clusterGuestConfig),
 		WorkerCount:                       azurekey.WorkerCount(customObject),
 	}
-	desiredConfigMaps, err := r.configMap.GetDesiredState(ctx, configMapValues)
+	desiredConfigMaps, err := r.configMap.GetDesiredState(ctx, configMapConfig, configMapValues)
 	if err != nil {
 		return nil, microerror.Mask(err)
 	}
