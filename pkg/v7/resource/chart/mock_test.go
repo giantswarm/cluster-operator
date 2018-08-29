115a8d1a3e16ed49f102fa3a2ffd876e6c679fba
Ross Fairbanks <ross@giantswarm.io>
2018-08-29 16:54:43 +0200

    Add pkg/v7

---
+++ pkg/v7/resource/chart/mock_test.go
@@ -0,0 +1,98 @@
+package chart
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/giantswarm/apiextensions/pkg/clientset/versioned"
+	"github.com/giantswarm/helmclient"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/helm/pkg/helm"
+)
+
+type apprMock struct {
+	defaultReleaseVersion string
+	defaultError          bool
+}
+
+func (a *apprMock) GetReleaseVersion(name, channel string) (string, error) {
+	if a.defaultError {
+		return "", fmt.Errorf("error getting default release")
+	}
+
+	return a.defaultReleaseVersion, nil
+}
+
+func (a *apprMock) PullChartTarball(name, channel string) (string, error) {
+	return "", nil
+}
+
+func (a *apprMock) PullChartTarballFromRelease(name, release string) (string, error) {
+	return "", nil
+}
+
+type guestMock struct {
+	fakeGuestG8sClient  versioned.Interface
+	fakeGuestHelmClient helmclient.Interface
+	fakeGuestK8sClient  kubernetes.Interface
+}
+
+func (g *guestMock) NewG8sClient(ctx context.Context, clusterID, apiDomain string) (versioned.Interface, error) {
+	return g.fakeGuestG8sClient, nil
+}
+func (g *guestMock) NewHelmClient(ctx context.Context, clusterID, apiDomain string) (helmclient.Interface, error) {
+	return g.fakeGuestHelmClient, nil
+}
+func (g *guestMock) NewK8sClient(ctx context.Context, clusterID, apiDomain string) (kubernetes.Interface, error) {
+	return g.fakeGuestK8sClient, nil
+}
+
+type helmMock struct {
+	defaultReleaseContent *helmclient.ReleaseContent
+	defaultReleaseHistory *helmclient.ReleaseHistory
+	defaultError          error
+}
+
+func (h *helmMock) DeleteRelease(releaseName string, options ...helm.DeleteOption) error {
+	if h.defaultError != nil {
+		return h.defaultError
+	}
+
+	return nil
+}
+
+func (h *helmMock) EnsureTillerInstalled() error {
+	return nil
+}
+
+func (h *helmMock) GetReleaseContent(releaseName string) (*helmclient.ReleaseContent, error) {
+	if h.defaultError != nil {
+		return nil, h.defaultError
+	}
+
+	return h.defaultReleaseContent, nil
+}
+
+func (h *helmMock) GetReleaseHistory(releaseName string) (*helmclient.ReleaseHistory, error) {
+	if h.defaultError != nil {
+		return nil, h.defaultError
+	}
+
+	return h.defaultReleaseHistory, nil
+}
+
+func (h *helmMock) InstallFromTarball(path, ns string, options ...helm.InstallOption) error {
+	return nil
+}
+
+func (h *helmMock) PingTiller() error {
+	return nil
+}
+
+func (h *helmMock) RunReleaseTest(releaseName string, options ...helm.ReleaseTestOption) error {
+	return nil
+}
+
+func (h *helmMock) UpdateReleaseFromTarball(releaseName, path string, options ...helm.UpdateOption) error {
+	return nil
+}
---
+++ pkg/v7/resource/chartconfig/resource.go
@@ -0,0 +1,192 @@
+package chartconfig
+
+import (
+	"context"
+	"reflect"
+
+	"github.com/giantswarm/apiextensions/pkg/apis/core/v1alpha1"
+	"github.com/giantswarm/apiextensions/pkg/clientset/versioned"
+	"github.com/giantswarm/guestcluster"
+	"github.com/giantswarm/microerror"
+	"github.com/giantswarm/micrologger"
+	"k8s.io/client-go/kubernetes"
+
+	"github.com/giantswarm/cluster-operator/pkg/cluster"
+	"github.com/giantswarm/cluster-operator/pkg/v6/key"
+)
+
+const (
+	// Name is the identifier of the resource.
+	Name = "chartconfigv6"
+
+	// resourceNamespace is the resource where the chartconfig CRs are created.
+	resourceNamespace = "giantswarm"
+)
+
+// Config represents the configuration used to create a new chart config resource.
+type Config struct {
+	BaseClusterConfig        cluster.Config
+	G8sClient                versioned.Interface
+	Guest                    guestcluster.Interface
+	K8sClient                kubernetes.Interface
+	Logger                   micrologger.Logger
+	ProjectName              string
+	Provider                 string
+	ToClusterGuestConfigFunc func(obj interface{}) (v1alpha1.ClusterGuestConfig, error)
+}
+
+// Resource implements the chart config resource.
+type Resource struct {
+	baseClusterConfig        cluster.Config
+	g8sClient                versioned.Interface
+	guest                    guestcluster.Interface
+	k8sClient                kubernetes.Interface
+	logger                   micrologger.Logger
+	projectName              string
+	provider                 string
+	toClusterGuestConfigFunc func(obj interface{}) (v1alpha1.ClusterGuestConfig, error)
+}
+
+// New creates a new configured chart config resource.
+func New(config Config) (*Resource, error) {
+	if reflect.DeepEqual(config.BaseClusterConfig, cluster.Config{}) {
+		return nil, microerror.Maskf(invalidConfigError, "%T.BaseClusterConfig must not be empty", config)
+	}
+	if config.G8sClient == nil {
+		return nil, microerror.Maskf(invalidConfigError, "%T.G8sClient must not be empty", config)
+	}
+	if config.Guest == nil {
+		return nil, microerror.Maskf(invalidConfigError, "%T.Guest must not be empty", config)
+	}
+	if config.K8sClient == nil {
+		return nil, microerror.Maskf(invalidConfigError, "%T.K8sClient must not be empty", config)
+	}
+	if config.Logger == nil {
+		return nil, microerror.Maskf(invalidConfigError, "%T.Logger must not be empty", config)
+	}
+	if config.ProjectName == "" {
+		return nil, microerror.Maskf(invalidConfigError, "%T.ProjectName must not be empty", config)
+	}
+	if config.Provider == "" {
+		return nil, microerror.Maskf(invalidConfigError, "%T.Provider must not be empty", config)
+	}
+	if config.ToClusterGuestConfigFunc == nil {
+		return nil, microerror.Maskf(invalidConfigError, "%T.ToClusterGuestConfigFunc must not be empty", config)
+	}
+
+	newResource := &Resource{
+		baseClusterConfig:        config.BaseClusterConfig,
+		g8sClient:                config.G8sClient,
+		guest:                    config.Guest,
+		k8sClient:                config.K8sClient,
+		logger:                   config.Logger,
+		projectName:              config.ProjectName,
+		provider:                 config.Provider,
+		toClusterGuestConfigFunc: config.ToClusterGuestConfigFunc,
+	}
+
+	return newResource, nil
+}
+
+// Name returns name of the Resource.
+func (r *Resource) Name() string {
+	return Name
+}
+
+func prepareClusterConfig(baseClusterConfig cluster.Config, clusterGuestConfig v1alpha1.ClusterGuestConfig) (cluster.Config, error) {
+	var err error
+
+	// Use baseClusterConfig as a basis and supplement it with information from
+	// clusterGuestConfig.
+	clusterConfig := baseClusterConfig
+
+	clusterConfig.ClusterID = key.ClusterID(clusterGuestConfig)
+
+	clusterConfig.Domain.API, err = key.APIDomain(clusterGuestConfig)
+	if err != nil {
+		return cluster.Config{}, microerror.Mask(err)
+	}
+
+	clusterConfig.Organization = clusterGuestConfig.Owner
+
+	return clusterConfig, nil
+}
+
+func (r *Resource) getGuestG8sClient(ctx context.Context, obj interface{}) (versioned.Interface, error) {
+	clusterGuestConfig, err := r.toClusterGuestConfigFunc(obj)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	clusterConfig, err := prepareClusterConfig(r.baseClusterConfig, clusterGuestConfig)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	guestAPIDomain, err := key.APIDomain(clusterGuestConfig)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	guestG8sClient, err := r.guest.NewG8sClient(ctx, clusterConfig.ClusterID, guestAPIDomain)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	return guestG8sClient, nil
+}
+
+func (r *Resource) getGuestK8sClient(ctx context.Context, guestConfig cluster.Config) (kubernetes.Interface, error) {
+	guestK8sClient, err := r.guest.NewK8sClient(ctx, guestConfig.ClusterID, guestConfig.Domain.API)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	return guestK8sClient, nil
+}
+
+func containsChartConfig(list []*v1alpha1.ChartConfig, item *v1alpha1.ChartConfig) bool {
+	for _, l := range list {
+		if reflect.DeepEqual(item, l) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func getChartConfigByName(list []*v1alpha1.ChartConfig, name string) (*v1alpha1.ChartConfig, error) {
+	for _, l := range list {
+		if l.Name == name {
+			return l, nil
+		}
+	}
+
+	return nil, microerror.Mask(notFoundError)
+}
+
+func isChartConfigModified(a, b *v1alpha1.ChartConfig) bool {
+	// If the Spec section has changed we need to update.
+	if !reflect.DeepEqual(a.Spec, b.Spec) {
+		return true
+	}
+	// If the Labels have changed we also need to update.
+	if !reflect.DeepEqual(a.Labels, b.Labels) {
+		return true
+	}
+
+	return false
+}
+
+func toChartConfigs(v interface{}) ([]*v1alpha1.ChartConfig, error) {
+	if v == nil {
+		return nil, nil
+	}
+
+	chartConfigs, ok := v.([]*v1alpha1.ChartConfig)
+	if !ok {
+		return nil, microerror.Maskf(wrongTypeError, "expected '%T', got '%T'", []*v1alpha1.ChartConfig{}, v)
+	}
+
+	return chartConfigs, nil
+}
