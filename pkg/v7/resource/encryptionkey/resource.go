259b2bddd39b4fadb1eac4a46513006ceb5dc4e9
Ross Fairbanks <ross@giantswarm.io>
2018-08-29 17:17:15 +0200

    Update pkg/v7 imports

--- pkg/v7/resource/encryptionkey/resource.go
+++ pkg/v7/resource/encryptionkey/resource.go
@@ -11,7 +11,7 @@ import (
 
 const (
 	// Name is the identifier of the resource.
-	Name = "encryptionkeyv6"
+	Name = "encryptionkeyv7"
 )
 
 // Config represents the configuration used to create a new cloud config resource.
