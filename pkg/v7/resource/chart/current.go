d99c8d7f877644a4a5fe0a1939683f5d2f912258
Ross Fairbanks <rossf7@users.noreply.github.com>
2018-08-29 17:11:29 +0200

    v7: copy v6 for AWS (#229)
    * v7: copy v6
    
    * Add pkg/v7

---
+++ pkg/v7/resource/chart/current.go
@@ -0,0 +1,72 @@
+package chart
+
+import (
+	"context"
+
+	"github.com/giantswarm/errors/guest"
+	"github.com/giantswarm/guestcluster"
+	"github.com/giantswarm/helmclient"
+	"github.com/giantswarm/microerror"
+	"github.com/giantswarm/operatorkit/controller/context/reconciliationcanceledcontext"
+)
+
+// GetCurrentState gets the state of the chart in the guest cluster.
+func (r *Resource) GetCurrentState(ctx context.Context, obj interface{}) (interface{}, error) {
+	r.logger.LogCtx(ctx, "level", "debug", "message", "finding chart-operator chart in the guest cluster")
+
+	guestHelmClient, err := r.getGuestHelmClient(ctx, obj)
+	if guestcluster.IsTimeout(err) {
+		r.logger.LogCtx(ctx, "level", "debug", "message", "did not get a Helm client for the guest cluster")
+
+		// We can't continue without a Helm client. We will retry during the
+		// next execution.
+		reconciliationcanceledcontext.SetCanceled(ctx)
+		r.logger.LogCtx(ctx, "level", "debug", "message", "canceling resource")
+
+		return nil, nil
+	} else if guest.IsAPINotAvailable(err) {
+		r.logger.LogCtx(ctx, "level", "debug", "message", "guest cluster is not available")
+
+		// We can't continue without a successful K8s connection. Cluster
+		// may not be up yet. We will retry during the next execution.
+		reconciliationcanceledcontext.SetCanceled(ctx)
+		r.logger.LogCtx(ctx, "level", "debug", "message", "canceling resource")
+
+		return nil, nil
+	} else if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	releaseContent, err := guestHelmClient.GetReleaseContent(chartOperatorRelease)
+	if helmclient.IsReleaseNotFound(err) {
+		r.logger.LogCtx(ctx, "level", "debug", "message", "did not find the chart-operator chart in the guest cluster")
+		return nil, nil
+	} else if guest.IsAPINotAvailable(err) {
+		r.logger.LogCtx(ctx, "level", "debug", "message", "guest cluster is not available")
+
+		// We can't continue without a successful K8s connection. Cluster
+		// may not be up yet. We will retry during the next execution.
+		reconciliationcanceledcontext.SetCanceled(ctx)
+		r.logger.LogCtx(ctx, "level", "debug", "message", "canceling resource")
+
+		return nil, nil
+	} else if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	releaseHistory, err := guestHelmClient.GetReleaseHistory(chartOperatorRelease)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	chartState := &ResourceState{
+		ChartName:      chartOperatorChart,
+		ReleaseName:    chartOperatorRelease,
+		ReleaseStatus:  releaseContent.Status,
+		ReleaseVersion: releaseHistory.Version,
+	}
+
+	r.logger.LogCtx(ctx, "level", "debug", "message", "found chart-operator chart in the guest cluster")
+
+	return chartState, nil
+}
---
+++ pkg/v7/resource/chart/current_test.go
@@ -0,0 +1,158 @@
+package chart
+
+import (
+	"context"
+	"fmt"
+	"reflect"
+	"testing"
+
+	"github.com/giantswarm/apiextensions/pkg/apis/core/v1alpha1"
+	"github.com/giantswarm/apiextensions/pkg/clientset/versioned/fake"
+	"github.com/giantswarm/helmclient"
+	"github.com/giantswarm/micrologger/microloggertest"
+	"github.com/spf13/afero"
+	clientgofake "k8s.io/client-go/kubernetes/fake"
+
+	"github.com/giantswarm/cluster-operator/pkg/cluster"
+)
+
+func Test_Chart_GetCurrentState(t *testing.T) {
+	testCases := []struct {
+		name           string
+		obj            interface{}
+		releaseContent *helmclient.ReleaseContent
+		releaseHistory *helmclient.ReleaseHistory
+		helmError      error
+		expectedState  ResourceState
+		expectedError  bool
+	}{
+		{
+			name: "case 0: basic match",
+			obj: v1alpha1.ClusterGuestConfig{
+				DNSZone: "5xchu.aws.giantswarm.io",
+				ID:      "5xchu",
+				Owner:   "giantswarm",
+			},
+			releaseContent: &helmclient.ReleaseContent{
+				Name:   "chart-operator",
+				Status: "DEPLOYED",
+				Values: map[string]interface{}{
+					"key": "value",
+				},
+			},
+			releaseHistory: &helmclient.ReleaseHistory{
+				Name:    "chart-operator",
+				Version: "0.1.2",
+			},
+			expectedState: ResourceState{
+				ChartName:      "chart-operator-chart",
+				ReleaseName:    "chart-operator",
+				ReleaseStatus:  "DEPLOYED",
+				ReleaseVersion: "0.1.2",
+			},
+		},
+		{
+			name: "case 1: different release status",
+			obj: v1alpha1.ClusterGuestConfig{
+				DNSZone: "5xchu.aws.giantswarm.io",
+				ID:      "5xchu",
+				Owner:   "giantswarm",
+			},
+			releaseContent: &helmclient.ReleaseContent{
+				Name:   "chart-operator",
+				Status: "FAILED",
+				Values: map[string]interface{}{
+					"key": "value",
+				},
+			},
+			releaseHistory: &helmclient.ReleaseHistory{
+				Name:    "chart-operator",
+				Version: "0.1.2",
+			},
+			expectedState: ResourceState{
+				ChartName:      "chart-operator-chart",
+				ReleaseName:    "chart-operator",
+				ReleaseStatus:  "FAILED",
+				ReleaseVersion: "0.1.2",
+			},
+		},
+		{
+			name: "case 2: state is empty when release does not exist",
+			obj: v1alpha1.ClusterGuestConfig{
+				DNSZone: "5xchu.aws.giantswarm.io",
+				ID:      "5xchu",
+				Owner:   "giantswarm",
+			},
+			releaseContent: &helmclient.ReleaseContent{},
+			releaseHistory: &helmclient.ReleaseHistory{},
+			helmError:      fmt.Errorf("No such release: chart-operator"),
+			expectedState:  ResourceState{},
+			expectedError:  false,
+		},
+		{
+			name: "case 3: unexpected error",
+			obj: v1alpha1.ClusterGuestConfig{
+				DNSZone: "5xchu.aws.giantswarm.io",
+				ID:      "5xchu",
+				Owner:   "giantswarm",
+			},
+			releaseContent: &helmclient.ReleaseContent{},
+			releaseHistory: &helmclient.ReleaseHistory{},
+			helmError:      fmt.Errorf("Unexpected error"),
+			expectedError:  true,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			helmClient := &helmMock{
+				defaultReleaseContent: tc.releaseContent,
+				defaultReleaseHistory: tc.releaseHistory,
+				defaultError:          tc.helmError,
+			}
+
+			c := Config{
+				ApprClient: &apprMock{},
+				BaseClusterConfig: cluster.Config{
+					ClusterID: "test-cluster",
+				},
+				Fs:        afero.NewMemMapFs(),
+				G8sClient: fake.NewSimpleClientset(),
+				Guest: &guestMock{
+					fakeGuestHelmClient: helmClient,
+				},
+				K8sClient:      clientgofake.NewSimpleClientset(),
+				Logger:         microloggertest.New(),
+				ProjectName:    "cluster-operator",
+				RegistryDomain: "quay.io",
+				ToClusterGuestConfigFunc: func(v interface{}) (v1alpha1.ClusterGuestConfig, error) {
+					return v.(v1alpha1.ClusterGuestConfig), nil
+				},
+			}
+
+			r, err := New(c)
+			if err != nil {
+				t.Fatalf("error == %#v, want nil", err)
+			}
+
+			result, err := r.GetCurrentState(context.TODO(), tc.obj)
+			switch {
+			case err != nil && !tc.expectedError:
+				t.Fatalf("error == %#v, want nil", err)
+			case err == nil && tc.expectedError:
+				t.Fatalf("error == nil, want non-nil")
+			}
+
+			if result != nil && !reflect.DeepEqual(tc.expectedState, ResourceState{}) {
+				chartState, err := toResourceState(result)
+				if err != nil {
+					t.Fatalf("error == %#v, want nil", err)
+				}
+
+				if !reflect.DeepEqual(chartState, tc.expectedState) {
+					t.Fatalf("ChartState == %q, want %q", chartState, tc.expectedState)
+				}
+			}
+		})
+	}
+}
