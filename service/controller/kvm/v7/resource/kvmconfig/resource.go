0cfcdafeb3f555bf2d93919b649289f6163111c5
Ross Fairbanks <ross@giantswarm.io>
2018-09-18 13:41:36 +0200

    Fix resource versions

--- service/controller/kvm/v7/resource/kvmconfig/resource.go
+++ service/controller/kvm/v7/resource/kvmconfig/resource.go
@@ -9,7 +9,7 @@ import (
 
 const (
 	// Name is the identifier of the resource.
-	Name = "kvmconfigv2"
+	Name = "kvmconfigv7"
 )
 
 // Config represents the configuration used to create a new cloud config resource.
