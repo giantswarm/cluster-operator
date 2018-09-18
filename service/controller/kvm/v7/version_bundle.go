3c8eddac0d05303ffcaa4048ca7994bd72bb78a7
Ross Fairbanks <ross@giantswarm.io>
2018-09-18 12:55:31 +0200

    v7: copy v6

---
+++ service/controller/kvm/v7/version_bundle.go
@@ -0,0 +1,54 @@
+package v6
+
+import (
+	"github.com/giantswarm/versionbundle"
+)
+
+func VersionBundle() versionbundle.Bundle {
+	return versionbundle.Bundle{
+		Changelogs: []versionbundle.Changelog{
+			{
+				Component:   "chart-operator",
+				Description: "Added monitoring for managed charts.",
+				Kind:        versionbundle.KindAdded,
+			},
+			{
+				Component:   "cluster-operator",
+				Description: "Added support for deploying cert-exporter.",
+				Kind:        versionbundle.KindAdded,
+			},
+			{
+				Component:   "cluster-operator",
+				Description: "Added support for deploying net-exporter.",
+				Kind:        versionbundle.KindAdded,
+			},
+			{
+				Component:   "cluster-operator",
+				Description: "Added support for deploying nginx-ingress-controller.",
+				Kind:        versionbundle.KindAdded,
+			},
+			{
+				Component:   "nginx-ingress-controller",
+				Description: "Updated to 0.15.0.",
+				Kind:        versionbundle.KindChanged,
+			},
+		},
+		Components: []versionbundle.Component{
+			{
+				Name:    "kube-state-metrics",
+				Version: "1.3.1",
+			},
+			{
+				Name:    "nginx-ingress-controller",
+				Version: "0.15.0",
+			},
+			{
+				Name:    "node-exporter",
+				Version: "0.15.1",
+			},
+		},
+		Name:     "cluster-operator",
+		Provider: "kvm",
+		Version:  "0.6.0",
+	}
+}
