60e661afca2b8d02258236b29ae359658e177cd6
Ross Fairbanks <rossf7@users.noreply.github.com>
2018-09-18 13:57:44 +0200

    V7 imports for KVM (#241)
    * controller/v7: update imports and packages
    
    * Fix resource versions

--- service/controller/kvm/v4/resource/kvmconfig/resource.go
+++ service/controller/kvm/v4/resource/kvmconfig/resource.go
@@ -9,7 +9,7 @@ import (
 
 const (
 	// Name is the identifier of the resource.
-	Name = "kvmconfigv2"
+	Name = "kvmconfigv4"
 )
 
 // Config represents the configuration used to create a new cloud config resource.
