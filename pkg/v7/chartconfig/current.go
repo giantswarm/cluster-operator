6a0359f073e2f20536bc4c1c56a321dfe23d46a2
Ross Fairbanks <ross@giantswarm.io>
2018-09-20 17:32:43 +0200

    Move cancel logic to resources

--- pkg/v7/chartconfig/current.go
+++ pkg/v7/chartconfig/current.go
@@ -5,11 +5,7 @@ import (
 	"fmt"
 
 	"github.com/giantswarm/apiextensions/pkg/apis/core/v1alpha1"
-	"github.com/giantswarm/errors/guest"
 	"github.com/giantswarm/microerror"
-	"github.com/giantswarm/operatorkit/controller/context/resourcecanceledcontext"
-	"github.com/giantswarm/tenantcluster"
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/giantswarm/cluster-operator/pkg/label"
@@ -19,17 +15,7 @@ func (c *ChartConfig) GetCurrentState(ctx context.Context, clusterConfig Cluster
 	c.logger.LogCtx(ctx, "level", "debug", "message", "looking for chartconfigs in the tenant cluster")
 
 	tenantG8sClient, err := c.newTenantG8sClient(ctx, clusterConfig)
-	if tenantcluster.IsTimeout(err) {
-		c.logger.LogCtx(ctx, "level", "debug", "message", "did not find the cluster-operator api cert in the Kubernetes API")
-
-		// We can't continue without the cert. We will retry during the next
-		// execution.
-		resourcecanceledcontext.SetCanceled(ctx)
-		c.logger.LogCtx(ctx, "level", "debug", "message", "canceling resource")
-
-		return nil, nil
-
-	} else if err != nil {
+	if err != nil {
 		return nil, microerror.Mask(err)
 	}
 
@@ -38,26 +24,7 @@ func (c *ChartConfig) GetCurrentState(ctx context.Context, clusterConfig Cluster
 	}
 
 	chartConfigList, err := tenantG8sClient.CoreV1alpha1().ChartConfigs(resourceNamespace).List(listOptions)
-	if apierrors.IsNotFound(err) {
-		c.logger.LogCtx(ctx, "level", "debug", "message", "did not find the chartconfig CRD in the tenant cluster")
-
-		// ChartConfig CRD is created by chart-operator which may not be
-		// running yet in the tenant cluster. We will retry during the next
-		// execution.
-		resourcecanceledcontext.SetCanceled(ctx)
-		c.logger.LogCtx(ctx, "level", "debug", "message", "canceling resource")
-
-		return nil, nil
-	} else if guest.IsAPINotAvailable(err) {
-		c.logger.LogCtx(ctx, "level", "debug", "message", "tenant cluster is not available")
-
-		// We can't continue without a successful K8s connection. Cluster
-		// may not be up yet. We will retry during the next execution.
-		resourcecanceledcontext.SetCanceled(ctx)
-		c.logger.LogCtx(ctx, "level", "debug", "message", "canceling resource")
-
-		return nil, nil
-	} else if err != nil {
+	if err != nil {
 		return nil, microerror.Mask(err)
 	}
 
--- service/controller/kvm/v7/resource/chartconfig/current.go
+++ service/controller/kvm/v7/resource/chartconfig/current.go
@@ -5,7 +5,8 @@ import (
 
 	"github.com/giantswarm/errors/guest"
 	"github.com/giantswarm/microerror"
-	"github.com/giantswarm/operatorkit/controller/context/reconciliationcanceledcontext"
+	"github.com/giantswarm/operatorkit/controller/context/resourcecanceledcontext"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 
 	"github.com/giantswarm/cluster-operator/pkg/v7/chartconfig"
 	"github.com/giantswarm/cluster-operator/pkg/v7/key"
@@ -29,13 +30,23 @@ func (r *Resource) GetCurrentState(ctx context.Context, obj interface{}) (interf
 		ClusterID: key.ClusterID(clusterGuestConfig),
 	}
 	chartConfigs, err := r.chartConfig.GetCurrentState(ctx, clusterConfig)
-	if guest.IsAPINotAvailable(err) {
+	if apierrors.IsNotFound(err) {
+		r.logger.LogCtx(ctx, "level", "debug", "message", "did not find the chartconfig CRD in the tenant cluster")
+
+		// ChartConfig CRD is created by chart-operator which may not be
+		// running yet in the tenant cluster. We will retry during the next
+		// execution.
+		resourcecanceledcontext.SetCanceled(ctx)
+		r.logger.LogCtx(ctx, "level", "debug", "message", "canceling resource")
+
+		return nil, nil
+	} else if guest.IsAPINotAvailable(err) {
 		r.logger.LogCtx(ctx, "level", "debug", "message", "tenant cluster is not available")
 
 		// We can't continue without a successful K8s connection. Cluster
 		// may not be up yet. We will retry during the next execution.
-		reconciliationcanceledcontext.SetCanceled(ctx)
-		r.logger.LogCtx(ctx, "level", "debug", "message", "canceling reconciliation")
+		resourcecanceledcontext.SetCanceled(ctx)
+		r.logger.LogCtx(ctx, "level", "debug", "message", "canceling resource")
 
 		return nil, nil
 	} else if err != nil {
