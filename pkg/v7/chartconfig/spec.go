463904ec0c3b3fe5201c93fda5290e3d2eaab928
Ross Fairbanks <ross@giantswarm.io>
2018-09-21 10:12:53 +0200

    Add update support to chartconfig service

--- pkg/v7/chartconfig/spec.go
+++ pkg/v7/chartconfig/spec.go
@@ -4,13 +4,16 @@ import (
 	"context"
 
 	"github.com/giantswarm/apiextensions/pkg/apis/core/v1alpha1"
+	"github.com/giantswarm/operatorkit/controller"
 
 	"github.com/giantswarm/cluster-operator/pkg/v7/key"
 )
 
 type Interface interface {
 	ApplyCreateChange(ctx context.Context, clusterConfig ClusterConfig, chartConfigsToCreate []*v1alpha1.ChartConfig) error
+	ApplyUpdateChange(ctx context.Context, clusterConfig ClusterConfig, chartConfigsToUpdate []*v1alpha1.ChartConfig) error
 	GetDesiredState(ctx context.Context, clusterConfig ClusterConfig, providerChartSpecs []key.ChartSpec) ([]*v1alpha1.ChartConfig, error)
+	NewUpdatePatch(ctx context.Context, currentState, desiredState []*v1alpha1.ChartConfig) (*controller.Patch, error)
 }
 
 // ClusterConfig is used by the chartconfig service to provide config to
