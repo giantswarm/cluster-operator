409e4ee8f571600fc6d6e0ade14aacf2eb3cda29
Ross Fairbanks <ross@giantswarm.io>
2018-09-22 00:20:01 +0200

    Updated configmap resource wiring for AWS

--- service/controller/aws/v7/resource/configmap/desired.go
+++ service/controller/aws/v7/resource/configmap/desired.go
@@ -28,16 +28,20 @@ func (r *Resource) GetDesiredState(ctx context.Context, obj interface{}) (interf
 	}
 
 	configMapValues := configmap.ConfigMapValues{
-		ClusterID:    key.ClusterID(clusterGuestConfig),
-		Organization: key.ClusterOrganization(clusterGuestConfig),
+		CalicoAddress:      r.calicoAddress,
+		CalicoPrefixLength: r.calicoPrefixLength,
+		ClusterID:          key.ClusterID(clusterGuestConfig),
+		ClusterIPRange:     r.clusterIPRange,
 		// Migration is enabled so existing k8scloudconfig resources are
 		// replaced.
 		IngressControllerMigrationEnabled: true,
 		// Proxy protocol is enabled for AWS clusters.
 		IngressControllerUseProxyProtocol: true,
+		Organization:                      key.ClusterOrganization(clusterGuestConfig),
+		RegistryDomain:                    r.registryDomain,
 		WorkerCount:                       awskey.WorkerCount(customObject),
 	}
-	desiredConfigMaps, err := r.configMap.GetDesiredState(ctx, clusterConfig, configMapValues)
+	desiredConfigMaps, err := r.configMap.GetDesiredState(ctx, clusterConfig, configMapValues, awskey.ChartSpecs())
 	if err != nil {
 		return nil, microerror.Mask(err)
 	}
