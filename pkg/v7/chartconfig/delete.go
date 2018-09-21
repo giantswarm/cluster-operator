53da5dc5dcb5a933d0349e5e38d49c3f5019f193
Ross Fairbanks <ross@giantswarm.io>
2018-09-21 11:13:58 +0200

    Add structure for delete support

---
+++ pkg/v7/chartconfig/delete.go
@@ -0,0 +1,16 @@
+package chartconfig
+
+import (
+	"context"
+
+	"github.com/giantswarm/apiextensions/pkg/apis/core/v1alpha1"
+	"github.com/giantswarm/operatorkit/controller"
+)
+
+func (c *ChartConfig) ApplyDeleteChange(ctx context.Context, clusterConfig ClusterConfig, chartConfigsToDelete []*v1alpha1.ChartConfig) error {
+	return nil
+}
+
+func (c *ChartConfig) NewUpdatePatch(ctx context.Context, currentState, desiredState []*v1alpha1.ChartConfig) (*controller.Patch, error) {
+	return nil, nil
+}
--- pkg/v7/chartconfig/spec.go
+++ pkg/v7/chartconfig/spec.go
@@ -4,12 +4,15 @@ import (
 	"context"
 
 	"github.com/giantswarm/apiextensions/pkg/apis/core/v1alpha1"
+	"github.com/giantswarm/operatorkit/controller"
 
 	"github.com/giantswarm/cluster-operator/pkg/v7/key"
 )
 
 type Interface interface {
+	ApplyDeleteChange(ctx context.Context, clusterConfig ClusterConfig, chartConfigsToDelete []*v1alpha1.ChartConfig) error
 	GetDesiredState(ctx context.Context, clusterConfig ClusterConfig, providerChartSpecs []key.ChartSpec) ([]*v1alpha1.ChartConfig, error)
+	NewDeletePatch(ctx context.Context, currentState, desiredState []*v1alpha1.ChartConfig) (*controller.Patch, error)
 }
 
 // ClusterConfig is used by the chartconfig service to provide config to
