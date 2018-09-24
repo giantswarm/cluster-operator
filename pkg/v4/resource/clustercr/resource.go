60129562f3cade3cfecef2fe2fe6f455763cc527
Tim Schindler <xh3b4sd@gmail.com>
2018-09-24 15:52:28 +0200

    fixes versions in resource names

--- pkg/v4/resource/clustercr/resource.go
+++ pkg/v4/resource/clustercr/resource.go
@@ -8,7 +8,7 @@ import (
 
 const (
 	// Name is the identifier of the resource.
-	Name = "clustercrv7"
+	Name = "clustercrv4"
 )
 
 // Config represents the configuration used to create a new namespace resource.
