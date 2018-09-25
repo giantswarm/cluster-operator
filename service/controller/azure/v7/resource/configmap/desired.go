db5217abb1e0843483c869f35862c1dac6b3b0de
Ross Fairbanks <ross@giantswarm.io>
2018-09-25 12:43:00 +0200

    Fix merge problem

--- service/controller/azure/v7/resource/configmap/desired.go
+++ service/controller/azure/v7/resource/configmap/desired.go
@@ -28,7 +28,6 @@ func (r *Resource) GetDesiredState(ctx context.Context, obj interface{}) (interf
 	}
 
 	configMapValues := configmap.ConfigMapValues{
-<<<<<<< HEAD
 		ClusterID:      key.ClusterID(clusterGuestConfig),
 		Organization:   key.ClusterOrganization(clusterGuestConfig),
 		RegistryDomain: r.registryDomain,
@@ -45,19 +44,6 @@ func (r *Resource) GetDesiredState(ctx context.Context, obj interface{}) (interf
 			// Proxy protocol is disabled for Azure clusters.
 			UseProxyProtocol: false,
 		},
-=======
-		CalicoAddress:      r.calicoAddress,
-		CalicoPrefixLength: r.calicoPrefixLength,
-		ClusterID:          key.ClusterID(clusterGuestConfig),
-		ClusterIPRange:     r.clusterIPRange,
-		// Migration is disabled because Azure is already migrated.
-		IngressControllerMigrationEnabled: false,
-		// Proxy protocol is disabled for Azure clusters.
-		IngressControllerUseProxyProtocol: false,
-		Organization:                      key.ClusterOrganization(clusterGuestConfig),
-		RegistryDomain:                    r.registryDomain,
-		WorkerCount:                       azurekey.WorkerCount(customObject),
->>>>>>> master
 	}
 	desiredConfigMaps, err := r.configMap.GetDesiredState(ctx, clusterConfig, configMapValues, azurekey.ChartSpecs())
 	if err != nil {
