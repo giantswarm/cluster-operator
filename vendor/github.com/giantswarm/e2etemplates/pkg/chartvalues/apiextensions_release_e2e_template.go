c31f4a4a69949071a0f93ad1752d37a09feb7ec8
Paweł Kopiczko <kopiczko@gmail.com>
2018-10-01 11:27:20 +0100

    unvendor 2

--- vendor/github.com/giantswarm/e2etemplates/pkg/chartvalues/apiextensions_release_e2e_template.go
+++
@@ -1,9 +0,0 @@
-package chartvalues
-
-const apiExtensionsReleaseE2ETemplate = `namespace: {{ .Namespace }}
-operator:
-  name: {{ .Operator.Name }}
-  version: {{ .Operator.Version }}
-versionBundle:
-  version: {{ .VersionBundle.Version }}
-`
