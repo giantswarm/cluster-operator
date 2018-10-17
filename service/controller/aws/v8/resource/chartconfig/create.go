e39619d166e608ed4f3e60f38539d3ae3095ec37
Theo Brigitte <theo.brigitte@gmail.com>
2018-10-18 01:11:23 +0200

    fix v8 wiring

--- service/controller/aws/v8/resource/chartconfig/create.go
+++ service/controller/aws/v8/resource/chartconfig/create.go
@@ -7,8 +7,8 @@ import (
 	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/operatorkit/controller/context/reconciliationcanceledcontext"
 
-	"github.com/giantswarm/cluster-operator/pkg/v7/chartconfig"
-	"github.com/giantswarm/cluster-operator/pkg/v7/key"
+	"github.com/giantswarm/cluster-operator/pkg/v8/chartconfig"
+	"github.com/giantswarm/cluster-operator/pkg/v8/key"
 	awskey "github.com/giantswarm/cluster-operator/service/controller/aws/v8/key"
 )
 
