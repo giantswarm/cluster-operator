1f7900d046c19defe6ba4b2c6f9e67c6916fa6f0
Theo Brigitte <theo.brigitte@gmail.com>
2018-10-31 17:35:54 +0100

    rename resource in service/controller/azure/v7patch1: v7 > v7patch1

--- service/controller/azure/v7patch1/resource/azureconfig/resource.go
+++ service/controller/azure/v7patch1/resource/azureconfig/resource.go
@@ -9,7 +9,7 @@ import (
 
 const (
 	// Name is the identifier of the resource.
-	Name = "azureconfigv7"
+	Name = "azureconfigv7patch1"
 )
 
 // Config represents the configuration used to create a new cloud config resource.
