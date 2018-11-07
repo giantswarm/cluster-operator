3aebfa92f38be2a162f79138c8925db8211b8bfa
Paweł Kopiczko <kopiczko@gmail.com>
2018-11-07 16:11:43 +0000

    pkg, vendor: update certs (#324)

--- pkg/v8/resource/certconfig/desired_test.go
+++ pkg/v8/resource/certconfig/desired_test.go
@@ -8,13 +8,12 @@ import (
 	"github.com/giantswarm/apiextensions/pkg/apis/core/v1alpha1"
 	"github.com/giantswarm/apiextensions/pkg/clientset/versioned/fake"
 	"github.com/giantswarm/certs"
+	"github.com/giantswarm/cluster-operator/pkg/cluster"
+	"github.com/giantswarm/cluster-operator/pkg/v8/key"
 	"github.com/giantswarm/micrologger/microloggertest"
 	"k8s.io/api/core/v1"
 	apismetav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	clientgofake "k8s.io/client-go/kubernetes/fake"
-
-	"github.com/giantswarm/cluster-operator/pkg/cluster"
-	"github.com/giantswarm/cluster-operator/pkg/v8/key"
 )
 
 func Test_GetDesiredState_Returns_CertConfig_For_All_Managed_Certs(t *testing.T) {
@@ -29,7 +28,6 @@ func Test_GetDesiredState_Returns_CertConfig_For_All_Managed_Certs(t *testing.T)
 			provider: "aws",
 			managedCertificates: []certs.Cert{
 				certs.APICert,
-				certs.CalicoCert,
 				certs.CalicoEtcdClientCert,
 				certs.ClusterOperatorAPICert,
 				certs.EtcdCert,
@@ -45,7 +43,6 @@ func Test_GetDesiredState_Returns_CertConfig_For_All_Managed_Certs(t *testing.T)
 			provider: "azure",
 			managedCertificates: []certs.Cert{
 				certs.APICert,
-				certs.CalicoCert,
 				certs.CalicoEtcdClientCert,
 				certs.ClusterOperatorAPICert,
 				certs.EtcdCert,
@@ -61,7 +58,6 @@ func Test_GetDesiredState_Returns_CertConfig_For_All_Managed_Certs(t *testing.T)
 			provider: "kvm",
 			managedCertificates: []certs.Cert{
 				certs.APICert,
-				certs.CalicoCert,
 				certs.CalicoEtcdClientCert,
 				certs.ClusterOperatorAPICert,
 				certs.EtcdCert,
