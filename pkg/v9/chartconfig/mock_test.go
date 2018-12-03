857235523e7f709d533e9625f978e75b7b7e5077
Théo Brigitte <theo.brigitte@gmail.com>
2018-12-03 12:49:59 +0100

    copy v8 to v9 (#337)

---
+++ pkg/v9/chartconfig/mock_test.go
@@ -0,0 +1,80 @@
+package chartconfig
+
+import (
+	"context"
+
+	"github.com/giantswarm/apiextensions/pkg/clientset/versioned"
+	"github.com/giantswarm/helmclient"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/helm/pkg/helm"
+)
+
+type tenantMock struct {
+	fakeTenantG8sClient  versioned.Interface
+	fakeTenantHelmClient helmclient.Interface
+	fakeTenantK8sClient  kubernetes.Interface
+}
+
+func (t *tenantMock) NewG8sClient(ctx context.Context, clusterID, apiDomain string) (versioned.Interface, error) {
+	return t.fakeTenantG8sClient, nil
+}
+func (t *tenantMock) NewHelmClient(ctx context.Context, clusterID, apiDomain string) (helmclient.Interface, error) {
+	return t.fakeTenantHelmClient, nil
+}
+func (t *tenantMock) NewK8sClient(ctx context.Context, clusterID, apiDomain string) (kubernetes.Interface, error) {
+	return t.fakeTenantK8sClient, nil
+}
+
+type helmMock struct {
+	defaultReleaseContent *helmclient.ReleaseContent
+	defaultReleaseHistory *helmclient.ReleaseHistory
+	defaultError          error
+}
+
+func (h *helmMock) DeleteRelease(releaseName string, options ...helm.DeleteOption) error {
+	if h.defaultError != nil {
+		return h.defaultError
+	}
+
+	return nil
+}
+
+func (h *helmMock) EnsureTillerInstalled() error {
+	return nil
+}
+
+func (h *helmMock) GetReleaseContent(releaseName string) (*helmclient.ReleaseContent, error) {
+	if h.defaultError != nil {
+		return nil, h.defaultError
+	}
+
+	return h.defaultReleaseContent, nil
+}
+
+func (h *helmMock) GetReleaseHistory(releaseName string) (*helmclient.ReleaseHistory, error) {
+	if h.defaultError != nil {
+		return nil, h.defaultError
+	}
+
+	return h.defaultReleaseHistory, nil
+}
+
+func (h *helmMock) InstallReleaseFromTarball(path, ns string, options ...helm.InstallOption) error {
+	return nil
+}
+
+func (h *helmMock) ListReleaseContents(ctx context.Context) ([]*helmclient.ReleaseContent, error) {
+	return nil, nil
+}
+
+func (h *helmMock) PingTiller() error {
+	return nil
+}
+
+func (h *helmMock) RunReleaseTest(releaseName string, options ...helm.ReleaseTestOption) error {
+	return nil
+}
+
+func (h *helmMock) UpdateReleaseFromTarball(releaseName, path string, options ...helm.UpdateOption) error {
+	return nil
+}
---
+++ service/controller/kvm/v9/version_bundle.go
@@ -0,0 +1,38 @@
+package v8
+
+import (
+	"github.com/giantswarm/versionbundle"
+)
+
+func VersionBundle() versionbundle.Bundle {
+	return versionbundle.Bundle{
+		Changelogs: []versionbundle.Changelog{
+			{
+				Component:   "coredns",
+				Description: "Allow advanced CoreDNS configuration. See https://docs.giantswarm.io/guides/advanced-coredns-configuration.",
+				Kind:        versionbundle.KindAdded,
+			},
+		},
+		Components: []versionbundle.Component{
+			{
+				Name:    "coredns",
+				Version: "1.1.1",
+			},
+			{
+				Name:    "kube-state-metrics",
+				Version: "1.3.1",
+			},
+			{
+				Name:    "nginx-ingress-controller",
+				Version: "0.15.0",
+			},
+			{
+				Name:    "node-exporter",
+				Version: "0.15.1",
+			},
+		},
+		Name:     "cluster-operator",
+		Provider: "kvm",
+		Version:  "0.8.0",
+	}
+}
