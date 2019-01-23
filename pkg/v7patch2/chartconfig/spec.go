6e2d62ede74efa633d9adc5a3b9953c516cdcebe
Fernando Ripoll <pipo02mix@gmail.com>
2019-01-23 11:59:20 +0100

    copy v7patch1 to v7patch2

---
+++ pkg/v7patch2/chartconfig/spec.go
@@ -0,0 +1,28 @@
+package chartconfig
+
+import (
+	"context"
+
+	"github.com/giantswarm/apiextensions/pkg/apis/core/v1alpha1"
+	"github.com/giantswarm/operatorkit/controller"
+
+	"github.com/giantswarm/cluster-operator/pkg/v7/key"
+)
+
+type Interface interface {
+	ApplyCreateChange(ctx context.Context, clusterConfig ClusterConfig, chartConfigsToCreate []*v1alpha1.ChartConfig) error
+	ApplyDeleteChange(ctx context.Context, clusterConfig ClusterConfig, chartConfigsToDelete []*v1alpha1.ChartConfig) error
+	ApplyUpdateChange(ctx context.Context, clusterConfig ClusterConfig, chartConfigsToUpdate []*v1alpha1.ChartConfig) error
+	GetCurrentState(ctx context.Context, clusterConfig ClusterConfig) ([]*v1alpha1.ChartConfig, error)
+	GetDesiredState(ctx context.Context, clusterConfig ClusterConfig, providerChartSpecs []key.ChartSpec) ([]*v1alpha1.ChartConfig, error)
+	NewUpdatePatch(ctx context.Context, currentState, desiredState []*v1alpha1.ChartConfig) (*controller.Patch, error)
+	NewDeletePatch(ctx context.Context, currentState, desiredState []*v1alpha1.ChartConfig) (*controller.Patch, error)
+}
+
+// ClusterConfig is used by the chartconfig service to provide config to
+// connect to the tenant cluster.
+type ClusterConfig struct {
+	APIDomain    string
+	ClusterID    string
+	Organization string
+}
---
+++ pkg/v7patch2/resource/chart/create.go
@@ -0,0 +1,125 @@
+package chart
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"reflect"
+
+	"github.com/giantswarm/cluster-operator/pkg/v7/key"
+	"github.com/giantswarm/errors/guest"
+	"github.com/giantswarm/helmclient"
+	"github.com/giantswarm/microerror"
+	"github.com/giantswarm/operatorkit/controller/context/resourcecanceledcontext"
+	"github.com/giantswarm/tenantcluster"
+	"k8s.io/helm/pkg/helm"
+)
+
+func (r *Resource) ApplyCreateChange(ctx context.Context, obj, createChange interface{}) error {
+	createState, err := toResourceState(createChange)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	tenantHelmClient, err := r.getTenantHelmClient(ctx, obj)
+	if tenantcluster.IsTimeout(err) {
+		r.logger.LogCtx(ctx, "level", "debug", "message", "did not get a Helm client for the guest cluster")
+
+		// A not found error here means that the cluster-operator certificate for
+		// the current guest cluster was not found. We can't continue without a Helm
+		// client. We will retry during the next execution, when the certificate
+		// might be available.
+		resourcecanceledcontext.SetCanceled(ctx)
+		r.logger.LogCtx(ctx, "level", "debug", "message", "canceling resource")
+
+		return nil
+	} else if helmclient.IsTillerInstallationFailed(err) {
+		r.logger.LogCtx(ctx, "level", "debug", "message", "Tiller installation failed")
+
+		// Tiller installation can fail during guest cluster setup. We will retry
+		// on next reconciliation loop.
+		resourcecanceledcontext.SetCanceled(ctx)
+		r.logger.LogCtx(ctx, "level", "debug", "message", "canceling resource")
+
+		return nil
+	} else if guest.IsAPINotAvailable(err) {
+		r.logger.LogCtx(ctx, "level", "debug", "message", "Guest API not available.")
+
+		// We should not hammer guest API if it is not available, the guest cluster
+		// might be initializing. We will retry on next reconciliation loop.
+		resourcecanceledcontext.SetCanceled(ctx)
+		r.logger.LogCtx(ctx, "level", "debug", "message", "canceling resource")
+
+		return nil
+	} else if err != nil {
+		return microerror.Mask(err)
+	}
+
+	if !reflect.DeepEqual(createState, ResourceState{}) {
+		r.logger.LogCtx(ctx, "level", "debug", "message", "creating chart-operator chart")
+
+		tarballPath, err := r.apprClient.PullChartTarball(ctx, createState.ChartName, chartOperatorChannel)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+		defer func() {
+			err := r.fs.Remove(tarballPath)
+			if err != nil {
+				r.logger.LogCtx(ctx, "level", "error", "message", fmt.Sprintf("deletion of %q failed", tarballPath), "stack", fmt.Sprintf("%#v", err))
+			}
+		}()
+
+		clusterDNSIP, err := key.DNSIP(r.clusterIPRange)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+		v := &Values{
+			ClusterDNSIP: clusterDNSIP,
+			Image: Image{
+				Registry: r.registryDomain,
+			},
+		}
+		b, err := json.Marshal(v)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+		err = tenantHelmClient.InstallReleaseFromTarball(ctx, tarballPath, chartOperatorNamespace,
+			helm.ReleaseName(createState.ReleaseName),
+			helm.ValueOverrides(b),
+			helm.InstallWait(true))
+		if err != nil {
+			return microerror.Mask(err)
+		}
+		r.logger.LogCtx(ctx, "level", "debug", "message", "created chart-operator chart")
+	} else {
+		r.logger.LogCtx(ctx, "level", "debug", "message", "not creating chart-operator chart")
+	}
+
+	return nil
+}
+
+func (r *Resource) newCreateChange(ctx context.Context, obj, currentState, desiredState interface{}) (interface{}, error) {
+	currentResourceState, err := toResourceState(currentState)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+	desiredResourceState, err := toResourceState(desiredState)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	r.logger.LogCtx(ctx, "level", "debug", "message", "finding out if chart-operator chart has to be created")
+
+	createState := &ResourceState{}
+
+	// chart-operator should be created if it is not present.
+	if reflect.DeepEqual(currentResourceState, ResourceState{}) {
+		r.logger.LogCtx(ctx, "level", "debug", "message", "chart-operator chart needs to be created")
+
+		createState = &desiredResourceState
+	} else {
+		r.logger.LogCtx(ctx, "level", "debug", "message", "chart-operator chart does not need to be created")
+	}
+
+	return createState, nil
+}
