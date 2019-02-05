7c419a50359a25e1a33299365479b61c236c50ad
Ross Fairbanks <ross@giantswarm.io>
2019-02-05 13:06:34 +0000

    Remove unused mocks

--- pkg/v10/configmap/mock_test.go
+++ pkg/v10/configmap/mock_test.go
@@ -7,7 +7,6 @@ import (
 	"github.com/giantswarm/helmclient"
 	apiextensionsclient "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/helm/pkg/helm"
 )
 
 type tenantMock struct {
@@ -29,66 +28,4 @@ func (g *tenantMock) NewK8sClient(ctx context.Context, clusterID, apiDomain stri
 
 func (g *tenantMock) NewK8sExtClient(ctx context.Context, clusterID, apiDomain string) (apiextensionsclient.Interface, error) {
 	return g.fakeTenantK8sExtClient, nil
-}
-
-type helmMock struct {
-	defaultReleaseContent *helmclient.ReleaseContent
-	defaultReleaseHistory *helmclient.ReleaseHistory
-	defaultError          error
-}
-
-func (h *helmMock) DeleteRelease(ctx context.Context, releaseName string, options ...helm.DeleteOption) error {
-	if h.defaultError != nil {
-		return h.defaultError
-	}
-
-	return nil
-}
-
-func (h *helmMock) EnsureTillerInstalled(ctx context.Context) error {
-	return nil
-}
-
-func (h *helmMock) GetReleaseContent(ctx context.Context, releaseName string) (*helmclient.ReleaseContent, error) {
-	if h.defaultError != nil {
-		return nil, h.defaultError
-	}
-
-	return h.defaultReleaseContent, nil
-}
-
-func (h *helmMock) GetReleaseHistory(ctx context.Context, releaseName string) (*helmclient.ReleaseHistory, error) {
-	if h.defaultError != nil {
-		return nil, h.defaultError
-	}
-
-	return h.defaultReleaseHistory, nil
-}
-
-func (h *helmMock) InstallReleaseFromTarball(ctx context.Context, path, ns string, options ...helm.InstallOption) error {
-	return nil
-}
-
-func (h *helmMock) ListReleaseContents(ctx context.Context) ([]*helmclient.ReleaseContent, error) {
-	return nil, nil
-}
-
-func (h *helmMock) LoadChart(ctx context.Context, chartPath string) (helmclient.Chart, error) {
-	return helmclient.Chart{}, nil
-}
-
-func (h *helmMock) PingTiller(ctx context.Context) error {
-	return nil
-}
-
-func (h *helmMock) PullChartTarball(ctx context.Context, tarballURL string) (string, error) {
-	return "", nil
-}
-
-func (h *helmMock) RunReleaseTest(ctx context.Context, releaseName string, options ...helm.ReleaseTestOption) error {
-	return nil
-}
-
-func (h *helmMock) UpdateReleaseFromTarball(ctx context.Context, releaseName, path string, options ...helm.UpdateOption) error {
-	return nil
 }
