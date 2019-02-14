26f77deea83ecff0750f9e5daa007cd3f3c2146d
Tim Schindler <xh3b4sd@gmail.com>
2019-02-14 11:24:43 +0100

    copy v10 to v11

---
+++ service/controller/azure/v11/version_bundle.go
@@ -0,0 +1,46 @@
+package v10
+
+import (
+	"github.com/giantswarm/versionbundle"
+)
+
+func VersionBundle() versionbundle.Bundle {
+	return versionbundle.Bundle{
+		Changelogs: []versionbundle.Changelog{
+			{
+				Component:   "kube-state-metrics",
+				Description: "Updated to 1.5.0. More info here: https://github.com/kubernetes/kube-state-metrics/blob/v1.5.0/CHANGELOG.md",
+				Kind:        versionbundle.KindChanged,
+			},
+		},
+		Components: []versionbundle.Component{
+			{
+				Name:    "nginx-ingress-controller",
+				Version: "0.21.0",
+			},
+			{
+				Name:    "external-dns",
+				Version: "0.5.2",
+			},
+			{
+				Name:    "kube-state-metrics",
+				Version: "1.5.0",
+			},
+			{
+				Name:    "node-exporter",
+				Version: "0.15.1",
+			},
+			{
+				Name:    "coredns",
+				Version: "1.1.1",
+			},
+			{
+				Name:    "metrics-server",
+				Version: "0.3.1",
+			},
+		},
+		Name:     "cluster-operator",
+		Provider: "azure",
+		Version:  "0.10.0",
+	}
+}
