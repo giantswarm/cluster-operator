ef7e1de6a788bb210a083449a0adb119bd83e90c
Tim Schindler <xh3b4sd@gmail.com>
2019-02-26 12:15:11 +0100

    wire v12

--- service/controller/azure/cluster.go
+++ service/controller/azure/cluster.go
@@ -17,6 +17,7 @@ import (
 	"github.com/giantswarm/cluster-operator/pkg/cluster"
 	"github.com/giantswarm/cluster-operator/service/controller/azure/v10"
 	"github.com/giantswarm/cluster-operator/service/controller/azure/v11"
+	"github.com/giantswarm/cluster-operator/service/controller/azure/v12"
 	"github.com/giantswarm/cluster-operator/service/controller/azure/v9"
 )
 
@@ -149,6 +150,30 @@ func NewCluster(config ClusterConfig) (*Cluster, error) {
 		}
 	}
 
+	var v12ResourceSet *controller.ResourceSet
+	{
+		c := v12.ResourceSetConfig{
+			ApprClient:        config.ApprClient,
+			BaseClusterConfig: config.BaseClusterConfig,
+			CertSearcher:      config.CertSearcher,
+			Fs:                config.Fs,
+			G8sClient:         config.G8sClient,
+			K8sClient:         config.K8sClient,
+			Logger:            config.Logger,
+
+			CalicoAddress:      config.CalicoAddress,
+			CalicoPrefixLength: config.CalicoPrefixLength,
+			ClusterIPRange:     config.ClusterIPRange,
+			ProjectName:        config.ProjectName,
+			RegistryDomain:     config.RegistryDomain,
+		}
+
+		v12ResourceSet, err = v12.NewResourceSet(c)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+	}
+
 	var clusterController *controller.Controller
 	{
 		c := controller.Config{
@@ -160,6 +185,7 @@ func NewCluster(config ClusterConfig) (*Cluster, error) {
 				v9ResourceSet,
 				v10ResourceSet,
 				v11ResourceSet,
+				v12ResourceSet,
 			},
 			RESTClient: config.G8sClient.CoreV1alpha1().RESTClient(),
 
--- service/controller/kvm/v12/version_bundle.go
+++ service/controller/kvm/v12/version_bundle.go
@@ -8,18 +8,8 @@ func VersionBundle() versionbundle.Bundle {
 	return versionbundle.Bundle{
 		Changelogs: []versionbundle.Changelog{
 			{
-				Component:   "cluster-operator",
-				Description: "Reduce installation time of managed services in new tenant clusters.",
-				Kind:        versionbundle.KindChanged,
-			},
-			{
-				Component:   "coredns",
-				Description: "Updated to 1.3.1. More info here: https://coredns.io/2019/01/13/coredns-1.3.1-release/",
-				Kind:        versionbundle.KindChanged,
-			},
-			{
-				Component:   "coredns",
-				Description: "Added a custom block parameter to allow advanced configuration",
+				Component:   "TODO",
+				Description: "TODO",
 				Kind:        versionbundle.KindChanged,
 			},
 		},
@@ -47,6 +37,6 @@ func VersionBundle() versionbundle.Bundle {
 		},
 		Name:     "cluster-operator",
 		Provider: "kvm",
-		Version:  "0.11.0",
+		Version:  "0.12.0",
 	}
 }
