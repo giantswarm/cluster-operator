e2a695169a18012354057fd9bc245a0de94598a2
Ross Fairbanks <ross@giantswarm.io>
2019-03-04 15:10:33 +0100

    copy v12 to v13

---
+++ pkg/v13/configmap/update_test.go
@@ -0,0 +1,334 @@
+package configmap
+
+import (
+	"context"
+	"reflect"
+	"testing"
+
+	"github.com/giantswarm/micrologger/microloggertest"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/giantswarm/cluster-operator/pkg/label"
+)
+
+func Test_ConfigMap_newUpdateChange(t *testing.T) {
+	testCases := []struct {
+		name               string
+		currentConfigMaps  []*corev1.ConfigMap
+		desiredConfigMaps  []*corev1.ConfigMap
+		expectedConfigMaps []*corev1.ConfigMap
+		errorMatcher       func(error) bool
+	}{
+		{
+			name:               "case 0: empty current and desired, expected empty",
+			currentConfigMaps:  []*corev1.ConfigMap{},
+			desiredConfigMaps:  []*corev1.ConfigMap{},
+			expectedConfigMaps: []*corev1.ConfigMap{},
+		},
+		{
+			name: "case 1: non-empty current and empty desired, expected empty",
+			currentConfigMaps: []*corev1.ConfigMap{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "test-configmap",
+					},
+					Data: map[string]string{
+						"test": "test",
+					},
+				},
+			},
+			desiredConfigMaps:  []*corev1.ConfigMap{},
+			expectedConfigMaps: []*corev1.ConfigMap{},
+		},
+		{
+			name:              "case 2: empty current and non-empty desired, expected empty",
+			currentConfigMaps: []*corev1.ConfigMap{},
+			desiredConfigMaps: []*corev1.ConfigMap{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "test-configmap",
+					},
+					Data: map[string]string{
+						"test": "test",
+					},
+				},
+			},
+			expectedConfigMaps: []*corev1.ConfigMap{},
+		},
+		{
+			name: "case 3: equal current and desired, expected empty",
+			currentConfigMaps: []*corev1.ConfigMap{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "test-configmap",
+					},
+					Data: map[string]string{
+						"test": "test",
+					},
+				},
+			},
+			desiredConfigMaps: []*corev1.ConfigMap{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "test-configmap",
+					},
+					Data: map[string]string{
+						"test": "test",
+					},
+				},
+			},
+			expectedConfigMaps: []*corev1.ConfigMap{},
+		},
+		{
+			name: "case 4: unequal data, expected desired",
+			currentConfigMaps: []*corev1.ConfigMap{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "test-configmap",
+					},
+					Data: map[string]string{
+						"test": "test",
+					},
+				},
+			},
+			desiredConfigMaps: []*corev1.ConfigMap{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "test-configmap",
+					},
+					Data: map[string]string{
+						"test": "updated",
+					},
+				},
+			},
+			expectedConfigMaps: []*corev1.ConfigMap{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "test-configmap",
+					},
+					Data: map[string]string{
+						"test": "updated",
+					},
+				},
+			},
+		},
+		{
+			name: "case 5: unequal labels, expected desired",
+			currentConfigMaps: []*corev1.ConfigMap{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "test-configmap",
+						Labels: map[string]string{
+							label.App: "test",
+						},
+					},
+					Data: map[string]string{
+						"test": "test",
+					},
+				},
+			},
+			desiredConfigMaps: []*corev1.ConfigMap{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "test-configmap",
+						Labels: map[string]string{
+							label.App:     "test",
+							label.Cluster: "rue99",
+						},
+					},
+					Data: map[string]string{
+						"test": "test",
+					},
+				},
+			},
+			expectedConfigMaps: []*corev1.ConfigMap{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "test-configmap",
+						Labels: map[string]string{
+							label.App:     "test",
+							label.Cluster: "rue99",
+						},
+					},
+					Data: map[string]string{
+						"test": "test",
+					},
+				},
+			},
+		},
+		{
+			name: "case 6: unequal current and desired, expected changed desired",
+			currentConfigMaps: []*corev1.ConfigMap{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "test-configmap",
+						Labels: map[string]string{
+							label.App: "test",
+						},
+					},
+					Data: map[string]string{
+						"test": "test",
+					},
+				},
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "another-configmap",
+						Labels: map[string]string{
+							label.App: "test",
+						},
+					},
+					Data: map[string]string{
+						"test": "test",
+					},
+				},
+			},
+			desiredConfigMaps: []*corev1.ConfigMap{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "test-configmap",
+						Labels: map[string]string{
+							label.App: "test",
+						},
+					},
+					Data: map[string]string{
+						"test": "test",
+					},
+				},
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "another-configmap",
+						Labels: map[string]string{
+							label.App: "test",
+						},
+					},
+					Data: map[string]string{
+						"test": "updated",
+					},
+				},
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "extra-configmap",
+						Labels: map[string]string{
+							label.App: "test",
+						},
+					},
+					Data: map[string]string{
+						"test": "test",
+					},
+				},
+			},
+			expectedConfigMaps: []*corev1.ConfigMap{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "another-configmap",
+						Labels: map[string]string{
+							label.App: "test",
+						},
+					},
+					Data: map[string]string{
+						"test": "updated",
+					},
+				},
+			},
+		},
+		{
+			name: "case 7: user configmaps are not updated",
+			currentConfigMaps: []*corev1.ConfigMap{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "test-app-values-configmap",
+						Labels: map[string]string{
+							label.ConfigMapType: label.ConfigMapTypeApp,
+						},
+					},
+					Data: map[string]string{
+						"test": "test",
+					},
+				},
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "test-app-user-values-configmap",
+						Labels: map[string]string{
+							label.ConfigMapType: label.ConfigMapTypeUser,
+						},
+					},
+					Data: map[string]string{
+						"test": "test",
+					},
+				},
+			},
+			desiredConfigMaps: []*corev1.ConfigMap{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "test-app-values-configmap",
+						Labels: map[string]string{
+							label.ConfigMapType: label.ConfigMapTypeApp,
+						},
+					},
+					Data: map[string]string{
+						"test":          "test",
+						"another-value": "test",
+					},
+				},
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "test-app-user-values-configmap",
+						Labels: map[string]string{
+							label.ConfigMapType: label.ConfigMapTypeUser,
+						},
+					},
+					Data: map[string]string{
+						"test": "test",
+					},
+				},
+			},
+			expectedConfigMaps: []*corev1.ConfigMap{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "test-app-values-configmap",
+						Labels: map[string]string{
+							label.ConfigMapType: label.ConfigMapTypeApp,
+						},
+					},
+					Data: map[string]string{
+						"test":          "test",
+						"another-value": "test",
+					},
+				},
+			},
+		},
+	}
+
+	c := Config{
+		Logger: microloggertest.New(),
+		Tenant: &tenantMock{},
+
+		ProjectName: "cluster-operator",
+	}
+	newService, err := New(c)
+	if err != nil {
+		t.Fatal("expected", nil, "got", err)
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			result, err := newService.newUpdateChange(context.TODO(), tc.currentConfigMaps, tc.desiredConfigMaps)
+			if err != nil {
+				switch {
+				case err == nil && tc.errorMatcher == nil: // correct; carry on
+				case err != nil && tc.errorMatcher != nil:
+					if !tc.errorMatcher(err) {
+						t.Fatalf("error == %#v, want matching", err)
+					}
+				case err != nil && tc.errorMatcher == nil:
+					t.Fatalf("error == %#v, want nil", err)
+				case err == nil && tc.errorMatcher != nil:
+					t.Fatalf("error == nil, want non-nil")
+				}
+			} else if !reflect.DeepEqual(result, tc.expectedConfigMaps) {
+				t.Fatalf("expected %#v got %#v", tc.expectedConfigMaps, result)
+			}
+		})
+	}
+}
---
+++ service/controller/aws/v13/version_bundle.go
@@ -0,0 +1,46 @@
+package v12
+
+import (
+	"github.com/giantswarm/versionbundle"
+)
+
+func VersionBundle() versionbundle.Bundle {
+	return versionbundle.Bundle{
+		Changelogs: []versionbundle.Changelog{
+			{
+				Component:   "cluster-operator",
+				Description: "Configure tiller namespace for chart-operator",
+				Kind:        versionbundle.KindChanged,
+			},
+		},
+		Components: []versionbundle.Component{
+			{
+				Name:    "kube-state-metrics",
+				Version: "1.5.0",
+			},
+			{
+				Name:    "nginx-ingress-controller",
+				Version: "0.21.0",
+			},
+			{
+				Name:    "node-exporter",
+				Version: "0.15.1",
+			},
+			{
+				Name:    "coredns",
+				Version: "1.3.1",
+			},
+			{
+				Name:    "cluster-autoscaler",
+				Version: "1.3.1",
+			},
+			{
+				Name:    "metrics-server",
+				Version: "0.3.1",
+			},
+		},
+		Name:     "cluster-operator",
+		Provider: "aws",
+		Version:  "0.12.0",
+	}
+}
