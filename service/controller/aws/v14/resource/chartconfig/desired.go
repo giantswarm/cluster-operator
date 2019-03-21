b952027f53e4ea7143bb7a6418c4b501e0132909
Ross Fairbanks <rossf7@users.noreply.github.com>
2019-03-21 10:44:40 +0100

    v14: copy v13 (#411)

---
+++ service/controller/aws/v14/resource/chartconfig/desired.go
@@ -0,0 +1,37 @@
+package chartconfig
+
+import (
+	"context"
+
+	"github.com/giantswarm/microerror"
+
+	"github.com/giantswarm/cluster-operator/pkg/v13/chartconfig"
+	"github.com/giantswarm/cluster-operator/pkg/v13/key"
+	awskey "github.com/giantswarm/cluster-operator/service/controller/aws/v13/key"
+)
+
+func (r *Resource) GetDesiredState(ctx context.Context, obj interface{}) (interface{}, error) {
+	customObject, err := awskey.ToCustomObject(obj)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	clusterGuestConfig := awskey.ClusterGuestConfig(customObject)
+	apiDomain, err := key.APIDomain(clusterGuestConfig)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	clusterConfig := chartconfig.ClusterConfig{
+		APIDomain:    apiDomain,
+		ClusterID:    key.ClusterID(clusterGuestConfig),
+		Organization: key.ClusterOrganization(clusterGuestConfig),
+	}
+
+	desiredConfigMaps, err := r.chartConfig.GetDesiredState(ctx, clusterConfig, awskey.ChartSpecs())
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	return desiredConfigMaps, nil
+}
