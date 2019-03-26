254a65d6248b38981f6735b8ef1a2f7976f9ff44
Ross Fairbanks <ross@giantswarm.io>
2019-03-26 16:07:39 +0100

    Fix comment

--- pkg/v14/resource/namespace/delete.go
+++ pkg/v14/resource/namespace/delete.go
@@ -4,8 +4,8 @@ import (
 	"context"
 )
 
-// EnsureDeleted is not implemented for the tiller resource. Tiller will be
-// deleted when the tenant cluster resources are deleted.
+// EnsureDeleted is not implemented for the namespace resource. The namespace
+// will be deleted when the tenant cluster resources are deleted.
 func (r *Resource) EnsureDeleted(ctx context.Context, obj interface{}) error {
 	return nil
 }
