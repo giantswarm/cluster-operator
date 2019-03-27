59c7b21d2ec29036e62a74303637318b1fb7db6d
Ross Fairbanks <ross@giantswarm.io>
2019-03-27 10:27:13 +0100

    Backport fix to not delete chartconfig CRs and configmaps in tenant clusters

--- pkg/v7patch1/chartconfig/delete.go
+++ pkg/v7patch1/chartconfig/delete.go
@@ -38,16 +38,10 @@ func (c *ChartConfig) ApplyDeleteChange(ctx context.Context, clusterConfig Clust
 
 }
 
+// NewDeletePatch is a no-op because chartconfig CRs in the tenant cluster are
+// deleted with the tenant cluster resources.
 func (c *ChartConfig) NewDeletePatch(ctx context.Context, currentState, desiredState []*v1alpha1.ChartConfig) (*controller.Patch, error) {
-	delete, err := c.newDeleteChangeForDeletePatch(ctx, currentState, desiredState)
-	if err != nil {
-		return nil, microerror.Mask(err)
-	}
-
-	patch := controller.NewPatch()
-	patch.SetDeleteChange(delete)
-
-	return patch, nil
+	return nil, nil
 }
 
 func (c *ChartConfig) newDeleteChangeForDeletePatch(ctx context.Context, currentChartConfigs, desiredChartConfigs []*v1alpha1.ChartConfig) ([]*v1alpha1.ChartConfig, error) {
