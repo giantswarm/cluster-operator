968b85d0ca0a0ceb63b7b299b5ad0c18f23e84a8
Fernando Ripoll <pipo02mix@gmail.com>
2019-04-25 11:20:29 +0200

    Copy v14 to v14patch1 (#444)

---
+++ service/controller/aws/v14patch1/version_bundle.go
@@ -0,0 +1,56 @@
+package v14
+
+import (
+	"github.com/giantswarm/versionbundle"
+)
+
+func VersionBundle() versionbundle.Bundle {
+	return versionbundle.Bundle{
+		Changelogs: []versionbundle.Changelog{
+			{
+				Component:   "coredns",
+				Description: "Updated to 1.5.0. More info here: https://github.com/giantswarm/kubernetes-coredns/blob/master/CHANGELOG.md",
+				Kind:        versionbundle.KindChanged,
+			},
+			{
+				Component:   "coredns",
+				Description: "Adjusted auto scaling settings to respond better to peaks in requests.",
+				Kind:        versionbundle.KindChanged,
+			},
+			{
+				Component:   "nginx-ingress-controller",
+				Description: "Disabled migration logic now migration to helm chart is complete.",
+				Kind:        versionbundle.KindRemoved,
+			},
+		},
+		Components: []versionbundle.Component{
+			{
+				Name:    "kube-state-metrics",
+				Version: "1.5.0",
+			},
+			{
+				Name:    "nginx-ingress-controller",
+				Version: "0.23.0",
+			},
+			{
+				Name:    "node-exporter",
+				Version: "0.15.1",
+			},
+			{
+				Name:    "coredns",
+				Version: "1.5.0",
+			},
+			{
+				Name:    "cluster-autoscaler",
+				Version: "1.3.1",
+			},
+			{
+				Name:    "metrics-server",
+				Version: "0.3.1",
+			},
+		},
+		Name:     "cluster-operator",
+		Provider: "aws",
+		Version:  "0.14.0",
+	}
+}
