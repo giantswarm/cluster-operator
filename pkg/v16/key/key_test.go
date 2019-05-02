3046a588097d7ecd0f084c1b0f843eb70ee86497
jihyuk <jhbok@giantswarm.io>
2019-05-02 17:01:04 +0200

    update key function

--- pkg/v16/key/key_test.go
+++ pkg/v16/key/key_test.go
@@ -221,6 +221,34 @@ func Test_DNSIP(t *testing.T) {
 	}
 }
 
+func Test_ClusterConfigMapName(t *testing.T) {
+	testCases := []struct {
+		description    string
+		guestConfig    v1alpha1.ClusterGuestConfig
+		expectedResult string
+	}{
+		{
+			description: "case 0: getting cluster configmap name",
+			guestConfig: v1alpha1.ClusterGuestConfig{
+				DNSZone: "giantswarm.io",
+				ID:      "w7utg",
+				Name:    "My own snowflake cluster",
+				Owner:   "giantswarm",
+			},
+			expectedResult: "w7utg-cluster-values",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.description, func(t *testing.T) {
+			result := ClusterConfigMapName(tc.guestConfig)
+			if result != tc.expectedResult {
+				t.Fatalf("expected ClusterConfigMapName %#q, got %#q", tc.expectedResult, result)
+			}
+		})
+	}
+}
+
 func Test_ClusterID(t *testing.T) {
 	testCases := []struct {
 		description   string
