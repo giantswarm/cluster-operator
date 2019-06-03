66d07939395c18d58c5d442b67061ac842e9678e
Ross Fairbanks <ross@giantswarm.io>
2019-06-03 19:37:58 +0200

    Consider chart-operator annotations when checking if chartconfig CRs are modified

--- pkg/label/annotation.go
+++ pkg/label/annotation.go
@@ -3,6 +3,9 @@
 package label
 
 const (
+	// ChartOperatorAnnotationPrefix is used to filter annotations.
+	ChartOperatorAnnotationPrefix = "chart-operator.giantswarm.io"
+
 	// ForceHelmUpgradeAnnotationName is the name of the annotation that
 	// controls whether force is used when upgrading the Helm release.
 	ForceHelmUpgradeAnnotationName = "chart-operator.giantswarm.io/force-helm-upgrade"
