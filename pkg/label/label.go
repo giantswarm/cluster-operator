ebed04131a0184cecf129d6c1eba9c3f98202cd9
Ross Fairbanks <rossf7@users.noreply.github.com>
2019-06-05 09:25:21 +0200

    Enable upgrade force for chartconfig CRs when safe to do so (#472)
    * Add chart-operator annotation to label package
    
    * Rename file and add new property
    
    * Set annotation
    
    * Enable upgrade force when safe to do so and update changelogs
    
    * Consider chart-operator annotations when checking if chartconfig CRs are modified

--- pkg/label/label.go
+++ pkg/label/label.go
@@ -1,4 +1,4 @@
-// Package label contains common Kubernetes object labels. These are defined in
+// Package label contains common Kubernetes metadata. These are defined in
 // https://github.com/giantswarm/fmt/blob/master/kubernetes/annotations_and_labels.md.
 package label
 
