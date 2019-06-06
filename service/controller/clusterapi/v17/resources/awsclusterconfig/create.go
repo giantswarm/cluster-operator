7539baf4089086935c4051bc7adf738876d30e71
Marcel Mueller <marcel.mueller1@rwth-aachen.de>
2019-06-06 11:04:08 +0200

    v17: copy v16

---
+++ service/controller/clusterapi/v17/resources/awsclusterconfig/create.go
@@ -0,0 +1,27 @@
+package awsclusterconfig
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/giantswarm/cluster-operator/service/controller/clusterapi/v16/key"
+
+	"github.com/giantswarm/microerror"
+)
+
+func (r *Resource) EnsureCreated(ctx context.Context, obj interface{}) error {
+	cluster, err := key.ToCluster(obj)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	if !key.IsProviderSpecForAWS(cluster) {
+		r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("provider extension in cluster cr %q is not for AWS", cluster.Name))
+		r.logger.LogCtx(ctx, "level", "debug", "message", "canceling resource")
+		return nil
+	}
+
+	// TODO: Map Cluster -> AWSClusterConfig.
+
+	return nil
+}
