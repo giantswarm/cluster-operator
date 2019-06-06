c5725302e6ee7bfe4b0ed1a2386234239afe41a2
Tuomas Mäkinen <tuomas@giantswarm.io>
2019-06-06 15:39:07 +0300

    Panic when release version label is missing

--- service/controller/clusterapi/v16/key/cluster.go
+++ service/controller/clusterapi/v16/key/cluster.go
@@ -55,7 +55,9 @@ func ClusterName(cluster v1alpha1.Cluster) string {
 func ClusterReleaseVersion(cluster v1alpha1.Cluster) string {
 	relVer, ok := cluster.Labels[label.ReleaseKey]
 	if !ok {
-		relVer = "unknown"
+		// Later on we will have admission controller validating Cluster object
+		// so we shall not hit this panic.
+		panic("Cluster object is missing release version label.")
 	}
 	return relVer
 }
