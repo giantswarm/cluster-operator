aa1621c9c327c7ac56eb0cfbee71721e7d9c0119
jihyuk <jhbok@giantswarm.io>
2019-06-07 20:02:47 +0200

    explicit namespace while deleting

--- service/controller/aws/v15/resource/certconfigcleansing/create.go
+++ service/controller/aws/v15/resource/certconfigcleansing/create.go
@@ -44,7 +44,7 @@ func (r Resource) EnsureCreated(ctx context.Context, obj interface{}) error {
 
 			r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("CertConfig %#q do not have related AWSClusterConfigs, going to delete it", cert.Name))
 
-			err := r.g8sClient.CoreV1alpha1().CertConfigs("").Delete(cert.Name, &v1.DeleteOptions{})
+			err := r.g8sClient.CoreV1alpha1().CertConfigs(cert.Namespace).Delete(cert.Name, &v1.DeleteOptions{})
 			if err != nil {
 				r.logger.LogCtx(ctx, "level", "error", "message", fmt.Sprintf("Failed to delete CertConfig %#q", cert.Name), "stack", fmt.Sprintf("%#v", err))
 			} else {
