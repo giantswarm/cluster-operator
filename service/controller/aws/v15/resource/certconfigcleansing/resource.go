9d1e1d404e0455ef228039ae285633ee29c9e0fc
jihyuk <jhbok@giantswarm.io>
2019-06-07 10:50:24 +0200

    change resource name to follow v15

--- service/controller/aws/v15/resource/certconfigcleansing/resource.go
+++ service/controller/aws/v15/resource/certconfigcleansing/resource.go
@@ -11,7 +11,7 @@ import (
 )
 
 const (
-	Name = "certconfigcleansingv1"
+	Name = "certconfigcleansingv15"
 )
 
 // Config represents the configuration used to create a new certconfigcleansing resource.
