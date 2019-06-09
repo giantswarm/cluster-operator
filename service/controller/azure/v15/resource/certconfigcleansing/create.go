b56d836684cc0580e9990ad9b4ce5a30eb47d98b
Jihyuk Bok <tomahawk28@users.noreply.github.com>
2019-06-09 12:25:05 +0200

    Temporarily resource: Cleansing remaining CertConfigs (#484)
    * aws certconfig cleansing
    
    * moving version to v15
    
    * use AWSClusterConfig rather than AWSConfig
    
    * change resource name to follow v15
    
    * do not log in case of error returning
    
    * moving functions to outer files
    
    * explicit namespace while deleting
    
    * adding to kvm, azure
    
    * finding exact cluster target

---
+++ service/controller/azure/v15/resource/certconfigcleansing/create.go
@@ -0,0 +1,63 @@
+package certconfigcleansing
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/giantswarm/microerror"
+	"k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func (r Resource) EnsureCreated(ctx context.Context, obj interface{}) error {
+
+	currentMap := map[string]bool{}
+
+	r.logger.LogCtx(ctx, "level", "debug", "message", "Collecting all AzureClusterConfigs")
+
+	rs, err := r.g8sClient.CoreV1alpha1().AzureClusterConfigs("").List(v1.ListOptions{})
+	if err != nil {
+		r.logger.LogCtx(ctx, "level", "error", "message", "could not get AzureClusterConfig resource", "stack", fmt.Sprintf("%#v", err))
+		return microerror.Mask(err)
+	}
+
+	r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("Collected %d AzureClusterConfigs", len(rs.Items)))
+
+	for _, azureClusterConfig := range rs.Items {
+		currentMap[azureClusterConfig.Spec.Guest.ID] = true
+	}
+
+	r.logger.LogCtx(ctx, "level", "debug", "message", "Collecting all CertConfigs")
+
+	certs, err := r.g8sClient.CoreV1alpha1().CertConfigs("").List(v1.ListOptions{})
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("Collected %d CertConfigs", len(certs.Items)))
+
+	count := 0
+	for _, cert := range certs.Items {
+
+		clusterID := cert.Spec.Cert.ClusterID
+
+		if _, ok := currentMap[clusterID]; !ok {
+
+			r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("CertConfig %#q do not have related AzureClusterConfigs, going to delete it", cert.Name))
+
+			err := r.g8sClient.CoreV1alpha1().CertConfigs(cert.Namespace).Delete(cert.Name, &v1.DeleteOptions{})
+			if err != nil {
+				r.logger.LogCtx(ctx, "level", "error", "message", fmt.Sprintf("Failed to delete CertConfig %#q", cert.Name), "stack", fmt.Sprintf("%#v", err))
+			} else {
+				count++
+				r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("Successfully delete CertConfig %#q", cert.Name))
+			}
+
+		} else {
+			r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("CertConfig %#q HAVE related AzureClusterConfigs, should not delete it", cert.Name))
+		}
+	}
+
+	r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("Total deleted %d certconfigs", count))
+
+	return nil
+}
