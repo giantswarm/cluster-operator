497e7d9e5e13f89e8b806f60bae6cf14742053a0
Tim Schindler <xh3b4sd@gmail.com>
2019-06-14 13:29:29 +0200

    handle tenant api errors

--- service/controller/clusterapi/v17/resources/machinedeploymentstatus/delete.go
+++ service/controller/clusterapi/v17/resources/machinedeploymentstatus/delete.go
@@ -3,8 +3,11 @@ package machinedeploymentstatus
 import (
 	"context"
 
+	"github.com/giantswarm/errors/tenant"
 	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/operatorkit/controller/context/reconciliationcanceledcontext"
+	"github.com/giantswarm/operatorkit/controller/context/resourcecanceledcontext"
+	"github.com/giantswarm/tenantcluster"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
@@ -25,8 +28,24 @@ func (r *Resource) EnsureDeleted(ctx context.Context, obj interface{}) error {
 	var nodes []corev1.Node
 	var ready []corev1.Node
 	{
+		r.logger.LogCtx(ctx, "level", "debug", "message", "finding nodes of tenant cluster")
+
 		l, err := cc.Client.TenantCluster.K8s.CoreV1().Nodes().List(metav1.ListOptions{})
-		if err != nil {
+		if tenantcluster.IsTimeout(err) {
+			r.logger.LogCtx(ctx, "level", "debug", "message", "timeout fetching certificates")
+
+			r.logger.LogCtx(ctx, "level", "debug", "message", "canceling resource")
+			resourcecanceledcontext.SetCanceled(ctx)
+
+			return nil
+		} else if tenant.IsAPINotAvailable(err) {
+			r.logger.LogCtx(ctx, "level", "debug", "message", "tenant API not available")
+
+			r.logger.LogCtx(ctx, "level", "debug", "message", "canceling resource")
+			resourcecanceledcontext.SetCanceled(ctx)
+
+			return nil
+		} else if err != nil {
 			return microerror.Mask(err)
 		}
 
@@ -39,6 +58,8 @@ func (r *Resource) EnsureDeleted(ctx context.Context, obj interface{}) error {
 				}
 			}
 		}
+
+		r.logger.LogCtx(ctx, "level", "debug", "message", "found nodes of tenant cluster")
 
 		replicasChanged := cr.Status.Replicas != int32(len(nodes))
 		readyReplicasChanged := cr.Status.ReadyReplicas != int32(len(ready))
