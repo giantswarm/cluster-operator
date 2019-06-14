a5d9d4f029b33a334b1428b05aa8cd5bc90383c2
Tim Schindler <tim@giantswarm.io>
2019-06-14 10:00:19 +0200

    add tenantclients resource to have them available in the controllercontext (#493)

---
+++ service/controller/clusterapi/v17/resources/tenantclients/create.go
@@ -0,0 +1,9 @@
+package tenantclients
+
+import (
+	"context"
+)
+
+func (r *Resource) EnsureCreated(ctx context.Context, obj interface{}) error {
+	return nil
+}
---
+++ service/controller/clusterapi/v17/resources/tenantclients/delete.go
@@ -0,0 +1,9 @@
+package tenantclients
+
+import (
+	"context"
+)
+
+func (r *Resource) EnsureDeleted(ctx context.Context, obj interface{}) error {
+	return nil
+}
