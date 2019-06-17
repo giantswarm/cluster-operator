c858556d6cfe8ece97015fb593b7ce0c88e981f3
Tim Schindler <tim@giantswarm.io>
2019-06-17 13:26:01 +0200

    update status of machine deployment (#495)

--- service/controller/clusterapi/v17/resources/machinedeploymentstatus/resource.go
+++ service/controller/clusterapi/v17/resources/machinedeploymentstatus/resource.go
@@ -1,10 +1,21 @@
 package machinedeploymentstatus
 
 import (
+	"context"
+
 	"github.com/giantswarm/apiextensions/pkg/clientset/versioned"
+	"github.com/giantswarm/errors/tenant"
 	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/micrologger"
+	"github.com/giantswarm/operatorkit/controller/context/reconciliationcanceledcontext"
+	"github.com/giantswarm/tenantcluster"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/cluster-api/pkg/apis/cluster/v1alpha1"
 	"sigs.k8s.io/cluster-api/pkg/client/clientset_generated/clientset"
+
+	"github.com/giantswarm/cluster-operator/service/controller/clusterapi/v17/controllercontext"
+	"github.com/giantswarm/cluster-operator/service/controller/clusterapi/v17/key"
 )
 
 const (
@@ -45,4 +56,93 @@ func New(config Config) (*Resource, error) {
 
 func (r *Resource) Name() string {
 	return Name
+}
+
+func (r *Resource) ensure(ctx context.Context, obj interface{}) error {
+	var err error
+
+	cr, err := key.ToMachineDeployment(obj)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+	cc, err := controllercontext.FromContext(ctx)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	var nodes []corev1.Node
+	var ready []corev1.Node
+	{
+		r.logger.LogCtx(ctx, "level", "debug", "message", "finding nodes of tenant cluster")
+
+		l, err := cc.Client.TenantCluster.K8s.CoreV1().Nodes().List(metav1.ListOptions{})
+		if tenantcluster.IsTimeout(err) {
+			r.logger.LogCtx(ctx, "level", "debug", "message", "timeout fetching certificates")
+			r.logger.LogCtx(ctx, "level", "debug", "message", "canceling resource")
+
+			return nil
+		} else if tenant.IsAPINotAvailable(err) {
+			r.logger.LogCtx(ctx, "level", "debug", "message", "tenant API not available")
+			r.logger.LogCtx(ctx, "level", "debug", "message", "canceling resource")
+
+			return nil
+		} else if err != nil {
+			return microerror.Mask(err)
+		}
+
+		nodes = l.Items
+
+		for _, n := range nodes {
+			for _, c := range n.Status.Conditions {
+				if c.Type == corev1.NodeReady && c.Status == corev1.ConditionTrue {
+					ready = append(ready, n)
+				}
+			}
+		}
+
+		r.logger.LogCtx(ctx, "level", "debug", "message", "found nodes of tenant cluster")
+	}
+
+	{
+		r.logger.LogCtx(ctx, "level", "debug", "message", "checking if status of machine deployment needs to be updated")
+
+		replicasChanged := cr.Status.Replicas != int32(len(nodes))
+		readyReplicasChanged := cr.Status.ReadyReplicas != int32(len(ready))
+
+		if !replicasChanged && !readyReplicasChanged {
+			r.logger.LogCtx(ctx, "level", "debug", "message", "status of machine deployment does not need to be updated")
+			return nil
+		}
+
+		r.logger.LogCtx(ctx, "level", "debug", "message", "status of machine deployment needs to be updated")
+	}
+
+	var cl *v1alpha1.MachineDeployment
+	{
+		cl, err = r.cmaClient.ClusterV1alpha1().MachineDeployments(cr.Namespace).Get(cr.Name, metav1.GetOptions{})
+		if err != nil {
+			return microerror.Mask(err)
+		}
+	}
+
+	{
+		cl.Status.Replicas = int32(len(nodes))
+		cl.Status.ReadyReplicas = int32(len(ready))
+	}
+
+	{
+		r.logger.LogCtx(ctx, "level", "debug", "message", "updating status of machine deployment")
+
+		_, err := r.cmaClient.ClusterV1alpha1().MachineDeployments(cl.Namespace).Update(cl)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+
+		r.logger.LogCtx(ctx, "level", "debug", "message", "updated status of machine deployment")
+
+		r.logger.LogCtx(ctx, "level", "debug", "message", "canceling reconciliation")
+		reconciliationcanceledcontext.SetCanceled(ctx)
+	}
+
+	return nil
 }
