029698857319a3a8eddbe9fcc3a5375ea8ee308d
jihyuk <jhbok@giantswarm.io>
2019-07-18 10:53:03 +0200

    skip comparing cordoned annotations

--- pkg/annotation/annotation.go
+++ pkg/annotation/annotation.go
@@ -6,6 +6,14 @@ const (
 	// ChartOperator is used to filter annotations.
 	ChartOperator = "chart-operator.giantswarm.io"
 
+	// CordonReason is the name of the annotation that indicates
+	// the reason of why chart-operator should not apply any update on this chart CR.
+	CordonReason = "chart-operator.giantswarm.io/cordon-reason"
+
+	// CordonUntilDate is the name of the annotation that indicates
+	// the expiration date of rule of this cordon.
+	CordonUntilDate = "chart-operator.giantswarm.io/cordon-until"
+
 	// ForceHelmUpgrade is the name of the annotation that controls whether force
 	// is used when upgrading the Helm release.
 	ForceHelmUpgrade = "chart-operator.giantswarm.io/force-helm-upgrade"
