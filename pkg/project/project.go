a53d924fa0aa4c3c7bb236548f692475de27fedc
Theo Brigitte <theo.brigitte@gmail.com>
2019-07-25 15:29:28 +0200

    add pkg/project

---
+++ pkg/project/project.go
@@ -0,0 +1,29 @@
+package project
+
+var (
+	description = "The cluster-operator manages Kubernetes guest cluster resources."
+	gitSHA      = "n/a"
+	name        = "cluster-operator"
+	source      = "https://github.com/giantswarm/cluster-operator"
+	version     = "n/a"
+)
+
+func Description() string {
+	return description
+}
+
+func GitSHA() string {
+	return gitSHA
+}
+
+func Name() string {
+	return name
+}
+
+func Source() string {
+	return source
+}
+
+func Version() string {
+	return version
+}
