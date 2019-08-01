0108a8e0a2f7a8e6d71f58cf4f0d2319f37d3c62
jihyuk <jhbok@giantswarm.io>
2019-08-01 13:58:05 +0200

    adding generator into new resource

--- service/controller/aws/v18/resource/app/resource.go
+++ service/controller/aws/v18/resource/app/resource.go
@@ -2,6 +2,7 @@ package app
 
 import (
 	"github.com/giantswarm/apiextensions/pkg/clientset/versioned"
+	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/micrologger"
 	"k8s.io/client-go/kubernetes"
 )
@@ -10,6 +11,41 @@ const (
 	// Name is the identifier of the resource.
 	Name = "appv18"
 )
+
+// Config represents the configuration used to create a new app resource.
+type Config struct {
+	K8sClient kubernetes.Interface
+	G8sClient versioned.Interface
+	Logger    micrologger.Logger
+
+	ProjectName string
+}
+
+func New(config Config) (*StateGetter, error) {
+	if config.K8sClient == nil {
+		return nil, microerror.Maskf(invalidConfigError, "%T.K8sClient must not be empty", config)
+	}
+	if config.G8sClient == nil {
+		return nil, microerror.Maskf(invalidConfigError, "%T.G8sClient must not be empty", config)
+	}
+	if config.Logger == nil {
+		return nil, microerror.Maskf(invalidConfigError, "%T.Logger must not be empty", config)
+	}
+
+	if config.ProjectName == "" {
+		return nil, microerror.Maskf(invalidConfigError, "%T.ProjectName must not be empty", config)
+	}
+
+	s := &StateGetter{
+		k8sClient: config.K8sClient,
+		g8sClient: config.G8sClient,
+		logger:    config.Logger,
+
+		projectName: config.ProjectName,
+	}
+
+	return s, nil
+}
 
 type StateGetter struct {
 	// Dependencies.
