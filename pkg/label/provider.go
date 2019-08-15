786f9a18a09a5b0056138dbf698dcc16d6708547
Tim Schindler <xh3b4sd@gmail.com>
2019-08-15 15:23:09 +0200

    separate labels for better overview

---
+++ pkg/label/provider.go
@@ -0,0 +1,10 @@
+package label
+
+const (
+	// ProviderAWS label specifies format for AWS provider ID.
+	ProviderAWS = "aws"
+	// ProviderAzure label specifies format for Azure provider ID.
+	ProviderAzure = "azure"
+	// ProviderKVM label specifies format for KVM provider ID.
+	ProviderKVM = "kvm"
+)
