a8e20af3cedbbb55e2e2ace5b19d4a666064dbdd
Jihyuk Bok <tomahawk28@users.noreply.github.com>
2019-08-16 12:41:22 +0200

    introduce new v17patch1 (#572)

---
+++ pkg/v17patch1/key/types.go
@@ -0,0 +1,13 @@
+package key
+
+// ChartSpec is used to define chartconfig custom resources.
+type ChartSpec struct {
+	AppName           string
+	ChannelName       string
+	ChartName         string
+	ConfigMapName     string
+	Namespace         string
+	ReleaseName       string
+	UseUpgradeForce   bool
+	UserConfigMapName string
+}
