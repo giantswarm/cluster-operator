9f194c2007fbd9ff87836c8428201bc606382c2c
Jihyuk Bok <tomahawk28@users.noreply.github.com>
2019-08-16 11:48:04 +0200

    adding aws/v14patch2 (#570)

---
+++ service/controller/aws/v14patch2/resource/chartconfig/desired.go
@@ -0,0 +1,37 @@
+package chartconfig
+
+import (
+	"context"
+
+	"github.com/giantswarm/microerror"
+
+	"github.com/giantswarm/cluster-operator/pkg/v14patch2/chartconfig"
+	"github.com/giantswarm/cluster-operator/pkg/v14patch2/key"
+	awskey "github.com/giantswarm/cluster-operator/service/controller/aws/v14patch2/key"
+)
+
+func (r *Resource) GetDesiredState(ctx context.Context, obj interface{}) (interface{}, error) {
+	customObject, err := awskey.ToCustomObject(obj)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	clusterGuestConfig := awskey.ClusterGuestConfig(customObject)
+	apiDomain, err := key.APIDomain(clusterGuestConfig)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	clusterConfig := chartconfig.ClusterConfig{
+		APIDomain:    apiDomain,
+		ClusterID:    key.ClusterID(clusterGuestConfig),
+		Organization: key.ClusterOrganization(clusterGuestConfig),
+	}
+
+	desiredConfigMaps, err := r.chartConfig.GetDesiredState(ctx, clusterConfig, awskey.ChartSpecs())
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	return desiredConfigMaps, nil
+}
