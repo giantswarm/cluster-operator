5a7c62609df4508a3b88ee0e16ff18f0e279e661
Tim Schindler <tim@giantswarm.io>
2019-08-26 11:20:17 +0200

    add workercount resource (#606)

--- service/controller/clusterapi/v19/controllercontext/status.go
+++ service/controller/clusterapi/v19/controllercontext/status.go
@@ -2,4 +2,10 @@ package controllercontext
 
 type ContextStatus struct {
 	Versions map[string]string
+	Worker   ContextStatusWorker
+}
+
+type ContextStatusWorker struct {
+	Nodes int
+	Ready int
 }
