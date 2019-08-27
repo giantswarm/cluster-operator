6a4b02635248d9f4e37665ee34b965dd20b6327d
Tim Schindler <tim@giantswarm.io>
2019-08-27 11:52:01 +0200

    prevent updating encryption key secrets (#611)

--- service/controller/clusterapi/v19/resources/encryptionkey/current.go
+++ service/controller/clusterapi/v19/resources/encryptionkey/current.go
@@ -7,7 +7,7 @@ import (
 	"github.com/giantswarm/microerror"
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
-	apismetav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/giantswarm/cluster-operator/service/controller/clusterapi/v19/key"
 )
@@ -22,7 +22,7 @@ func (r *Resource) GetCurrentState(ctx context.Context, obj interface{}) ([]*cor
 	{
 		r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("finding secret %#q in namespace %#q", secretName(cr), cr.Namespace))
 
-		secret, err = r.k8sClient.Core().Secrets(cr.Namespace).Get(secretName(cr), apismetav1.GetOptions{})
+		secret, err = r.k8sClient.Core().Secrets(cr.Namespace).Get(secretName(cr), metav1.GetOptions{})
 		if apierrors.IsNotFound(err) {
 			r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("did not find secret %#q in namespace %#q", secretName(cr), cr.Namespace))
 			return nil, nil
