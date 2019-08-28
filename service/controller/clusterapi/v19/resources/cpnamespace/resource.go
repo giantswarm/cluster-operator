47a127e5f54a3f54411934c8c7aaf179c81963e9
Tim Schindler <xh3b4sd@gmail.com>
2019-08-28 13:36:01 +0200

    wire cpnamespace resource

--- service/controller/clusterapi/v19/resources/cpnamespace/resource.go
+++ service/controller/clusterapi/v19/resources/cpnamespace/resource.go
@@ -9,7 +9,7 @@ import (
 
 const (
 	// Name is the identifier of the resource.
-	Name = "cpnamespacev29"
+	Name = "cpnamespacev19"
 )
 
 // Config represents the configuration used to create a new namespace resource.
