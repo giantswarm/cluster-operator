379d7f6b12e15a0f371705c208832ee2e8fc8df6
Bricktop <marcel.mueller1@rwth-aachen.de>
2019-08-30 17:55:54 +0200

    v20: imports azure (#638)

--- service/controller/azure/v20/resource/configmap/current.go
+++ service/controller/azure/v20/resource/configmap/current.go
@@ -8,9 +8,9 @@ import (
 	"github.com/giantswarm/operatorkit/controller/context/reconciliationcanceledcontext"
 	"github.com/giantswarm/operatorkit/controller/context/resourcecanceledcontext"
 
-	"github.com/giantswarm/cluster-operator/pkg/v19/configmap"
-	"github.com/giantswarm/cluster-operator/pkg/v19/key"
-	azurekey "github.com/giantswarm/cluster-operator/service/controller/azure/v19/key"
+	"github.com/giantswarm/cluster-operator/pkg/v20/configmap"
+	"github.com/giantswarm/cluster-operator/pkg/v20/key"
+	azurekey "github.com/giantswarm/cluster-operator/service/controller/azure/v20/key"
 )
 
 func (r *Resource) GetCurrentState(ctx context.Context, obj interface{}) (interface{}, error) {
