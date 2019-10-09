3e3820a0d001210264942636b53453fd8ba8252d
jihyuk <jhbok@giantswarm.io>
2019-10-09 16:36:49 +0200

    check chartconfig crd first

---
+++ pkg/v21/key/error_internal.go
@@ -0,0 +1,21 @@
+package key
+
+import (
+	"strings"
+
+	"github.com/giantswarm/microerror"
+)
+
+const (
+	errorSentence = "the server could not find the requested resource (get chartconfigs.core.giantswarm.io)"
+)
+
+func IsChartConfigNotInstalled(err error) bool {
+	if err == nil {
+		return false
+	}
+
+	c := microerror.Cause(err)
+
+	return strings.Contains(c.Error(), errorSentence)
+}
