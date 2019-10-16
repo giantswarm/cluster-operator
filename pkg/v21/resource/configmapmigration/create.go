3460ad88ec628a2d9aeaeae4ac337558a421018e
Ross Fairbanks <ross@rossfairbanks.com>
2019-10-16 09:02:59 +0200

    compiling

--- pkg/v21/resource/configmapmigration/create.go
+++ pkg/v21/resource/configmapmigration/create.go
@@ -10,6 +10,7 @@ import (
 	"github.com/giantswarm/operatorkit/controller/context/resourcecanceledcontext"
 	yaml "gopkg.in/yaml.v2"
 	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 
