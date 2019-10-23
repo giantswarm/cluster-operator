f555aabe1042f5cfa6bc4e63baee39ba8f26907a
Tim Schindler <tim@giantswarm.io>
2019-10-23 14:09:16 +0200

    increase delete event reliability by dropping operator versions from controller context (#723)

--- service/controller/clusterapi/v21/resource/operatorversions/delete.go
+++ service/controller/clusterapi/v21/resource/operatorversions/delete.go
@@ -2,15 +2,14 @@ package operatorversions
 
 import (
 	"context"
-
-	"github.com/giantswarm/microerror"
 )
 
+// EnsureDeleted is not putting the operator versions into the controller
+// context because we do not want to fetch the version information on delete
+// events. This is to reduce eventual friction. Cluster deletion should not be
+// affected only because some releases are missing or broken when fetching them
+// from cluster-service. Other resources must not rely on operator version
+// information on delete events.
 func (r *Resource) EnsureDeleted(ctx context.Context, obj interface{}) error {
-	err := r.ensure(ctx, obj)
-	if err != nil {
-		return microerror.Mask(err)
-	}
-
 	return nil
 }
