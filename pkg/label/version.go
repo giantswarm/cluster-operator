c3a550261ffafcbc291b17b4511b7fe58eacf9fb
Tim Schindler <tim@giantswarm.io>
2019-10-23 15:58:59 +0200

    improve label management and prepare CertConfig CRs for VOO (#726)

--- pkg/label/version.go
+++ pkg/label/version.go
@@ -1,9 +1,12 @@
 package label
 
 const (
-	//AppOperatorVersion sets the version label for app custom resources
-	// managed by the operator.
+	// AppOperatorVersion sets the version label for app custom resources managed
+	// by the operator.
 	AppOperatorVersion = "app-operator.giantswarm.io/version"
+	// CertOperatorVersion sets the version label for CertConfig CRs managed by
+	// cert-operator.
+	CertOperatorVersion = "cert-operator.giantswarm.io/version"
 	// OperatorVersion label transports the operator version requested to be used
 	// when reconciling the observed runtime object.
 	OperatorVersion = "cluster-operator.giantswarm.io/version"
