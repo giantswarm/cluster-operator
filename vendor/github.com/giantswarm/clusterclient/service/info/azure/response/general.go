51058fd0f3d197c4db93f92403761a02cccc452c
Tim Schindler <xh3b4sd@gmail.com>
2019-10-25 10:57:25 +0200

    update vendor deps

--- vendor/github.com/giantswarm/clusterclient/service/info/azure/response/general.go
+++ vendor/github.com/giantswarm/clusterclient/service/info/azure/response/general.go
@@ -1,15 +1,14 @@
 package response
 
 type General struct {
+	Datacenter       string `json:"datacenter"`
 	InstallationName string `json:"installation_name"`
 	Provider         string `json:"provider"`
-
-	// To be implemented:
-	// datacenter
 }
 
 func DefaultGeneral() General {
 	return General{
+		Datacenter:       "",
 		InstallationName: "",
 		Provider:         "",
 	}
