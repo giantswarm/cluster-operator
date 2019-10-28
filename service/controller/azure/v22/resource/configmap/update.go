190fae479872e45736a8271ac97915bcd3b46554
Ross Fairbanks <ross@rossfairbanks.com>
2019-10-28 11:58:15 +0100

    Update imports

--- service/controller/azure/v22/resource/configmap/update.go
+++ service/controller/azure/v22/resource/configmap/update.go
@@ -8,9 +8,9 @@ import (
 	"github.com/giantswarm/operatorkit/controller"
 	"github.com/giantswarm/operatorkit/controller/context/reconciliationcanceledcontext"
 
-	"github.com/giantswarm/cluster-operator/pkg/v21/configmap"
-	"github.com/giantswarm/cluster-operator/pkg/v21/key"
-	azurekey "github.com/giantswarm/cluster-operator/service/controller/azure/v21/key"
+	"github.com/giantswarm/cluster-operator/pkg/v22/configmap"
+	"github.com/giantswarm/cluster-operator/pkg/v22/key"
+	azurekey "github.com/giantswarm/cluster-operator/service/controller/azure/v22/key"
 )
 
 func (r *Resource) ApplyUpdateChange(ctx context.Context, obj, updateChange interface{}) error {
