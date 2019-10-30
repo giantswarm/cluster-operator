b34ebbacb897e67d219b07f27075c2ccdf443ca4
Tim Schindler <xh3b4sd@gmail.com>
2019-10-30 13:28:00 +0100

    add node pools changelog

--- service/controller/clusterapi/v22/version_bundle.go
+++ service/controller/clusterapi/v22/version_bundle.go
@@ -8,8 +8,8 @@ func VersionBundle() versionbundle.Bundle {
 	return versionbundle.Bundle{
 		Changelogs: []versionbundle.Changelog{
 			{
-				Component:   "TODO",
-				Description: "Add your changes here.",
+				Component:   "nodepools",
+				Description: "Add Node Pools functionality.",
 				Kind:        versionbundle.KindChanged,
 			},
 		},
