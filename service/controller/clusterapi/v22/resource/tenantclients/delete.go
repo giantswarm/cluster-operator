9c1bf4e3dbce95d3244533a90b86c0b808ae1f9a
Tim Schindler <xh3b4sd@gmail.com>
2019-11-06 01:42:35 +0100

    drop tenant clients from controller context on delete events (#728)

--- service/controller/clusterapi/v22/resource/tenantclients/delete.go
+++ service/controller/clusterapi/v22/resource/tenantclients/delete.go
@@ -2,15 +2,13 @@ package tenantclients
 
 import (
 	"context"
-
-	"github.com/giantswarm/microerror"
 )
 
+// EnsureDeleted is not putting the tenant clients into the controller context
+// because we do not want to interact with the Tenant Cluster API on delete
+// events. This is to reduce eventual friction. Cluster deletion should not be
+// affected only because the Tenant Cluster API is not available for some
+// reason. Other resources must not rely on tenant clients on delete events.
 func (r *Resource) EnsureDeleted(ctx context.Context, obj interface{}) error {
-	err := r.ensure(ctx, obj)
-	if err != nil {
-		return microerror.Mask(err)
-	}
-
 	return nil
 }
