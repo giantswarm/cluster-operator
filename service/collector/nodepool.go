47e5b7f78e5207eca0bbe51e5806a391ecaca96b
Tuomas Mäkinen <tuomas@giantswarm.io>
2019-11-14 09:04:48 +0200

    change np count to nodepool subsystem

--- service/collector/nodepool.go
+++ service/collector/nodepool.go
@@ -13,7 +13,7 @@ import (
 
 var (
 	nodePools *prometheus.Desc = prometheus.NewDesc(
-		prometheus.BuildFQName(namespace, subsystemCluster, "nodepools"),
+		prometheus.BuildFQName(namespace, subsystemNodePool, "count"),
 		"Number of Node Pools in a cluster as provided by the MachineDeployment CRs associated with a given cluster ID.",
 		[]string{
 			"cluster_id",
