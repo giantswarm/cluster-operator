4a92e5aee6dbbe760b7f199514b8a77a3a04fe5f
Ross Fairbanks <ross@rossfairbanks.com>
2019-11-15 15:26:27 +0100

    Use default catalog

--- pkg/v22/key/key.go
+++ pkg/v22/key/key.go
@@ -136,7 +136,7 @@ func CommonAppSpecs() []AppSpec {
 		},
 		{
 			App:           "nginx-ingress-controller",
-			Catalog:       "default-test",
+			Catalog:       "default",
 			Chart:         "nginx-ingress-controller-app",
 			ConfigMapName: IngressControllerConfigMapName,
 			// For clusterapi clusters ingress controller is an optional app.
@@ -144,7 +144,7 @@ func CommonAppSpecs() []AppSpec {
 			Namespace:  metav1.NamespaceSystem,
 			// Upgrade force is disabled to avoid affecting customer workloads.
 			UseUpgradeForce: false,
-			Version:         "1.1.0-963189b8e0981e65810e41a65d027c65f64e948b",
+			Version:         "1.1.0",
 		},
 		{
 			App:             "node-exporter",
