6f3f050ceeedf4a2184d02e1e1f70ecb7f4e549c
Paweł Kopiczko <kopiczko@gmail.com>
2019-11-15 11:38:13 +0000

    add v23 resource set

--- service/controller/clusterapi/cluster.go
+++ service/controller/clusterapi/cluster.go
@@ -21,6 +21,7 @@ import (
 	"github.com/giantswarm/cluster-operator/pkg/project"
 	v21 "github.com/giantswarm/cluster-operator/service/controller/clusterapi/v21"
 	v22 "github.com/giantswarm/cluster-operator/service/controller/clusterapi/v22"
+	v23 "github.com/giantswarm/cluster-operator/service/controller/clusterapi/v23"
 )
 
 // ClusterConfig contains necessary dependencies and settings for
@@ -142,6 +143,35 @@ func NewCluster(config ClusterConfig) (*Cluster, error) {
 		}
 	}
 
+	var resourceSetV23 *controller.ResourceSet
+	{
+		c := v23.ClusterResourceSetConfig{
+			ApprClient:    config.ApprClient,
+			CertsSearcher: config.CertsSearcher,
+			ClusterClient: config.ClusterClient,
+			CMAClient:     config.CMAClient,
+			FileSystem:    config.FileSystem,
+			G8sClient:     config.G8sClient,
+			K8sClient:     config.K8sClient,
+			Logger:        config.Logger,
+			Tenant:        config.Tenant,
+
+			APIIP:              config.APIIP,
+			CalicoAddress:      config.CalicoAddress,
+			CalicoPrefixLength: config.CalicoPrefixLength,
+			CertTTL:            config.CertTTL,
+			ClusterIPRange:     config.ClusterIPRange,
+			DNSIP:              config.DNSIP,
+			Provider:           config.Provider,
+			RegistryDomain:     config.RegistryDomain,
+		}
+
+		resourceSetV23, err = v23.NewClusterResourceSet(c)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+	}
+
 	var clusterController *controller.Controller
 	{
 		c := controller.Config{
@@ -152,6 +182,7 @@ func NewCluster(config ClusterConfig) (*Cluster, error) {
 			ResourceSets: []*controller.ResourceSet{
 				resourceSetV21,
 				resourceSetV22,
+				resourceSetV23,
 			},
 			RESTClient: config.CMAClient.ClusterV1alpha1().RESTClient(),
 
