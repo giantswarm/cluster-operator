c7fd70911faf03e58f7f5475989416d4f6bf80a5
Ross Fairbanks <ross@rossfairbanks.com>
2019-11-20 19:24:19 +0100

    fmt

--- pkg/v22/key/types.go
+++ pkg/v22/key/types.go
@@ -5,12 +5,12 @@ type AppSpec struct {
 	App     string
 	Catalog string
 	Chart   string
-	// Whether app is installed for clusterapi clusters only
+	// Whether app is installed for clusterapi clusters only.
 	ClusterAPIOnly bool
 	// ConfigMapName overrides the name, otherwise the cluster values configmap
 	// is used.
 	ConfigMapName string
-	// Whether app is installed for legacy clusters only
+	// Whether app is installed for legacy clusters only.
 	LegacyOnly      bool
 	Namespace       string
 	UseUpgradeForce bool
