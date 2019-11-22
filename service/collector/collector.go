636d5e5404bcb3b58d2a4cb22fbef781419a4def
Paweł Kopiczko <kopiczko@gmail.com>
2019-11-22 07:05:12 +0000

    prepare legacy branch
    - Remove all but legacy 0.22.0 ({aws,azure,kvm}/v22). - Drop clusterapi.
    - Drop controller package because it was exporting only clusterapi
      related metrics.

--- service/collector/collector.go
+++
@@ -1,8 +0,0 @@
-package collector
-
-const (
-	GaugeValue        float64 = 1
-	namespace                 = "cluster_operator"
-	subsystemCluster          = "cluster"
-	subsystemNodePool         = "node_pool"
-)
