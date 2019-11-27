1b9d09317b4ae2336469938eb9fc58613c373e7b
Marcel Mueller <marcel.mueller1@rwth-aachen.de>
2019-11-27 11:51:34 +0100

    remove old label

--- pkg/label/k8s.go
+++ pkg/label/k8s.go
@@ -3,6 +3,4 @@ package label
 const (
 	// MasterNode label denotes a K8s cluster master node.
 	MasterNode = "node.kubernetes.io/master"
-	// MasterNodeRole label denotes K8s cluster master node role.
-	MasterNodeRole = "node-role.kubernetes.io/master"
 )
