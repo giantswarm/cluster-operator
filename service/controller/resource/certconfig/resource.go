d16f3b25078d7f65a100c53a9de2aa5de53efdfb
Michael Gliwinski <42101611+mig4@users.noreply.github.com>
2019-11-29 12:34:39 +0000

    service, pkg: flatten pkg (#819)
    Towards https://github.com/giantswarm/giantswarm/issues/7722
    
    - Package `pkg/v22/controllercontext` moved to `service/controller/controllercontext`.
    - Package `pkg/v22/key` moved to `service/controller/key`.
    - Package `pkg/v22/resource` moved to `service/controller/resource`.
    - Package `pkg/v22/chartconfig` moved to `service/controller/internal/chartconfig`.
    - Package `pkg/v22/configmap` moved to `service/controller/internal/configmap`.
    - Package `pkg/v22/errors` moved to `service/controller/internal/errors`.
    - Package `pkg/cluster` moved to `service/internal/cluster`.
    
    Clean-up import order after moves using `go fmt`.

--- service/controller/resource/certconfig/resource.go
+++ service/controller/resource/certconfig/resource.go
@@ -1,72 +1,83 @@
 package certconfig
 
 import (
+	"reflect"
+
 	"github.com/giantswarm/apiextensions/pkg/apis/core/v1alpha1"
 	"github.com/giantswarm/apiextensions/pkg/clientset/versioned"
 	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/micrologger"
+	apismetav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
 
 	"github.com/giantswarm/cluster-operator/service/controller/key"
+	"github.com/giantswarm/cluster-operator/service/internal/cluster"
 )
 
 const (
 	// Name is the identifier of the resource.
 	Name = "certconfigv22"
-)
 
-const (
-	// listCertConfigLimit is the suggested maximum number of CertConfigs returned
-	// in one List() call to K8s API. Server may choose to not support this so
-	// this is not strict maximum.
+	// listCertConfigLimit is the suggested maximum number of CertConfigs
+	// returned in one List() call to K8s API. Server may choose to not support
+	// this so this is not strict maximum.
 	listCertConfigLimit = 25
 )
 
 // Config represents the configuration used to create a new cloud config resource.
 type Config struct {
-	G8sClient versioned.Interface
-	Logger    micrologger.Logger
-
-	APIIP    string
-	CertTTL  string
-	Provider string
+	BaseClusterConfig        cluster.Config
+	G8sClient                versioned.Interface
+	K8sClient                kubernetes.Interface
+	Logger                   micrologger.Logger
+	Provider                 string
+	ToClusterGuestConfigFunc func(obj interface{}) (v1alpha1.ClusterGuestConfig, error)
+	ToClusterObjectMetaFunc  func(obj interface{}) (apismetav1.ObjectMeta, error)
 }
 
 // Resource implements the cloud config resource.
 type Resource struct {
-	g8sClient versioned.Interface
-	logger    micrologger.Logger
-
-	apiIP    string
-	certTTL  string
-	provider string
+	baseClusterConfig        cluster.Config
+	g8sClient                versioned.Interface
+	k8sClient                kubernetes.Interface
+	logger                   micrologger.Logger
+	provider                 string
+	toClusterGuestConfigFunc func(obj interface{}) (v1alpha1.ClusterGuestConfig, error)
+	toClusterObjectMetaFunc  func(obj interface{}) (apismetav1.ObjectMeta, error)
 }
 
 // New creates a new configured cloud config resource.
 func New(config Config) (*Resource, error) {
+	if reflect.DeepEqual(config.BaseClusterConfig, cluster.Config{}) {
+		return nil, microerror.Maskf(invalidConfigError, "%T.BaseClusterConfig must not be empty", config)
+	}
 	if config.G8sClient == nil {
 		return nil, microerror.Maskf(invalidConfigError, "%T.G8sClient must not be empty", config)
+	}
+	if config.K8sClient == nil {
+		return nil, microerror.Maskf(invalidConfigError, "%T.K8sClient must not be empty", config)
 	}
 	if config.Logger == nil {
 		return nil, microerror.Maskf(invalidConfigError, "%T.Logger must not be empty", config)
 	}
-
-	if config.APIIP == "" {
-		return nil, microerror.Maskf(invalidConfigError, "%T.APIIP must not be empty", config)
-	}
-	if config.CertTTL == "" {
-		return nil, microerror.Maskf(invalidConfigError, "%T.CertTTL must not be empty", config)
-	}
 	if config.Provider == "" {
 		return nil, microerror.Maskf(invalidConfigError, "%T.Provider must not be empty", config)
 	}
+	if config.ToClusterGuestConfigFunc == nil {
+		return nil, microerror.Maskf(invalidConfigError, "%T.ToClusterGuestConfigFunc must not be empty", config)
+	}
+	if config.ToClusterObjectMetaFunc == nil {
+		return nil, microerror.Maskf(invalidConfigError, "%T.ToClusterObjectMetaFunc must not be empty", config)
+	}
 
 	newService := &Resource{
-		g8sClient: config.G8sClient,
-		logger:    config.Logger,
-
-		apiIP:    config.APIIP,
-		certTTL:  config.CertTTL,
-		provider: config.Provider,
+		baseClusterConfig:        config.BaseClusterConfig,
+		g8sClient:                config.G8sClient,
+		k8sClient:                config.K8sClient,
+		logger:                   config.Logger,
+		provider:                 config.Provider,
+		toClusterGuestConfigFunc: config.ToClusterGuestConfigFunc,
+		toClusterObjectMetaFunc:  config.ToClusterObjectMetaFunc,
 	}
 
 	return newService, nil
