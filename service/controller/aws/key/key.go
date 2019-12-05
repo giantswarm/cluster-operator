6e599bafef2afe80f7f57196ab7c78fc7c5cc081
mig4 <42650719@auril.club>
2019-12-05 11:03:14 +0000

    service/controller/aws: remove obsolete functions

--- service/controller/aws/key/key.go
+++ service/controller/aws/key/key.go
@@ -1,8 +1,6 @@
 package key
 
 import (
-	"github.com/giantswarm/apiextensions/pkg/apis/core/v1alpha1"
-	"github.com/giantswarm/microerror"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/giantswarm/cluster-operator/service/controller/key"
@@ -48,46 +46,4 @@ func AppSpecs() []key.AppSpec {
 			Version:         "1.1.0",
 		},
 	}
-}
-
-// ChartSpecs returns charts installed only for AWS.
-func ChartSpecs() []key.ChartSpec {
-	// Add any provider specific charts here.
-	return []key.ChartSpec{
-		{
-			AppName:           "cluster-autoscaler",
-			ChannelName:       "0-9-stable",
-			ChartName:         "kubernetes-cluster-autoscaler-chart",
-			ConfigMapName:     "cluster-autoscaler-values",
-			HasAppCR:          true,
-			Namespace:         metav1.NamespaceSystem,
-			ReleaseName:       "cluster-autoscaler",
-			UseUpgradeForce:   true,
-			UserConfigMapName: "cluster-autoscaler-user-values",
-		},
-	}
-}
-
-// ClusterGuestConfig extracts ClusterGuestConfig from AWSClusterConfig.
-func ClusterGuestConfig(awsClusterConfig v1alpha1.AWSClusterConfig) v1alpha1.ClusterGuestConfig {
-	return awsClusterConfig.Spec.Guest.ClusterGuestConfig
-}
-
-// ToCustomObject converts value to v1alpha1.AWSClusterConfig and returns it or
-// error if type does not match.
-func ToCustomObject(v interface{}) (v1alpha1.AWSClusterConfig, error) {
-	customObjectPointer, ok := v.(*v1alpha1.AWSClusterConfig)
-	if !ok {
-		return v1alpha1.AWSClusterConfig{}, microerror.Maskf(wrongTypeError, "expected '%T', got '%T'", &v1alpha1.AWSClusterConfig{}, v)
-	}
-
-	if customObjectPointer == nil {
-		return v1alpha1.AWSClusterConfig{}, microerror.Maskf(emptyValueError, "empty value cannot be converted to CustomObject")
-	}
-
-	return *customObjectPointer, nil
-}
-
-func WorkerCount(awsClusterConfig v1alpha1.AWSClusterConfig) int {
-	return len(awsClusterConfig.Spec.Guest.Workers)
 }
