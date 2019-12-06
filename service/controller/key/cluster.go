f81a0846cbffa42c080292903aafba3afc5299e7
Tim Schindler <xh3b4sd@gmail.com>
2019-12-06 16:21:29 +0100

    drop unused key funcs

--- service/controller/key/cluster.go
+++ service/controller/key/cluster.go
@@ -7,45 +7,12 @@ import (
 	"sigs.k8s.io/cluster-api/pkg/apis/cluster/v1alpha1"
 )
 
-func AWSClusterConfigName(cluster v1alpha1.Cluster) string {
-	return cluster.Name
-}
-
 func ClusterAPIEndpoint(cluster v1alpha1.Cluster) string {
 	return fmt.Sprintf("api.%s.k8s.%s", ClusterID(&cluster), ClusterBaseDomain(cluster))
 }
 
 func ClusterBaseDomain(cluster v1alpha1.Cluster) string {
 	return clusterProviderSpec(cluster).Cluster.DNS.Domain
-}
-
-func ClusterCredentialSecretName(cluster v1alpha1.Cluster) string {
-	return clusterProviderSpec(cluster).Provider.CredentialSecret.Name
-}
-
-func ClusterCredentialSecretNamespace(cluster v1alpha1.Cluster) string {
-	return clusterProviderSpec(cluster).Provider.CredentialSecret.Namespace
-}
-
-func ClusterDNSZone(cluster v1alpha1.Cluster) string {
-	return fmt.Sprintf("%s.k8s.%s", ClusterID(&cluster), clusterProviderSpec(cluster).Cluster.DNS.Domain)
-}
-
-func ClusterMasterAZ(cluster v1alpha1.Cluster) string {
-	return clusterProviderSpec(cluster).Provider.Master.AvailabilityZone
-}
-
-func ClusterMasterInstanceType(cluster v1alpha1.Cluster) string {
-	return clusterProviderSpec(cluster).Provider.Master.InstanceType
-}
-
-func ClusterName(cluster v1alpha1.Cluster) string {
-	return clusterProviderSpec(cluster).ClusterName
-}
-
-func IsProviderSpecForAWS(cluster v1alpha1.Cluster) bool {
-	_, err := g8sClusterSpecFromCMAClusterSpec(cluster.Spec.ProviderSpec)
-	return err == nil
 }
 
 func TenantBaseDomain(cluster v1alpha1.Cluster) string {
--- service/controller/key/machine_deployment.go
+++ service/controller/key/machine_deployment.go
@@ -5,10 +5,6 @@ import (
 	"sigs.k8s.io/cluster-api/pkg/apis/cluster/v1alpha1"
 )
 
-func MachineDeploymentWorkerInstanceType(machineDeployment v1alpha1.MachineDeployment) string {
-	return machineDeploymentProviderSpec(machineDeployment).Provider.Worker.InstanceType
-}
-
 func ToMachineDeployment(v interface{}) (v1alpha1.MachineDeployment, error) {
 	if v == nil {
 		return v1alpha1.MachineDeployment{}, microerror.Maskf(wrongTypeError, "expected '%T', got '%T'", &v1alpha1.MachineDeployment{}, v)
