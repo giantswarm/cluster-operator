222dba40b5ece8fd36784c01d29cbdf00356d343
mig4 <42650719@auril.club>
2019-12-06 10:37:52 +0000

    project: extract project.BundleVersion
    Towards https://github.com/giantswarm/giantswarm/issues/7722.

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -1,12 +1,17 @@
 package project
 
 var (
-	description = "The cluster-operator manages Kubernetes guest cluster resources."
-	gitSHA      = "n/a"
-	name        = "cluster-operator"
-	source      = "https://github.com/giantswarm/cluster-operator"
-	version     = "n/a"
+	bundleVersion = "2.0.0-dev"
+	description   = "The cluster-operator manages Kubernetes guest cluster resources."
+	gitSHA        = "n/a"
+	name          = "cluster-operator"
+	source        = "https://github.com/giantswarm/cluster-operator"
+	version       = "n/a"
 )
+
+func BundleVersion() string {
+	return bundleVersion
+}
 
 func Description() string {
 	return description
