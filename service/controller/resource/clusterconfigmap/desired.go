b7490e38bac66a0e3c98c941a3d5a765ee0212db
Ross Fairbanks <ross@rossfairbanks.com>
2019-12-12 19:24:59 +0100

    Add configmap for optional ingress controller

--- service/controller/resource/clusterconfigmap/desired.go
+++ service/controller/resource/clusterconfigmap/desired.go
@@ -7,9 +7,11 @@ import (
 	yaml "gopkg.in/yaml.v2"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	cmav1alpha1 "sigs.k8s.io/cluster-api/pkg/apis/cluster/v1alpha1"
 
 	"github.com/giantswarm/cluster-operator/pkg/label"
 	"github.com/giantswarm/cluster-operator/pkg/project"
+	"github.com/giantswarm/cluster-operator/service/controller/controllercontext"
 	"github.com/giantswarm/cluster-operator/service/controller/key"
 )
 
@@ -19,35 +21,92 @@ func (r *Resource) GetDesiredState(ctx context.Context, obj interface{}) ([]*cor
 		return nil, microerror.Mask(err)
 	}
 
-	var configMap *corev1.ConfigMap
+	cc, err := controllercontext.FromContext(ctx)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	var ingressControllerReplicas int32
 	{
-		v := map[string]string{
-			"baseDomain":   key.TenantBaseDomain(cr),
-			"clusterDNSIP": r.dnsIP,
-			"clusterID":    key.ClusterID(&cr),
+		// We set the number of replicas to the number of worker nodes. This is set
+		// by the workercount resource using the current number of nodes from the
+		// tenant cluster.
+		for _, v := range cc.Status.Worker {
+			ingressControllerReplicas += v.Nodes
 		}
 
-		b, err := yaml.Marshal(v)
+		// TODO Fallback to desired number of nodes.
+	}
+
+	// We limit the number of replicas to 20 as running more than this does
+	// not make sense.
+	//
+	// TODO: Remove Ingress Controller configmap once HPA is enabled by default.
+	//
+	//	https://github.com/giantswarm/giantswarm/issues/8080
+	//
+	if ingressControllerReplicas > 20 {
+		ingressControllerReplicas = 20
+	}
+
+	configMapSpecs := []configMapSpec{
+		{
+			Name:      key.ClusterConfigMapName(&cr),
+			Namespace: key.ClusterID(&cr),
+			Values: map[string]interface{}{
+				"baseDomain":   key.TenantBaseDomain(cr),
+				"clusterDNSIP": r.dnsIP,
+				"clusterID":    key.ClusterID(&cr),
+			},
+		},
+		{
+			Name:      "ingress-controller-values",
+			Namespace: key.ClusterID(&cr),
+			Values: map[string]interface{}{
+				"baseDomain": key.TenantBaseDomain(cr),
+				"clusterID":  key.ClusterID(&cr),
+				"ingressController": map[string]interface{}{
+					"replicas": ingressControllerReplicas,
+				},
+			},
+		},
+	}
+
+	var configMaps []*corev1.ConfigMap
+
+	for _, spec := range configMapSpecs {
+		configMap, err := newConfigMap(cr, spec)
 		if err != nil {
 			return nil, microerror.Mask(err)
 		}
 
-		configMap = &corev1.ConfigMap{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      key.ClusterConfigMapName(&cr),
-				Namespace: key.ClusterID(&cr),
-				Labels: map[string]string{
-					label.Cluster:      key.ClusterID(&cr),
-					label.ManagedBy:    project.Name(),
-					label.Organization: key.OrganizationID(&cr),
-					label.ServiceType:  label.ServiceTypeManaged,
-				},
-			},
-			Data: map[string]string{
-				"values": string(b),
-			},
-		}
+		configMaps = append(configMaps, configMap)
 	}
 
-	return []*corev1.ConfigMap{configMap}, nil
+	return configMaps, nil
+}
+
+func newConfigMap(cr cmav1alpha1.Cluster, configMapSpec configMapSpec) (*corev1.ConfigMap, error) {
+	yamlValues, err := yaml.Marshal(configMapSpec.Values)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	cm := &corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      configMapSpec.Name,
+			Namespace: configMapSpec.Namespace,
+			Labels: map[string]string{
+				label.Cluster:      key.ClusterID(&cr),
+				label.ManagedBy:    project.Name(),
+				label.Organization: key.OrganizationID(&cr),
+				label.ServiceType:  label.ServiceTypeManaged,
+			},
+		},
+		Data: map[string]string{
+			"values": string(yamlValues),
+		},
+	}
+
+	return cm, nil
 }
