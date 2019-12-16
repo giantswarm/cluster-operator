b572af1bef639ce21e957e2cae820dd915b734ce
Tim Schindler <xh3b4sd@gmail.com>
2019-12-16 17:59:52 +0100

    drop api endpoint prefix

--- service/controller/resource/basedomain/resource.go
+++ service/controller/resource/basedomain/resource.go
@@ -3,6 +3,7 @@ package basedomain
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/micrologger"
@@ -62,7 +63,17 @@ func (r *Resource) ensure(ctx context.Context, obj interface{}) error {
 		return nil
 	}
 
-	cc.Status.Endpoint.Base = cr.Status.APIEndpoints[0].Host
+	// In order to get the base domain from the API Endpoint we need to remove the
+	// API relevent prefix. The CR status contains an API Endpoint like the
+	// following.
+	//
+	//     api.n2fm4.k8s.gauss.eu-central-1.aws.gigantic.io
+	//
+	// What we want to dispatch via the controller context is something like this.
+	//
+	//     gauss.eu-central-1.aws.gigantic.io
+	//
+	cc.Status.Endpoint.Base = strings.Replace(cr.Status.APIEndpoints[0].Host, fmt.Sprintf("api.%s.k8s.", key.ClusterID(&cr)), "", 1)
 
 	return nil
 }
