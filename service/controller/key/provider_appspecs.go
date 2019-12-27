751f1c3bd8a283eba08bd295da33c9258687bfa4
Stevo Slavić <stevo@giantswarm.io>
2019-12-27 14:29:08 +0100

    Update kiam App from 1.0.0 to 1.0.1

--- service/controller/key/provider_appspecs.go
+++ service/controller/key/provider_appspecs.go
@@ -24,7 +24,7 @@ func AWSAppSpecs() []AppSpec {
 			ClusterAPIOnly:  true,
 			Namespace:       metav1.NamespaceSystem,
 			UseUpgradeForce: true,
-			Version:         "1.0.0",
+			Version:         "1.0.1",
 		},
 		{
 			App:             "cert-manager",
