9ed84187ce8cf5ea17bbb9d7e7f125133ef7dd9c
Volodymyr Stoiko <me@volodymyrstoiko.com>
2020-01-03 09:57:32 +0200

    Use external-dns v1.1.0

--- service/controller/key/provider_appspecs.go
+++ service/controller/key/provider_appspecs.go
@@ -15,7 +15,7 @@ func AWSAppSpecs() []AppSpec {
 			ClusterAPIOnly:  true,
 			Namespace:       metav1.NamespaceSystem,
 			UseUpgradeForce: true,
-			Version:         "1.0.1",
+			Version:         "1.1.0",
 		},
 		{
 			App:             "kiam",
