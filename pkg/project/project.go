5fcce502dc0a9cbe2d3e345f12dd9c49f5fc58e0
Tim Schindler <xh3b4sd@gmail.com>
2020-01-06 10:40:23 +0100

    revert version change

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -1,7 +1,7 @@
 package project
 
 var (
-	bundleVersion = "2.0.1-xh3b4sd"
+	bundleVersion = "2.0.1-dev"
 	description   = "The cluster-operator manages Kubernetes guest cluster resources."
 	gitSHA        = "n/a"
 	name          = "cluster-operator"
