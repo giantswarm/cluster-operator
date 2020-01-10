83d217ac16ab5f4310c4ebd8cb04383380faa8f3
Ross Fairbanks <ross@rossfairbanks.com>
2020-01-10 10:02:23 +0100

    Remove calico error checks due to azure

--- service/controller/resource/clusterconfigmap/resource.go
+++ service/controller/resource/clusterconfigmap/resource.go
@@ -47,12 +47,6 @@ func New(config Config) (*Resource, error) {
 		return nil, microerror.Maskf(invalidConfigError, "%T.Logger must not be empty", config)
 	}
 
-	if config.CalicoAddress == "" {
-		return nil, microerror.Maskf(invalidConfigError, "%T.CalicoAddress must not be empty", config)
-	}
-	if config.CalicoPrefixLength == "" {
-		return nil, microerror.Maskf(invalidConfigError, "%T.CalicoPrefixLength must not be empty", config)
-	}
 	if config.ClusterIPRange == "" {
 		return nil, microerror.Maskf(invalidConfigError, "%T.ClusterIPRange must not be empty", config)
 	}
