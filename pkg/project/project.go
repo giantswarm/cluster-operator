76ac4a840c66713507269689cc97c34d6dd97802
Ross Fairbanks <ross@rossfairbanks.com>
2020-01-10 12:21:32 +0100

    Bump version to 0.21.2

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -1,7 +1,7 @@
 package project
 
 var (
-	bundleVersion = "0.21.1"
+	bundleVersion = "0.21.2-dev"
 	description   = "The cluster-operator manages Kubernetes tenant cluster resources."
 	gitSHA        = "n/a"
 	name          = "cluster-operator"
