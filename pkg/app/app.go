c2c64ff892a232e0ceeb37062479d583b0511edd
jihyuk <jhbok@giantswarm.io>
2020-01-17 14:30:40 +0100

    adding appspecs

--- pkg/app/app.go
+++ pkg/app/app.go
@@ -1,1 +1,30 @@
 package app
+
+import "github.com/giantswarm/cluster-operator/service/controller/key"
+
+var (
+	Default = key.AppSpec{
+		Catalog:         "default",
+		Namespace:       "kube-system",
+		UseUpgradeForce: true,
+	}
+
+	Exceptions = map[string]key.AppSpec{
+		"cert-exporter": {
+			Chart: "cert-exporter",
+		},
+		"chart-operator": {
+			// chart-operator must be installed first so the chart CRD is
+			// created in the tenant cluster.
+			Chart:     "chart-operator",
+			Namespace: "giantswarm",
+		},
+		"coredns": {
+			// Upgrade force is disabled to avoid affecting customer workloads.
+			UseUpgradeForce: false,
+		},
+		"net-exporter": {
+			Chart: "net-exporter",
+		},
+	}
+)
