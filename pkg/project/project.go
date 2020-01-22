cc296783135995b51d480dab2c6597b64c2a372d
Bricktop <marcel.mueller1@rwth-aachen.de>
2020-01-22 12:51:48 +0100

    Update legacy dev version to 0.21.4-dev (#896)
    * Update legacy dev version to 0.21.4-dev
    
    * update chantgelog

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -1,7 +1,7 @@
 package project
 
 var (
-	bundleVersion = "0.21.3"
+	bundleVersion = "0.21.4-dev"
 	description   = "The cluster-operator manages Kubernetes tenant cluster resources."
 	gitSHA        = "n/a"
 	name          = "cluster-operator"
