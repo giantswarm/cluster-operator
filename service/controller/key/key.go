f07c8b64f21d6005d62d91036c3f4c3182e6cf83
Thomas Fussell <thomas@fussell.io>
2020-01-21 19:32:26 -0500

    update kube-state-metrics to 1.0.1, merge changelogs from unreleased previous versions

--- service/controller/key/key.go
+++ service/controller/key/key.go
@@ -76,7 +76,7 @@ func CommonAppSpecs() []AppSpec {
 			Chart:           "kube-state-metrics-app",
 			Namespace:       metav1.NamespaceSystem,
 			UseUpgradeForce: true,
-			Version:         "1.0.0",
+			Version:         "1.0.1",
 		},
 		{
 			App:             "metrics-server",
