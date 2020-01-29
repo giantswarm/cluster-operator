41e66bc6067c2b3a2a69ce378465921a86fc9b5b
Julien Garcia Gonzalez <julien@giantswarm.io>
2020-01-29 11:48:10 +0100

    Update Net-exporter to 1.6.0 (#902)
    * Test disable check in net-exporter
    
    Signed-off-by: Julien Garcia Gonzalez <julien@giantswarm.io>
    
    * use 2.0.0-2b98e6bae04b4285e4914dfcc02208d83f87a1a2 as version
    
    Signed-off-by: Julien Garcia Gonzalez <julien@giantswarm.io>
    
    * Prepare release net-exporter
    
    Signed-off-by: Julien Garcia Gonzalez <julien@giantswarm.io>
    
    * Update net-expoter to 1.6.0
    
    Signed-off-by: Julien Garcia Gonzalez <julien@giantswarm.io>
    
    * update changelog
    
    Signed-off-by: Julien Garcia Gonzalez <julien@giantswarm.io>

--- service/controller/key/key.go
+++ service/controller/key/key.go
@@ -92,7 +92,7 @@ func CommonAppSpecs() []AppSpec {
 			Chart:           "net-exporter",
 			Namespace:       metav1.NamespaceSystem,
 			UseUpgradeForce: true,
-			Version:         "1.5.1",
+			Version:         "1.6.0",
 		},
 		{
 			App:             "node-exporter",
