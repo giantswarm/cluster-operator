fb53b5ebd1fa390339e2d86e50f803036f3e4cbe
Jihyuk Bok <tomahawk28@users.noreply.github.com>
2020-02-10 13:39:50 +0100

    2.1.1-dev for WIP (#917)

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -1,7 +1,7 @@
 package project
 
 var (
-	bundleVersion = "2.1.1"
+	bundleVersion = "2.1.1-dev"
 	description   = "The cluster-operator manages Kubernetes tenant cluster resources."
 	gitSHA        = "n/a"
 	name          = "cluster-operator"
