597343309bf098d814c8b72df5c265d52039a00e
Jihyuk Bok <tomahawk28@users.noreply.github.com>
2020-02-11 16:57:19 +0100

    Legacy 0.23.0 release (#921)
    * Preparing 0.24.0 release for legacy
    
    * 0.23.0

--- pkg/project/version_bundle_azure.go
+++ pkg/project/version_bundle_azure.go
@@ -8,44 +8,15 @@ var versionBundleAzure = versionbundle.Bundle{
 	Changelogs: []versionbundle.Changelog{
 		{
 			Component:   "cluster-operator",
-			Description: "TODO",
+			Description: "Move default app list from cluster-operator code to release repository.",
 			Kind:        versionbundle.KindChanged,
 			URLs: []string{
-				"",
+				"https://github.com/giantswarm/cluster-operator/pull/889",
 			},
 		},
 	},
-	Components: []versionbundle.Component{
-		{
-			Name:    "kube-state-metrics",
-			Version: "1.9.2",
-		},
-		{
-			Name:    "nginx-ingress-controller",
-			Version: "0.28.0",
-		},
-		{
-			Name:    "node-exporter",
-			Version: "0.18.1",
-		},
-		{
-			Name:    "coredns",
-			Version: "1.6.5",
-		},
-		{
-			Name:    "metrics-server",
-			Version: "0.3.3",
-		},
-		{
-			Name:    "external-dns",
-			Version: "0.5.11",
-		},
-		{
-			Name:    "net-exporter",
-			Version: "1.6.0",
-		},
-	},
-	Name:     "cluster-operator",
-	Provider: "azure",
-	Version:  BundleVersion(),
+	Components: []versionbundle.Component{},
+	Name:       "cluster-operator",
+	Provider:   "azure",
+	Version:    BundleVersion(),
 }
--- pkg/project/version_bundle_kvm.go
+++ pkg/project/version_bundle_kvm.go
@@ -8,40 +8,15 @@ var versionBundleKVM = versionbundle.Bundle{
 	Changelogs: []versionbundle.Changelog{
 		{
 			Component:   "cluster-operator",
-			Description: "TODO",
+			Description: "Move default app list from cluster-operator code to release repository.",
 			Kind:        versionbundle.KindChanged,
 			URLs: []string{
-				"",
+				"https://github.com/giantswarm/cluster-operator/pull/889",
 			},
 		},
 	},
-	Components: []versionbundle.Component{
-		{
-			Name:    "kube-state-metrics",
-			Version: "1.9.2",
-		},
-		{
-			Name:    "nginx-ingress-controller",
-			Version: "0.28.0",
-		},
-		{
-			Name:    "node-exporter",
-			Version: "0.18.1",
-		},
-		{
-			Name:    "coredns",
-			Version: "1.6.5",
-		},
-		{
-			Name:    "metrics-server",
-			Version: "0.3.3",
-		},
-		{
-			Name:    "net-exporter",
-			Version: "1.6.0",
-		},
-	},
-	Name:     "cluster-operator",
-	Provider: "kvm",
-	Version:  BundleVersion(),
+	Components: []versionbundle.Component{},
+	Name:       "cluster-operator",
+	Provider:   "kvm",
+	Version:    BundleVersion(),
 }
