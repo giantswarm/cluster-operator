cf3c7d78d676e80f938d2e2847efb277a8291776
jihyuk <jhbok@giantswarm.io>
2020-02-20 11:44:22 +0100

    dev version

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -1,7 +1,7 @@
 package project
 
 var (
-	bundleVersion = "0.23.1-dev"
+	bundleVersion = "0.24.0-dev"
 	description   = "The cluster-operator manages Kubernetes tenant cluster resources."
 	gitSHA        = "n/a"
 	name          = "cluster-operator"
