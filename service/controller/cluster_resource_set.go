2d2286d9d50c791e4d88dfc627c21053b7deba63
Michael Gliwinski <42101611+mig4@users.noreply.github.com>
2020-02-25 11:49:37 +0000

    pkg, service: replace `bundleVersion` with `version` (#934)
    Towards https://github.com/giantswarm/giantswarm/issues/8637
    
    We want to invert how the project version is maintained and now that
    it isn't injected during the build from git data, we can remove the
    separate concept of _bundle version_ and replace it with just _version_.

--- service/controller/cluster_resource_set.go
+++ service/controller/cluster_resource_set.go
@@ -460,7 +460,7 @@ func newClusterResourceSet(config clusterResourceSetConfig) (*controller.Resourc
 			return false
 		}
 
-		if key.OperatorVersion(&cr) == project.BundleVersion() {
+		if key.OperatorVersion(&cr) == project.Version() {
 			return true
 		}
 
