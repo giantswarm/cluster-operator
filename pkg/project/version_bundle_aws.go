d80739292e2c78762ebd46c78b5ef1183d47bf24
Jihyuk Bok <tomahawk28@users.noreply.github.com>
2020-02-28 13:45:36 +0100

    rollback to dev version (#942)

--- pkg/project/version_bundle_aws.go
+++ pkg/project/version_bundle_aws.go
@@ -8,18 +8,10 @@ var versionBundleAWS = versionbundle.Bundle{
 	Changelogs: []versionbundle.Changelog{
 		{
 			Component:   "cluster-operator",
-			Description: "Fix cluster deletion by gracefully handling Tenant Cluster API errors.",
+			Description: "TODO",
 			Kind:        versionbundle.KindChanged,
 			URLs: []string{
-				"https://github.com/giantswarm/cluster-operator/pull/936",
-			},
-		},
-		{
-			Component:   "cluster-operator",
-			Description: "User app override rules applied",
-			Kind:        versionbundle.KindChanged,
-			URLs: []string{
-				"https://github.com/giantswarm/cluster-operator/pull/939",
+				"",
 			},
 		},
 	},
