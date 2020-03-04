2b9e197f3e86b4b4136c1ba7f83cfd3e0fa40955
Stevo Slavić <stevo@giantswarm.io>
2020-03-04 14:57:40 +0100

    Reset changelogs and bump next dev (#952)

--- pkg/project/version_bundle_aws.go
+++ pkg/project/version_bundle_aws.go
@@ -8,10 +8,10 @@ var versionBundleAWS = versionbundle.Bundle{
 	Changelogs: []versionbundle.Changelog{
 		{
 			Component:   "cluster-operator",
-			Description: "Stop setting IC replicas count",
+			Description: "TODO",
 			Kind:        versionbundle.KindChanged,
 			URLs: []string{
-				"https://github.com/giantswarm/cluster-operator/pull/950",
+				"",
 			},
 		},
 	},
