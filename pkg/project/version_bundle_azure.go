95c33fca5eb374f5b8df526fe59f718977e556b2
Stevo Slavić <stevo@giantswarm.io>
2020-03-16 10:45:40 +0100

    Expand changelog with more details

--- pkg/project/version_bundle_azure.go
+++ pkg/project/version_bundle_azure.go
@@ -8,7 +8,7 @@ var versionBundleAzure = versionbundle.Bundle{
 	Changelogs: []versionbundle.Changelog{
 		{
 			Component:   "cluster-operator",
-			Description: "Support xxs, xs, and small cluster profile detection.",
+			Description: "Support xxs, xs, and small cluster profile detection; xxs is new xs; xs and small profile rules are based on determined worker max CPU cores which is currently limited to / known for kvm provider only.",
 			Kind:        versionbundle.KindChanged,
 			URLs: []string{
 				"https://github.com/giantswarm/cluster-operator/pull/963",
