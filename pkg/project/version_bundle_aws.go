4377e256d1854f634e021172d10a6ff55202b4c1
Stevo Slavić <stevo@giantswarm.io>
2020-03-17 12:51:11 +0100

    Reset changelogs and bump next dev

--- pkg/project/version_bundle_aws.go
+++ pkg/project/version_bundle_aws.go
@@ -8,10 +8,10 @@ var versionBundleAWS = versionbundle.Bundle{
 	Changelogs: []versionbundle.Changelog{
 		{
 			Component:   "cluster-operator",
-			Description: "Support xxs, xs, and small cluster profile detection; xxs is new xs; xs and small profile rules are based on determined worker max CPU cores which is currently limited to / known for kvm provider only.",
+			Description: "TODO",
 			Kind:        versionbundle.KindChanged,
 			URLs: []string{
-				"https://github.com/giantswarm/cluster-operator/pull/963",
+				"",
 			},
 		},
 	},
