21ac7dbcfa7600b5e3d23869ec02fe3f2608cdeb
Stevo Slavić <stevo@giantswarm.io>
2020-03-17 13:06:43 +0100

    Reset changelogs and bump next dev (#976)

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -1,7 +1,7 @@
 package project
 
 var (
-	bundleVersion = "0.23.6"
+	bundleVersion = "0.23.7-dev"
 	description   = "The cluster-operator manages Kubernetes tenant cluster resources."
 	gitSHA        = "n/a"
 	name          = "cluster-operator"
