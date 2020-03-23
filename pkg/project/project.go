fc927aa10bc8aa28928429e33fd6c9eabf884bc6
Stevo Slavić <stevo@giantswarm.io>
2020-03-23 15:56:38 +0100

    Release 0.23.8 (#983)

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -1,7 +1,7 @@
 package project
 
 var (
-	bundleVersion = "0.23.8-dev"
+	bundleVersion = "0.23.8"
 	description   = "The cluster-operator manages Kubernetes tenant cluster resources."
 	gitSHA        = "n/a"
 	name          = "cluster-operator"
