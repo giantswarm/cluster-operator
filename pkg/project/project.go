33e1378b7230d0f32a0ab17d867c366360a565d3
Stevo Slavić <stevo@giantswarm.io>
2020-03-23 10:27:54 +0100

    Release 0.23.7

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -1,7 +1,7 @@
 package project
 
 var (
-	bundleVersion = "0.23.7-dev"
+	bundleVersion = "0.23.7"
 	description   = "The cluster-operator manages Kubernetes tenant cluster resources."
 	gitSHA        = "n/a"
 	name          = "cluster-operator"
