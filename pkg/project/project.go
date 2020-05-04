5ffab4090b125b7eac7220fc8e15f6db5083eabf
Jihyuk Bok <jhbok@giantswarm.io>
2020-05-04 17:08:06 +0200

    delete release.time from helm template

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,8 @@ var (
 	gitSHA      = "n/a"
 	name        = "cluster-operator"
 	source      = "https://github.com/giantswarm/cluster-operator"
-	version     = "2.1.7"
+
+//	version     = "2.1.7-1"
 )
 
 func Description() string {
@@ -25,5 +26,5 @@ func Source() string {
 }
 
 func Version() string {
-	return version
+	return "2.1.7"
 }
