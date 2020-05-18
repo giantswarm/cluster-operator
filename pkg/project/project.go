f8ebcbf7ae9c6ae358e1094229366525cb3718cb
Tim Schindler <xh3b4sd@gmail.com>
2020-05-18 22:08:41 +0200

    Revert "set dev version 2.1.11-xh3b4sd"
    This reverts commit 021b749918325ef7ef7b371d61f7ea182e268189.

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "cluster-operator"
 	source      = "https://github.com/giantswarm/cluster-operator"
-	version     = "2.1.11-xh3b4sd"
+	version     = "2.1.11-dev"
 )
 
 func Description() string {
