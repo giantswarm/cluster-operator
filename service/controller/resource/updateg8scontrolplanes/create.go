9cb05a53c094f78c9f3dbf3e5c5765d2247c4ad5
Tim Schindler <xh3b4sd@gmail.com>
2020-05-21 18:00:23 +0200

    fix linter issues

--- service/controller/resource/updateg8scontrolplanes/create.go
+++ service/controller/resource/updateg8scontrolplanes/create.go
@@ -36,6 +36,8 @@ func (r *Resource) EnsureCreated(ctx context.Context, obj interface{}) error {
 	}
 
 	for _, cp := range cpList.Items {
+		cp := cp // dereferencing pointer value into new scope
+
 		var updated bool
 
 		// Syncing the cluster-operator version.
