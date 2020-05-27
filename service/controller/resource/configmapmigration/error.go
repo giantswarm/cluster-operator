5facd1c1588c3755e03db62dbbcab9043913adb4
Ross Fairbanks <ross@rossfairbanks.com>
2020-05-27 10:35:47 +0200

    Revert "Add check to prevent nested configmaps being copied in error"
    This reverts commit 3e5346b0b4961b0023ef05c387a281998b477164.

--- service/controller/resource/configmapmigration/error.go
+++ service/controller/resource/configmapmigration/error.go
@@ -4,10 +4,6 @@ import (
 	"github.com/giantswarm/microerror"
 )
 
-var executionFailedError = &microerror.Error{
-	Kind: "executionFailedError",
-}
-
 var invalidConfigError = &microerror.Error{
 	Kind: "invalidConfigError",
 }
