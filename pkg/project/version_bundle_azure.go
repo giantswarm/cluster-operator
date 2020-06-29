030944c37588efe3e5e8bbfc0f20368506a1689d
Stevo Slavić <stevo@giantswarm.io>
2020-06-29 14:56:01 +0200

    Add changelog entries

--- pkg/project/version_bundle_azure.go
+++ pkg/project/version_bundle_azure.go
@@ -8,9 +8,11 @@ var versionBundleAzure = versionbundle.Bundle{
 	Changelogs: []versionbundle.Changelog{
 		{
 			Component:   Name(),
-			Description: "TODO",
+			Description: "Align with NGINX IC App 1.7.0, move of LB Service management from azure-operator to the app itself",
 			Kind:        versionbundle.KindChanged,
-			URLs:        []string{},
+			URLs: []string{
+				"https://github.com/giantswarm/cluster-operator/pull/1067",
+			},
 		},
 	},
 	Components: []versionbundle.Component{},
