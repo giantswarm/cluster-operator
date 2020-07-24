7d3f5a2429fb5081e535725b636931111a27c1f7
Nick Jüttner <nick@giantswarm.io>
2020-07-24 17:06:48 +0200

    handle basedomain error not found gracefully

--- service/controller/resource/controlplanestatus/resource.go
+++ service/controller/resource/controlplanestatus/resource.go
@@ -13,6 +13,7 @@ import (
 
 	"github.com/giantswarm/cluster-operator/pkg/label"
 	"github.com/giantswarm/cluster-operator/service/controller/key"
+	"github.com/giantswarm/cluster-operator/service/internal/basedomain"
 	"github.com/giantswarm/cluster-operator/service/internal/nodecount"
 	"github.com/giantswarm/cluster-operator/service/internal/tenantclient"
 )
@@ -73,6 +74,12 @@ func (r *Resource) ensure(ctx context.Context, obj interface{}) error {
 	masterNodes, err := r.nodeCount.MasterCount(ctx, cr)
 	if tenantclient.IsNotAvailable(err) {
 		r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("not getting master nodes for tenant cluster %#q", key.ClusterID(cr)))
+		r.logger.LogCtx(ctx, "level", "debug", "message", "canceling resource")
+		resourcecanceledcontext.SetCanceled(ctx)
+		return nil
+	} else if basedomain.IsNotFound(err) {
+		// in case of a cluster deletion AWSCluster CR does not exist anymore, handle basedomain error gracefully
+		r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("not getting basedomain for tenant client %#q", key.ClusterID(cr)))
 		r.logger.LogCtx(ctx, "level", "debug", "message", "canceling resource")
 		resourcecanceledcontext.SetCanceled(ctx)
 		return nil
