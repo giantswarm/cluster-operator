705e2e974408032adb41aa2370f660fc9d7e36d0
GitHub Action <action@github.com>
2020-08-21 14:29:57 +0000

    Bump version to 2.3.4-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "cluster-operator"
 	source      = "https://github.com/giantswarm/cluster-operator"
-	version     = "2.3.3"
+	version     = "2.3.4-dev"
 )
 
 func Description() string {
