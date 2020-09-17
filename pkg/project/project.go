570db8d52b88123ba4e1c31224f7a3e599af0434
Antonia von den Driesch <antonia@giantswarm.io>
2020-09-17 21:44:14 +0900

    test

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "cluster-operator"
 	source      = "https://github.com/giantswarm/cluster-operator"
-	version     = "3.2.0"
+	version     = "3.2.0-antonia"
 )
 
 func Description() string {
