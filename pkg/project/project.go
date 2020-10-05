58b621dbd55e688390cc335d0573bf626f2d891e
Tim Schindler <xh3b4sd@gmail.com>
2020-10-05 16:23:38 +0200

    set dev version 3.3.1-xh3b4sd

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "cluster-operator"
 	source      = "https://github.com/giantswarm/cluster-operator"
-	version     = "3.3.1-dev"
+	version     = "3.3.1-xh3b4sd"
 )
 
 func Description() string {
