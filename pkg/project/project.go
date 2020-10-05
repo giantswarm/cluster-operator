e8acccd4b896e0290e64a7e63fec6d2fe755c3f3
Tim Schindler <xh3b4sd@gmail.com>
2020-10-05 17:41:40 +0200

    Update pkg/project/project.go

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "cluster-operator"
 	source      = "https://github.com/giantswarm/cluster-operator"
-	version     = "3.3.1-xh3b4sd"
+	version     = "3.3.1-dev"
 )
 
 func Description() string {
