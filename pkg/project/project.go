411378bfda7ddb8fb80c4781bb111aec2ad1dcd2
Ross Fairbanks <ross@rossfairbanks.com>
2020-11-25 12:10:16 +0100

    Set version for testing

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "cluster-operator"
 	source      = "https://github.com/giantswarm/cluster-operator"
-	version     = "3.3.2-dev"
+	version     = "3.3.2-dev-rossf7"
 )
 
 func Description() string {
