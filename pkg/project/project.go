873aa31763c0f3f09b7a9fa1667abfbe6ebf8438
Ross Fairbanks <ross@rossfairbanks.com>
2020-12-11 12:41:44 +0100

    Test version

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -1,12 +1,12 @@
 package project
 
 var (
-	bundleVersion = "0.23.19"
+	bundleVersion = "0.23.19-rossf7"
 	description   = "The cluster-operator manages Kubernetes tenant cluster resources."
 	gitSHA        = "n/a"
 	name          = "cluster-operator"
 	source        = "https://github.com/giantswarm/cluster-operator"
-	version       = "0.23.19"
+	version       = "0.23.19-rossf7"
 )
 
 func BundleVersion() string {
