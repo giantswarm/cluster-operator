451e86510f8838362c5b8b1d202e00be194dfe08
Gerald Pape <gerald@giantswarm.io>
2021-01-29 12:48:58 +0100

    Bump to 0.23.23-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -1,12 +1,12 @@
 package project
 
 var (
-	bundleVersion = "0.23.22"
+	bundleVersion = "0.23.23-dev"
 	description   = "The cluster-operator manages Kubernetes tenant cluster resources."
 	gitSHA        = "n/a"
 	name          = "cluster-operator"
 	source        = "https://github.com/giantswarm/cluster-operator"
-	version       = "0.23.22"
+	version       = "0.23.23-dev"
 )
 
 func BundleVersion() string {
