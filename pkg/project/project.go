048fe058318a4bf74341b8697e53fe4190ab9acb
Gerald Pape <ubergesundheit@users.noreply.github.com>
2021-01-29 09:32:58 +0100

    Manually apply changes to project.go

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -1,12 +1,12 @@
 package project
 
 var (
-	bundleVersion = "0.23.21-dev"
+	bundleVersion = "0.23.22-dev"
 	description   = "The cluster-operator manages Kubernetes tenant cluster resources."
 	gitSHA        = "n/a"
 	name          = "cluster-operator"
 	source        = "https://github.com/giantswarm/cluster-operator"
-	version       = "0.23.21"
+	version       = "0.23.22-dev"
 )
 
 func BundleVersion() string {
