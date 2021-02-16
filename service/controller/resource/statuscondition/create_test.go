22cbe6fe60ba0263280c26c157a08a893b2ca1d2
Antonia von den Driesch <antonia@giantswarm.io>
2021-02-16 17:53:14 +0900

    simplify test

--- service/controller/resource/statuscondition/create_test.go
+++ service/controller/resource/statuscondition/create_test.go
@@ -6,30 +6,19 @@ import (
 	"testing"
 
 	infrastructurev1alpha2 "github.com/giantswarm/apiextensions/v3/pkg/apis/infrastructure/v1alpha2"
-	"github.com/giantswarm/apiextensions/v3/pkg/apis/release/v1alpha1"
 	"github.com/giantswarm/k8sclient/v5/pkg/k8sclienttest"
 	"github.com/giantswarm/micrologger/microloggertest"
 	apiv1alpha2 "sigs.k8s.io/cluster-api/api/v1alpha2"
 
-	v1 "k8s.io/api/core/v1"
-
 	"github.com/giantswarm/cluster-operator/v3/service/internal/recorder"
-	"github.com/giantswarm/cluster-operator/v3/service/internal/releaseversion"
-	tcunittest "github.com/giantswarm/cluster-operator/v3/service/internal/tenantclient/unittest"
 	"github.com/giantswarm/cluster-operator/v3/service/internal/unittest"
 )
-
-type nodeConfig struct {
-	replicas      int
-	readyReplicas int
-}
 
 func TestComputeClusterStatusConditions(t *testing.T) {
 	testCases := []struct {
 		name string
 
-		controlPlane    nodeConfig
-		nodePools       []nodeConfig
+		nodesReady      bool
 		conditions      []infrastructurev1alpha2.CommonClusterStatusCondition
 		versions        []infrastructurev1alpha2.CommonClusterStatusVersion
 		operatorVersion string
@@ -41,8 +30,7 @@ func TestComputeClusterStatusConditions(t *testing.T) {
 		{
 			name: "case 0",
 
-			controlPlane:    nodeConfig{1, 0},
-			nodePools:       []nodeConfig{{2, 0}},
+			nodesReady:      false,
 			conditions:      []infrastructurev1alpha2.CommonClusterStatusCondition{},
 			versions:        []infrastructurev1alpha2.CommonClusterStatusVersion{},
 			operatorVersion: "8.7.5",
@@ -54,8 +42,7 @@ func TestComputeClusterStatusConditions(t *testing.T) {
 		{
 			name: "case 1",
 
-			controlPlane: nodeConfig{1, 0},
-			nodePools:    []nodeConfig{{2, 1}},
+			nodesReady: false,
 			conditions: []infrastructurev1alpha2.CommonClusterStatusCondition{
 				unittest.GetCreatingCondition(90),
 			},
@@ -69,8 +56,7 @@ func TestComputeClusterStatusConditions(t *testing.T) {
 		{
 			name: "case 2",
 
-			controlPlane: nodeConfig{1, 1},
-			nodePools:    []nodeConfig{{2, 2}},
+			nodesReady: true,
 			conditions: []infrastructurev1alpha2.CommonClusterStatusCondition{
 				unittest.GetCreatingCondition(90),
 			},
@@ -84,8 +70,7 @@ func TestComputeClusterStatusConditions(t *testing.T) {
 		{
 			name: "case 3",
 
-			controlPlane: nodeConfig{1, 1},
-			nodePools:    []nodeConfig{{2, 2}, {2, 0}},
+			nodesReady: false,
 			conditions: []infrastructurev1alpha2.CommonClusterStatusCondition{
 				unittest.GetCreatedCondition(60),
 				unittest.GetCreatingCondition(90),
@@ -102,8 +87,7 @@ func TestComputeClusterStatusConditions(t *testing.T) {
 		{
 			name: "case 4",
 
-			controlPlane: nodeConfig{1, 0},
-			nodePools:    []nodeConfig{{2, 2}, {2, 0}},
+			nodesReady: false,
 			conditions: []infrastructurev1alpha2.CommonClusterStatusCondition{
 				unittest.GetCreatedCondition(60),
 				unittest.GetCreatingCondition(90),
@@ -120,8 +104,7 @@ func TestComputeClusterStatusConditions(t *testing.T) {
 		{
 			name: "case 5",
 
-			controlPlane: nodeConfig{1, 0},
-			nodePools:    []nodeConfig{{2, 2}, {2, 0}},
+			nodesReady: false,
 			conditions: []infrastructurev1alpha2.CommonClusterStatusCondition{
 				unittest.GetUpdatingCondition(15),
 				unittest.GetCreatedCondition(60),
@@ -139,8 +122,7 @@ func TestComputeClusterStatusConditions(t *testing.T) {
 		{
 			name: "case 6",
 
-			controlPlane: nodeConfig{1, 1},
-			nodePools:    []nodeConfig{{2, 2}, {2, 2}},
+			nodesReady: true,
 			conditions: []infrastructurev1alpha2.CommonClusterStatusCondition{
 				unittest.GetUpdatingCondition(15),
 				unittest.GetCreatedCondition(60),
@@ -158,8 +140,7 @@ func TestComputeClusterStatusConditions(t *testing.T) {
 		{
 			name: "case 7",
 
-			controlPlane: nodeConfig{3, 1},
-			nodePools:    []nodeConfig{{2, 2}, {2, 2}},
+			nodesReady: false,
 			conditions: []infrastructurev1alpha2.CommonClusterStatusCondition{
 				unittest.GetUpdatedCondition(5),
 				unittest.GetUpdatingCondition(15),
@@ -179,8 +160,7 @@ func TestComputeClusterStatusConditions(t *testing.T) {
 		{
 			name: "case 8",
 
-			controlPlane: nodeConfig{3, 1},
-			nodePools:    []nodeConfig{{2, 0}, {2, 1}},
+			nodesReady: false,
 			conditions: []infrastructurev1alpha2.CommonClusterStatusCondition{
 				unittest.GetUpdatedCondition(5),
 				unittest.GetUpdatingCondition(15),
@@ -204,33 +184,6 @@ func TestComputeClusterStatusConditions(t *testing.T) {
 			fakek8sclient := unittest.FakeK8sClient()
 			ctx := context.Background()
 
-			// The worker and master nodes are created
-			var nodes []v1.Node
-			var cps []infrastructurev1alpha2.G8sControlPlane
-			var mds []apiv1alpha2.MachineDeployment
-			{
-				for _, nodePool := range tc.nodePools {
-					md := unittest.DefaultMachineDeployment()
-					md.Status.Replicas = int32(nodePool.replicas)
-					md.Status.ReadyReplicas = int32(nodePool.readyReplicas)
-					mds = append(mds, md)
-					for i := 0; i < nodePool.readyReplicas; i++ {
-						workerNode := unittest.NewWorkerNode()
-						workerNode.Labels["aws-operator.giantswarm.io/version"] = tc.operatorVersion
-						nodes = append(nodes, workerNode)
-					}
-				}
-				cp := unittest.DefaultControlPlane()
-				cp.Status.Replicas = int32(tc.controlPlane.replicas)
-				cp.Status.ReadyReplicas = int32(tc.controlPlane.readyReplicas)
-				cps = append(cps, cp)
-				for i := 0; i < tc.controlPlane.readyReplicas; i++ {
-					masterNode := unittest.NewMasterNode()
-					masterNode.Labels["aws-operator.giantswarm.io/version"] = tc.operatorVersion
-					nodes = append(nodes, masterNode)
-				}
-			}
-
 			// The cluster is created
 			var cluster infrastructurev1alpha2.AWSCluster
 			var cl apiv1alpha2.Cluster
@@ -241,49 +194,21 @@ func TestComputeClusterStatusConditions(t *testing.T) {
 				cl = apiv1alpha2.Cluster{}
 			}
 
-			// The release is created
-			var release v1alpha1.Release
-			{
-				release = unittest.DefaultRelease()
-				release.Spec.Components[1].Version = tc.operatorVersion
-			}
-			err := fakek8sclient.CtrlClient().Create(ctx, &release)
-			if err != nil {
-				t.Fatal(err)
-			}
-
 			var e recorder.Interface
 			{
 				c := recorder.Config{
 					K8sClient: k8sclienttest.NewEmpty(),
 				}
 				e = recorder.New(c)
-				if err != nil {
-					t.Fatal(err)
-				}
-			}
-			var rv *releaseversion.ReleaseVersion
-			{
-				c := releaseversion.Config{
-					K8sClient: fakek8sclient,
-				}
-				rv, err = releaseversion.New(c)
-				if err != nil {
-					t.Fatal(err)
-				}
 			}
 
 			r := Resource{
-				event:                      e,
-				k8sClient:                  fakek8sclient,
-				logger:                     microloggertest.New(),
-				releaseVersion:             rv,
-				tenantClient:               tcunittest.FakeTenantClient(fakek8sclient),
-				newCommonClusterObjectFunc: newCommonClusterObjectFunc("aws"),
-				provider:                   "aws",
+				event:     e,
+				k8sClient: fakek8sclient,
+				logger:    microloggertest.New(),
 			}
 
-			err = r.computeClusterStatusConditions(ctx, cl, &cluster, nodes, cps, mds)
+			err := r.writeClusterStatusConditions(ctx, cl, &cluster, tc.nodesReady, tc.operatorVersion)
 			if err != nil {
 				t.Fatal(err)
 			}
@@ -308,13 +233,6 @@ func TestComputeClusterStatusConditions(t *testing.T) {
 				t.Fatalf("expected %#q to differ from %#q", tc.expectVersion, currentVersion)
 			}
 		})
-	}
-}
-
-func newCommonClusterObjectFunc(provider string) func() infrastructurev1alpha2.CommonClusterObject {
-	// Deal with different providers in here once they reach Cluster API.
-	return func() infrastructurev1alpha2.CommonClusterObject {
-		return new(infrastructurev1alpha2.AWSCluster)
 	}
 }
 
