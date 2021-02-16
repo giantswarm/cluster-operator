1481717dbf9dfedf0bb7ffa57746ad1d245dba31
Antonia <56017655+anvddriesch@users.noreply.github.com>
2021-02-16 19:09:29 +0900

    add separate unit tests for cluster status computation (#1329)
    * splitting computeCreateClusterStatusConditions into separate logic
    
    * add separate unit tests for cluster status computation
    
    * typo
    
    * add more cases
    
    * whitespace

--- service/controller/resource/statuscondition/create_test.go
+++ service/controller/resource/statuscondition/create_test.go
@@ -24,7 +24,7 @@ type nodeConfig struct {
 	readyReplicas int
 }
 
-func TestComputeCreateClusterStatusConditions(t *testing.T) {
+func TestComputeClusterStatusConditions(t *testing.T) {
 	testCases := []struct {
 		name string
 
@@ -283,7 +283,7 @@ func TestComputeCreateClusterStatusConditions(t *testing.T) {
 				provider:                   "aws",
 			}
 
-			err = r.computeCreateClusterStatusConditions(ctx, cl, &cluster, nodes, cps, mds)
+			err = r.computeClusterStatusConditions(ctx, cl, &cluster, nodes, cps, mds)
 			if err != nil {
 				t.Fatal(err)
 			}
@@ -315,5 +315,423 @@ func newCommonClusterObjectFunc(provider string) func() infrastructurev1alpha2.C
 	// Deal with different providers in here once they reach Cluster API.
 	return func() infrastructurev1alpha2.CommonClusterObject {
 		return new(infrastructurev1alpha2.AWSCluster)
+	}
+}
+
+func TestComputeCreatingCondition(t *testing.T) {
+	testCases := []struct {
+		name string
+
+		status         infrastructurev1alpha2.CommonClusterStatus
+		expectedResult bool
+	}{
+		// There are no previous versions or conditions in the status
+		{
+			name: "case 0",
+
+			status: infrastructurev1alpha2.CommonClusterStatus{
+				Conditions: []infrastructurev1alpha2.CommonClusterStatusCondition{},
+				Versions:   []infrastructurev1alpha2.CommonClusterStatusVersion{},
+			},
+			expectedResult: true,
+		},
+		// There are previous versions in the status
+		{
+			name: "case 1",
+
+			status: infrastructurev1alpha2.CommonClusterStatus{
+				Conditions: []infrastructurev1alpha2.CommonClusterStatusCondition{},
+				Versions: []infrastructurev1alpha2.CommonClusterStatusVersion{
+					unittest.GetVersion(5, "8.7.6"),
+				},
+			},
+			expectedResult: false,
+		},
+		// There are previous conditions in the status
+		{
+			name: "case 2",
+
+			status: infrastructurev1alpha2.CommonClusterStatus{
+				Conditions: []infrastructurev1alpha2.CommonClusterStatusCondition{
+					unittest.GetCreatingCondition(90),
+				},
+				Versions: []infrastructurev1alpha2.CommonClusterStatusVersion{},
+			},
+			expectedResult: false,
+		},
+		// There are previous conditions and versions in the status
+		{
+			name: "case 3",
+
+			status: infrastructurev1alpha2.CommonClusterStatus{
+				Conditions: []infrastructurev1alpha2.CommonClusterStatusCondition{
+					unittest.GetCreatingCondition(90),
+				},
+				Versions: []infrastructurev1alpha2.CommonClusterStatusVersion{
+					unittest.GetVersion(60, "8.7.6"),
+				},
+			},
+			expectedResult: false,
+		},
+		// There are previous conditions and versions in the status
+		{
+			name: "case 4",
+
+			status: infrastructurev1alpha2.CommonClusterStatus{
+				Conditions: []infrastructurev1alpha2.CommonClusterStatusCondition{
+					unittest.GetUpdatedCondition(10),
+					unittest.GetUpdatingCondition(30),
+					unittest.GetCreatedCondition(60),
+					unittest.GetCreatingCondition(90),
+				},
+				Versions: []infrastructurev1alpha2.CommonClusterStatusVersion{
+					unittest.GetVersion(60, "8.7.5"),
+					unittest.GetVersion(60, "8.7.6"),
+				},
+			},
+			expectedResult: false,
+		},
+	}
+
+	for i, tc := range testCases {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			result := computeCreatingCondition(tc.status)
+			if result != tc.expectedResult {
+				t.Fatalf("expected %v, got %v", tc.expectedResult, result)
+			}
+		})
+	}
+}
+func TestComputeCreatedCondition(t *testing.T) {
+	testCases := []struct {
+		name string
+
+		status     infrastructurev1alpha2.CommonClusterStatus
+		nodesReady bool
+
+		expectedResult bool
+	}{
+		// the cluster is creating and nodes are ready
+		{
+			name: "case 0",
+
+			status: infrastructurev1alpha2.CommonClusterStatus{
+				Conditions: []infrastructurev1alpha2.CommonClusterStatusCondition{
+					unittest.GetCreatingCondition(90),
+				},
+			},
+			nodesReady: true,
+
+			expectedResult: true,
+		},
+		// the cluster is creating but nodes are not ready
+		{
+			name: "case 1",
+
+			status: infrastructurev1alpha2.CommonClusterStatus{
+				Conditions: []infrastructurev1alpha2.CommonClusterStatusCondition{
+					unittest.GetCreatingCondition(90),
+				},
+			},
+			nodesReady: false,
+
+			expectedResult: false,
+		},
+		// The cluster already has a created condition
+		{
+			name: "case 2",
+
+			status: infrastructurev1alpha2.CommonClusterStatus{
+				Conditions: []infrastructurev1alpha2.CommonClusterStatusCondition{
+					unittest.GetCreatedCondition(60),
+					unittest.GetCreatingCondition(90),
+				},
+			},
+			nodesReady: true,
+
+			expectedResult: false,
+		},
+	}
+
+	for i, tc := range testCases {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			result := computeCreatedCondition(tc.status, tc.nodesReady)
+			if result != tc.expectedResult {
+				t.Fatalf("expected %v, got %v", tc.expectedResult, result)
+			}
+		})
+	}
+}
+func TestComputeUpdatingCondition(t *testing.T) {
+	testCases := []struct {
+		name string
+
+		status         infrastructurev1alpha2.CommonClusterStatus
+		desiredVersion string
+
+		expectedResult bool
+	}{
+		// the cluster is created and not in updating state but the desired version differs from the current version
+		{
+			name: "case 0",
+
+			status: infrastructurev1alpha2.CommonClusterStatus{
+				Conditions: []infrastructurev1alpha2.CommonClusterStatusCondition{
+					unittest.GetCreatedCondition(60),
+					unittest.GetCreatingCondition(90),
+				},
+				Versions: []infrastructurev1alpha2.CommonClusterStatusVersion{
+					unittest.GetVersion(60, "8.7.5"),
+				},
+			},
+			desiredVersion: "8.7.6",
+
+			expectedResult: true,
+		},
+		// the cluster does not have a created condition
+		{
+			name: "case 1",
+
+			status: infrastructurev1alpha2.CommonClusterStatus{
+				Conditions: []infrastructurev1alpha2.CommonClusterStatusCondition{
+					unittest.GetCreatingCondition(90),
+				},
+				Versions: []infrastructurev1alpha2.CommonClusterStatusVersion{
+					unittest.GetVersion(60, "8.7.5"),
+				},
+			},
+			desiredVersion: "8.7.6",
+
+			expectedResult: false,
+		},
+		// the cluster is already updating
+		{
+			name: "case 2",
+
+			status: infrastructurev1alpha2.CommonClusterStatus{
+				Conditions: []infrastructurev1alpha2.CommonClusterStatusCondition{
+					unittest.GetUpdatingCondition(30),
+					unittest.GetCreatedCondition(60),
+					unittest.GetCreatingCondition(90),
+				},
+				Versions: []infrastructurev1alpha2.CommonClusterStatusVersion{
+					unittest.GetVersion(60, "8.7.5"),
+				},
+			},
+			desiredVersion: "8.7.6",
+
+			expectedResult: false,
+		},
+		// the version is already changed
+		{
+			name: "case 3",
+
+			status: infrastructurev1alpha2.CommonClusterStatus{
+				Conditions: []infrastructurev1alpha2.CommonClusterStatusCondition{
+					unittest.GetCreatedCondition(60),
+					unittest.GetCreatingCondition(90),
+				},
+				Versions: []infrastructurev1alpha2.CommonClusterStatusVersion{
+					unittest.GetVersion(60, "8.7.6"),
+				},
+			},
+			desiredVersion: "8.7.6",
+
+			expectedResult: false,
+		},
+		// the version is already present and cluster is already updating
+		{
+			name: "case 4",
+
+			status: infrastructurev1alpha2.CommonClusterStatus{
+				Conditions: []infrastructurev1alpha2.CommonClusterStatusCondition{
+					unittest.GetUpdatingCondition(30),
+					unittest.GetCreatedCondition(60),
+					unittest.GetCreatingCondition(90),
+				},
+				Versions: []infrastructurev1alpha2.CommonClusterStatusVersion{
+					unittest.GetVersion(60, "8.7.5"),
+					unittest.GetVersion(60, "8.7.6"),
+				},
+			},
+			desiredVersion: "8.7.6",
+
+			expectedResult: false,
+		},
+	}
+
+	for i, tc := range testCases {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			result := computeUpdatingCondition(tc.status, tc.desiredVersion)
+			if result != tc.expectedResult {
+				t.Fatalf("expected %v, got %v", tc.expectedResult, result)
+			}
+		})
+	}
+}
+func TestComputeUpdatedCondition(t *testing.T) {
+	testCases := []struct {
+		name string
+
+		status     infrastructurev1alpha2.CommonClusterStatus
+		nodesReady bool
+
+		expectedResult bool
+	}{
+		// the cluster is updating and nodes are ready
+		// TODO this should also work a second time
+		{
+			name: "case 0",
+
+			status: infrastructurev1alpha2.CommonClusterStatus{
+				Conditions: []infrastructurev1alpha2.CommonClusterStatusCondition{
+					unittest.GetUpdatingCondition(30),
+					unittest.GetCreatedCondition(60),
+					unittest.GetCreatingCondition(90),
+				},
+			},
+			nodesReady: true,
+
+			expectedResult: true,
+		},
+		// the cluster is updating and nodes are not ready
+		{
+			name: "case 1",
+
+			status: infrastructurev1alpha2.CommonClusterStatus{
+				Conditions: []infrastructurev1alpha2.CommonClusterStatusCondition{
+					unittest.GetUpdatingCondition(30),
+					unittest.GetCreatedCondition(60),
+					unittest.GetCreatingCondition(90),
+				},
+			},
+			nodesReady: false,
+
+			expectedResult: false,
+		},
+		// the cluster is already updated
+		{
+			name: "case 1",
+
+			status: infrastructurev1alpha2.CommonClusterStatus{
+				Conditions: []infrastructurev1alpha2.CommonClusterStatusCondition{
+					unittest.GetUpdatedCondition(10),
+					unittest.GetUpdatingCondition(30),
+					unittest.GetCreatedCondition(60),
+					unittest.GetCreatingCondition(90),
+				},
+			},
+			nodesReady: true,
+
+			expectedResult: false,
+		},
+	}
+
+	for i, tc := range testCases {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			result := computeUpdatedCondition(tc.status, tc.nodesReady)
+			if result != tc.expectedResult {
+				t.Fatalf("expected %v, got %v", tc.expectedResult, result)
+			}
+		})
+	}
+}
+
+func TestComputeVersionChange(t *testing.T) {
+	testCases := []struct {
+		name string
+
+		status         infrastructurev1alpha2.CommonClusterStatus
+		nodesReady     bool
+		desiredVersion string
+
+		expectedResult bool
+	}{
+		// the cluster has transitioned, nodes are ready but the version is not set yet
+		{
+			name: "case 0",
+
+			status: infrastructurev1alpha2.CommonClusterStatus{
+				Conditions: []infrastructurev1alpha2.CommonClusterStatusCondition{
+					unittest.GetUpdatedCondition(10),
+					unittest.GetUpdatingCondition(30),
+					unittest.GetCreatedCondition(60),
+					unittest.GetCreatingCondition(90),
+				},
+				Versions: []infrastructurev1alpha2.CommonClusterStatusVersion{
+					unittest.GetVersion(60, "8.7.5"),
+				},
+			},
+			nodesReady:     true,
+			desiredVersion: "8.7.6",
+
+			expectedResult: true,
+		},
+		// the cluster has not transitioned yet
+		// TODO this should also work for updates
+		{
+			name: "case 1",
+
+			status: infrastructurev1alpha2.CommonClusterStatus{
+				Conditions: []infrastructurev1alpha2.CommonClusterStatusCondition{
+					unittest.GetCreatingCondition(90),
+				},
+				Versions: []infrastructurev1alpha2.CommonClusterStatusVersion{},
+			},
+			nodesReady:     true,
+			desiredVersion: "8.7.5",
+
+			expectedResult: false,
+		},
+		// the nodes are not ready yet
+		{
+			name: "case 2",
+
+			status: infrastructurev1alpha2.CommonClusterStatus{
+				Conditions: []infrastructurev1alpha2.CommonClusterStatusCondition{
+					unittest.GetUpdatedCondition(10),
+					unittest.GetUpdatingCondition(30),
+					unittest.GetCreatedCondition(60),
+					unittest.GetCreatingCondition(90),
+				},
+				Versions: []infrastructurev1alpha2.CommonClusterStatusVersion{
+					unittest.GetVersion(60, "8.7.5"),
+				},
+			},
+			nodesReady:     false,
+			desiredVersion: "8.7.6",
+
+			expectedResult: false,
+		},
+		// the version is already set
+		{
+			name: "case 3",
+
+			status: infrastructurev1alpha2.CommonClusterStatus{
+				Conditions: []infrastructurev1alpha2.CommonClusterStatusCondition{
+					unittest.GetUpdatedCondition(10),
+					unittest.GetUpdatingCondition(30),
+					unittest.GetCreatedCondition(60),
+					unittest.GetCreatingCondition(90),
+				},
+				Versions: []infrastructurev1alpha2.CommonClusterStatusVersion{
+					unittest.GetVersion(60, "8.7.5"),
+					unittest.GetVersion(60, "8.7.6"),
+				},
+			},
+			nodesReady:     true,
+			desiredVersion: "8.7.6",
+
+			expectedResult: false,
+		},
+	}
+
+	for i, tc := range testCases {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			result := computeVersionChange(tc.status, tc.nodesReady, tc.desiredVersion)
+			if result != tc.expectedResult {
+				t.Fatalf("expected %v, got %v", tc.expectedResult, result)
+			}
+		})
 	}
 }
