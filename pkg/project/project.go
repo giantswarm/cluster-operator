dc5bd97b81469dc0a04df5aedca5cd3f37e760ac
Zach Stone <zach@giantswarm.io>
2021-02-23 19:57:10 +0100

    Bump dev version

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -1,12 +1,12 @@
 package project
 
 var (
-	bundleVersion = "0.24.1"
+	bundleVersion = "0.24.2-dev"
 	description   = "The cluster-operator manages Kubernetes tenant cluster resources."
 	gitSHA        = "n/a"
 	name          = "cluster-operator"
 	source        = "https://github.com/giantswarm/cluster-operator"
-	version       = "0.24.1"
+	version       = "0.24.2-dev"
 )
 
 func BundleVersion() string {
