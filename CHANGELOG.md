d9a7c61977fa1c6b0ac106a4b823ac69933fe793
Zach Stone <zach@giantswarm.io>
2021-02-23 19:23:05 +0100

    Set both versions to 0.24.1 for new release (#1347)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Changed
+
+- Align version bundle version and project version.
+
 ## [0.24.0] - 2021-02-23
 
 ### Changed
--- pkg/project/project.go
+++ pkg/project/project.go
@@ -1,12 +1,12 @@
 package project
 
 var (
-	bundleVersion = "0.24.1-dev"
+	bundleVersion = "0.24.1"
 	description   = "The cluster-operator manages Kubernetes tenant cluster resources."
 	gitSHA        = "n/a"
 	name          = "cluster-operator"
 	source        = "https://github.com/giantswarm/cluster-operator"
-	version       = "0.24.1-dev"
+	version       = "0.24.1"
 )
 
 func BundleVersion() string {
