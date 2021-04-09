d705e99f1379c7527f6dea7812c70485247ce5d1
Nick Jüttner <nick@giantswarm.io>
2021-04-09 11:34:38 +0200

    fix client fake deprecation

--- service/internal/unittest/default_k8sclient.go
+++ service/internal/unittest/default_k8sclient.go
@@ -11,6 +11,8 @@ import (
 	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/kubernetes"
 	fakek8s "k8s.io/client-go/kubernetes/fake"
+
+	//nolint:staticcheck // v0.6.4 has a deprecation on pkg/client/fake that was removed in later versions
 	"k8s.io/client-go/rest"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
