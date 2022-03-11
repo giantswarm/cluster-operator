0dcc29f843dc13da9fd34d0fe16aece81877caf4
GitHub Action <action@github.com>
2022-03-11 14:12:35 +0000

    Bump version to 3.14.2-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "cluster-operator"
 	source      = "https://github.com/giantswarm/cluster-operator"
-	version     = "3.14.1"
+	version     = "3.14.2-dev"
 )
 
 func Description() string {
