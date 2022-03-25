392083a69927a2d7450af448bc765ef8ba0f6d10
Nick Jüttner <nick@giantswarm.io>
2022-03-25 13:20:44 +0100

    tidy

--- service/internal/unittest/default_machinedeployment.go
+++ service/internal/unittest/default_machinedeployment.go
@@ -2,16 +2,16 @@ package unittest
 
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	apiv1beta1 "sigs.k8s.io/cluster-api/api/v1beta1
+	apiv1beta1 "sigs.k8s.io/cluster-api/api/v1beta1"
 )
 
-func DefaultMachineDeployment() apiv1alpha3.MachineDeployment {
-	cr := apiv1alpha3.MachineDeployment{
+func DefaultMachineDeployment() apiv1beta1.MachineDeployment {
+	cr := apiv1beta1.MachineDeployment{
 		TypeMeta: metav1.TypeMeta{
 			Kind:       "MachineDeployment",
 			APIVersion: "cluster.x-k8s.io/v1alpha3",
 		},
-		Status: apiv1alpha3.MachineDeploymentStatus{
+		Status: apiv1beta1.MachineDeploymentStatus{
 			ObservedGeneration:  0,
 			Selector:            "",
 			Replicas:            1,
