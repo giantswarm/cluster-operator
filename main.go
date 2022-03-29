c530d60a095915b28a111c09b09e88d4f0b2f363
Nick Jüttner <nick@giantswarm.io>
2022-03-29 11:22:40 +0200

    upgrade version of cluster-operator

--- main.go
+++ main.go
@@ -9,10 +9,10 @@ import (
 	"github.com/giantswarm/micrologger"
 	"github.com/spf13/viper"
 
-	"github.com/giantswarm/cluster-operator/v3/flag"
-	"github.com/giantswarm/cluster-operator/v3/pkg/project"
-	"github.com/giantswarm/cluster-operator/v3/server"
-	"github.com/giantswarm/cluster-operator/v3/service"
+	"github.com/giantswarm/cluster-operator/v4/flag"
+	"github.com/giantswarm/cluster-operator/v4/pkg/project"
+	"github.com/giantswarm/cluster-operator/v4/server"
+	"github.com/giantswarm/cluster-operator/v4/service"
 )
 
 var (
