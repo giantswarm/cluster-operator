aebcf6bad859b7a7fe25d262ed0ee8f710046303
Mario Nitchev <marionitchev@gmail.com>
2023-07-18 13:05:52 +0300

    Patch all apps with current app operator version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Changed
+
+- Patch app operator version on all apps instead of just optional ones.
+
 ## [5.6.1] - 2023-05-03
 
 ### Fixed
--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "cluster-operator"
 	source      = "https://github.com/giantswarm/cluster-operator"
-	version     = "5.6.1"
+	// version     = "5.6.1-patch1"
 )
 
 func Description() string {
@@ -25,5 +25,5 @@ func Source() string {
 }
 
 func Version() string {
-	return version
+	return "5.6.1"
 }
