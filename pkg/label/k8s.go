3393451fa42f5f07d146e795fca84b1fd4c7e84f
Christian Bianchi <whites11@gmail.com>
2023-08-28 08:23:15 +0200

    removed unused var

--- pkg/label/k8s.go
+++ pkg/label/k8s.go
@@ -1,7 +1,6 @@
 package label
 
 const (
-	MasterNodeRole = "node-role.kubernetes.io/master"
 	// WorkerNodeRole label denotes K8s cluster worker node role.
 	WorkerNodeRole = "node-role.kubernetes.io/worker"
 )
